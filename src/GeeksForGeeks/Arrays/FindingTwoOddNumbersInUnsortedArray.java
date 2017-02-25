29c5ccc240d552fa832396e402a46227a282dcb0
Srinivas Vemula <srivemul@oradev.oraclecorp.com>
2017-02-25 20:42:47 +0530

    GeeksForGeeks

--- src/GeeksForGeeks/Arrays/FindingTwoOddNumbersInUnsortedArray.java
+++ src/GeeksForGeeks/Arrays/FindingTwoOddNumbersInUnsortedArray.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package GeeksForGeeks;
+package GeeksForGeeks.Arrays;
 
 import java.util.Scanner;
 
