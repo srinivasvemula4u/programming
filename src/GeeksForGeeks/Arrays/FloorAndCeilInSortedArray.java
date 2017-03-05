2ad629df4aada5b5c8376fcc628d570fbc242e5e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-05 19:36:47 +0530

    GeeksForGeeeks

--- src/GeeksForGeeks/Arrays/FloorAndCeilInSortedArray.java
+++ src/GeeksForGeeks/Arrays/FloorAndCeilInSortedArray.java
@@ -17,7 +17,7 @@ public class FloorAndCeilInSortedArray {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arr = {1,2,8,10,10,12,19};
-        int x = 20;
+        int x = 4;
         int ceil = findCeil(arr,x);
         if(ceil == -1) {
             System.out.println("No ceil present in the array");
@@ -26,6 +26,20 @@ public class FloorAndCeilInSortedArray {
         }
         System.out.println();
         int floor = findFloor(arr,x);
+        if(floor == -1) {
+            System.out.println("No floor present in the array");
+        } else {
+            System.out.println("floor of " + x + " is " + arr[floor]);
+        }
+        System.out.println();
+        ceil = findCeilUsingBinarySearch(arr,0,arr.length-1,x);
+        if(ceil == -1) {
+            System.out.println("No ceil present in the array");
+        } else {
+            System.out.println("Ceil of " + x + " is " + arr[ceil]);
+        }
+        System.out.println();
+        floor = findFloorByUsingBinarySearch(arr,0,arr.length-1,x);
         if(floor == -1) {
             System.out.println("No floor present in the array");
         } else {
@@ -46,6 +60,28 @@ public class FloorAndCeilInSortedArray {
        }
        return -1;
     }
+    private static int findCeilUsingBinarySearch(int[] arr, int low, int high,int x) {
+        if(arr[low] >= x)
+            return low;
+        if( x > arr[high]) 
+            return -1;
+        int mid = (low + high) / 2;
+        if( arr[mid] == x)
+            return mid;
+        else if(arr[mid] < x) {
+            if( mid + 1 <= high && arr[mid+1] >= x)
+                return mid+1;
+            else 
+                return findCeilUsingBinarySearch(arr, mid+1, high, x);
+        }
+        else {
+            if( mid -1  >= low && arr[mid-1] < x) {
+                return mid;
+            }
+            else 
+                return findCeilUsingBinarySearch(arr, low, mid-1, x);
+        }
+    }
      private static int findFloor(int[] arr,int x) {
        int low = 0, high = arr.length-1;
        if( x < arr[low] )
@@ -60,4 +96,26 @@ public class FloorAndCeilInSortedArray {
            return high;
        return -1;
     }
+     private static int findFloorByUsingBinarySearch(int[] arr, int low, int high,int x) {
+         if(x < arr[low] ) 
+             return -1;
+         if(x > arr[high])
+             return high;
+         int mid = (low+high)/2;
+         if(arr[mid] == x)
+             return mid;
+         if(arr[mid] < x) {
+             if( mid +1 <= high && arr[mid+1] > x)
+                 return mid;
+             else 
+                 return findFloorByUsingBinarySearch(arr, mid+1, high, x);
+         }
+         else {
+             if( mid -1 >= low && arr[mid - 1] <= x)
+                 return mid-1;
+             else return findFloorByUsingBinarySearch(arr, low, mid-1, x);
+         }
+      
+         
+     }
 }
