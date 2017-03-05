26dcf64ce1eaf4cf35f6c32aa376bf9db3d9c4f0
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-05 22:17:48 +0530

    GeeksForGeeeks

---
+++ src/GeeksForGeeks/Arrays/ProductArrayPuzzle.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays;
+
+/**
+ *
+ * @author srivemul
+ */
+public class ProductArrayPuzzle {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static void productArrayPuzzleMethod1(int[] arr) {
+        int n = arr.length;
+        int[] tempLeft = new int[arr.length];
+        int[] tempRight = new int[arr.length]; 
+        int[] prod = new int[arr.length];
+        tempLeft[0] = 1;
+        for(int i =1; i < n; i++) {
+          tempLeft[i] = arr[i-1]*tempLeft[i-1];            
+        }
+        tempRight[n-1] = 1;
+        for(int j = n - 2; j >= 0; j--) {
+            tempRight[j] = arr[j+1]*tempRight[j+1];
+        }
+        for(int i= 0; i < n; i++) {
+            prod[i] = tempLeft[i] * tempRight[i];
+        }
+        printArray(prod);
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 3, 5, 6, 2};
+        productArrayPuzzleMethod1(arr);
+      }
+
+    private static void printArray(int[] prod) {
+       for(int val : prod) {
+           System.out.print(val + " ");
+       }
+       System.out.println();
+    }
+    
+}
