329a8572831e2080e62a1d39cb91c33280180042
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-07 08:23:16 +0530

    GeeksForGeeks

--- src/GeeksForGeeks/Arrays/NextGreaterElementInArray.java
+++ src/GeeksForGeeks/Arrays/NextGreaterElementInArray.java
@@ -5,6 +5,8 @@
  */
 package GeeksForGeeks.Arrays;
 
+import java.util.Stack;
+
 /**
  *
  * @author srivemul
@@ -30,9 +32,37 @@ public class NextGreaterElementInArray {
        }
        
     }
+    private static void greaterElementMethod2(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        int next = Integer.MIN_VALUE;
+        for(int i=1; i < arr.length;  i++) {
+           next = arr[i];
+           if(!stack.isEmpty()) {
+               int element = stack.pop();
+               while(element < next) {
+                   System.out.println(element + " ------------ " + next);
+                   if(stack.isEmpty())
+                       break;
+                   element = stack.pop();
+               }
+               if(element > next)
+                   stack.push(element);
+           }
+           stack.push(next);
+        }
+        while(!stack.isEmpty()) {
+            int element = stack.pop();
+            next = -1;
+            System.out.println(element + " ------------ " + next);
+        }
+        
+    }
     public static void main(String[] args) {
        int[] arr= {11, 13, 21, 3};
         greaterElementMethod(arr);
+        System.out.println("*******************");
+        greaterElementMethod2(arr);
     }
     
 }
