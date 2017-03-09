a6f49fb935de69d37251c2b83dfb34485f587571
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-09 19:52:41 +0530

    GeeksForGeeks

--- src/GeeksForGeeks/Arrays/SlidingWindowMaximun.java
+++ src/GeeksForGeeks/Arrays/SlidingWindowMaximun.java
@@ -5,6 +5,9 @@
  */
 package GeeksForGeeks.Arrays;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  *
  * @author srivemul
@@ -23,13 +26,35 @@ public class SlidingWindowMaximun {
                     max = arr[i + j];
                 }
             }
-            System.out.println(max);
+            System.out.print(max + " ");
         }
+        System.out.println();
+    }
+    private static void slidingWindowMaximum2(int[] arr,int k) {
+        int n = arr.length;
+        Deque<Integer> deque = new ArrayDeque<>();
+        int i = 0;
+        for(; i < k; i++) {
+           while(!deque.isEmpty() && arr[i] >= arr[deque.getLast()])
+               deque.pollLast();
+           deque.offerLast(i);
+        }
+        for(; i < n; i++) {
+            System.out.print(arr[deque.getFirst()] + " ");
+            while(!deque.isEmpty() && deque.getFirst() <= i - k)
+                deque.pollFirst();
+            while(!deque.isEmpty() && arr[i] >= arr[deque.getLast()])
+                deque.pollLast();
+            deque.offerLast(i);
+        }
+          System.out.print(arr[deque.getFirst()] + " ");
+        System.out.println();
     }
 
     public static void main(String[] args) {
        int[] arr = {8, 5, 10, 7, 9, 4, 15, 12, 90, 13};
        slidingWindowMaximum(arr,4);
+        slidingWindowMaximum2(arr,4);
     }
 
 }
