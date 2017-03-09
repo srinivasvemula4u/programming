04e5720a13415c4cd7bb45d5d55ee86a54259407
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-09 17:55:54 +0530

    GeeksForGeeks

--- src/GeeksForGeeks/Arrays/MaxIndexDifferenceInArrayRightElementGreaterThanLeft.java
+++ src/GeeksForGeeks/Arrays/MaxIndexDifferenceInArrayRightElementGreaterThanLeft.java
@@ -25,10 +25,34 @@ public class MaxIndexDifferenceInArrayRightElementGreaterThanLeft {
         }
         return maxDiff;
     }
+    
+    private static int maxIndexDiffMethod2(int[] arr) {
+        int n = arr.length;
+        int[] minLeft = new int[n];
+        int[] maxRight = new int[n];
+        minLeft[0] = arr[0];
+        for(int i =1; i < n; i++) 
+            minLeft[i] = Math.min(minLeft[i-1], arr[i]);
+        maxRight[n-1] = arr[n-1];
+        for(int i = n-2; i >=0; i--) {
+            maxRight[i] = Math.max(arr[i], maxRight[i+1]);
+        }
+        int maxDiff = Integer.MIN_VALUE;
+        for(int i =0, j = 0; i < n && j < n;) {
+            if(minLeft[i] < maxRight[j]) {
+               maxDiff = Math.max(maxDiff, j-i);
+               j++;
+        }
+            else 
+                i++;
+    }
+       return maxDiff; 
+    }
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arr = {34, 8, 10, 3, 2, 80, 30, 33, 1};
         System.out.println(maxIndexDiff(arr));
+        System.out.println(maxIndexDiffMethod2(arr));
     }
     
 }
