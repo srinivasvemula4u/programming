dafcd39002b199598812c588743cbc790ecf7b06
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-17 21:36:42 +0530

    JavaChange

---
+++ src/FinalMethodTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author srivemul
+ */
+class A {
+    public final void test() {
+        System.out.println("Base");
+    }
+}
+class B extends A {
+
+//    public void test() {
+//        System.out.println("Derviced");
+//    }
+}
+public class FinalMethodTest {
+
+    /**
+     * @param args the command line arguments
+     */
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
