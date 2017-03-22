1139df48f249708939b06d15b8ac184e2e88296d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-22 22:01:26 +0530

    GeeksForGeeks Geometry

--- src/GeeksForGeeks/geometry/Orientation.java
+++ src/GeeksForGeeks/geometry/Orientation.java
@@ -8,38 +8,51 @@ package GeeksForGeeks.geometry;
 /**
  *
  * @author srivemul
- * 
-
-Orientation of an ordered triplet of points in the plane can be
-counterclockwise
-clockwise
-colinear
-* If orientation of (p1, p2, p3) is collinear, then orientation of (p3, p2, p1) is also collinear.
-If orientation of (p1, p2, p3) is clockwise, then orientation of (p3, p2, p1) is counterclockwise and 
-* vice versa is also true.
-* Slope of line segment (p1, p2): σ = (y2 - y1)/(x2 - x1)
-Slope of line segment (p2, p3): τ = (y3 - y2)/(x3 - x2)
-
-If  σ < τ, the orientation is counterclockwise (left turn)
-If  σ = τ, the orientation is collinear
-If  σ > τ, the orientation is clockwise (right turn)
-
-Using above values of σ and τ, we can conclude that, 
-the orientation depends on sign of  below expression: 
-
-(y2 - y1)*(x3 - x2) - (y3 - y2)*(x2 - x1)
-
-Above expression is negative when σ < τ, i.e., counterclockwise
-Above expression is 0 when σ = τ, i.e., collinear
-Above expression is positive when σ > τ, i.e., clockwise
+ *
+ *
+ * Orientation of an ordered triplet of points in the plane can be
+ * counterclockwise clockwise colinear If orientation of (p1, p2, p3) is
+ * collinear, then orientation of (p3, p2, p1) is also collinear. If orientation
+ * of (p1, p2, p3) is clockwise, then orientation of (p3, p2, p1) is
+ * counterclockwise and vice versa is also true. Slope of line segment (p1, p2):
+ * σ = (y2 - y1)/(x2 - x1) Slope of line segment (p2, p3): τ = (y3 - y2)/(x3 -
+ * x2)
+ *
+ * If σ < τ, the orientation is counterclockwise (left turn)
+ * If  σ = τ, the orientation is collinear
+ * If  σ > τ, the orientation is clockwise (right turn)
+ *
+ * Using above values of σ and τ, we can conclude that, the orientation depends
+ * on sign of below expression:  *
+ * (y2 - y1)*(x3 - x2) - (y3 - y2)*(x2 - x1)
+ *
+ * Above expression is negative when σ < τ, i.e., counterclockwise
+ * Above expression is 0 when σ = τ, i.e., collinear
+ * Above expression is positive when σ > τ, i.e., clockwise
  */
+class Point {
+
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Orientation {
 
     /**
      * @param args the command line arguments
      */
+    
+    public static void findOrientation(Point p1, Point p2,Point p3) {
+        //int val = 
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+
     }
-    
+
 }
