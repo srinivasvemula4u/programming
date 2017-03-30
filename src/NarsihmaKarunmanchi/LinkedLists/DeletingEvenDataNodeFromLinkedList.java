3b277cde1e299c68ad64ed890d78c10b69e33133
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-03-30 20:05:42 +0530

    GeeksForGeeks

--- src/NarsihmaKarunmanchi/LinkedLists/DeletingEvenDataNodeFromLinkedList.java
+++ src/NarsihmaKarunmanchi/LinkedLists/DeletingEvenDataNodeFromLinkedList.java
@@ -14,8 +14,32 @@ public class DeletingEvenDataNodeFromLinkedList {
     /**
      * @param args the command line arguments
      */
+    static LinkedListNode deleteEven(LinkedListNode list) {
+        LinkedListNode current = list, prev = null, head = null;
+        if (list == null) {
+            return null;
+        }
+        while (current != null) {
+            if (current.getData() % 2 == 0) {
+                current = current.getNext();
+
+            } else {
+                if (prev == null) {
+                    prev = current;
+                    head = prev;
+                } else {
+                    prev.setNext(current);
+                }
+                prev = current;
+                current = current.getNext();
+            }
+        }
+        prev.setNext(null);
+        return head;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
