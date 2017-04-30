e85d0d9c578643aa61cc063c99105fec13ea8401
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-04-30 19:55:18 +0530

    Leetcode

--- src/LeetCode/Problems/SumOfTwoNumbers.java
+++ src/LeetCode/Problems/SumOfTwoNumbers.java
@@ -9,6 +9,12 @@ package LeetCode.Problems;
  *
  * @author srivemul
  */
+/*
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example:
+Given a = 1 and b = 2, return 3.
+ */
 public class SumOfTwoNumbers {
 
     /**
@@ -17,5 +23,9 @@ public class SumOfTwoNumbers {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public static int getSum(int a, int b) {
+        return b == 0 ? a : getSum(a ^ b, (a & b) << 1);
+    }
+
 }
