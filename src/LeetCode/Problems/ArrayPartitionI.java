b059daafbd745d54179d9c6dd03921315194162d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-04-30 19:18:54 +0530

    Leetcode

---
+++ src/LeetCode/Problems/ArrayPartitionI.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), 
+(a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+
+Example 1:
+Input: [1,4,3,2]
+
+Output: 4
+Explanation: n is 2, and the maximum sum of pairs is 4.
+Note:
+n is a positive integer, which is in the range of [1, 10000].
+All the integers in the array will be in the range of [-10000, 10000].
+*/
+public class ArrayPartitionI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
