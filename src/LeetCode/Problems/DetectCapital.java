990b5a7e9b0bb55717ec0382123c0e07a30876db
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-01 21:19:39 +0530

    Leetcode

--- src/LeetCode/Problems/DetectCapital.java
+++ src/LeetCode/Problems/DetectCapital.java
@@ -27,5 +27,13 @@ public class DetectCapital {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    public boolean detectCapitalUse(String word) {
+        int cnt = 0;
+        for(char c: word.toCharArray()) if('Z' - c >= 0) cnt++;
+        return ((cnt==0 || cnt==word.length()) || (cnt==1 && 'Z' - word.charAt(0)>=0));
+    }
+    public boolean detectCapitalUse1(String word) {
+    return word.matches("[A-Z]+|[a-z]+|[A-Z][a-z]+");
+}
     
 }
