5da23255aa656db0e6c1a352bf8147e21c9cec77
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-01 21:26:29 +0530

    Leetcode

---
+++ src/LeetCode/Problems/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+*/
+public class FindAllNumbersDisappearedInAnArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
