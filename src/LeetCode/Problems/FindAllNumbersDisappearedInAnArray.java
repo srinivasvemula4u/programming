a85c1e2bc868fa186c366f280f7e5ddcce63677a
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-02 19:43:29 +0530

    Leetcode

--- src/LeetCode/Problems/FindAllNumbersDisappearedInAnArray.java
+++ src/LeetCode/Problems/FindAllNumbersDisappearedInAnArray.java
@@ -59,4 +59,18 @@ public class FindAllNumbersDisappearedInAnArray {
         return list;
 
     }
+
+    public List<Integer> findDisappearedNumbers2(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            nums[(nums[i] - 1) % n] += n;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= n) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+    }
 }
