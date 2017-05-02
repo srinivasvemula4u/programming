147b225ca702714012308ebd0ce059853ff40ad4
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-02 19:39:47 +0530

    Leetcode

--- src/LeetCode/Problems/FindAllNumbersDisappearedInAnArray.java
+++ src/LeetCode/Problems/FindAllNumbersDisappearedInAnArray.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author srivemul
@@ -23,7 +26,14 @@ Input:
 
 Output:
 [5,6]
-*/
+ */
+ /*
+Solution Hint : 
+The basic idea is that we iterate through the input array and mark elements as negative using nums[nums[i] -1] = -nums[nums[i]-1]. 
+In this way all the numbers that we have seen will be marked as negative. In the second iteration, 
+if a value is not marked as negative, 
+it implies we have never seen that index before, so just add it to the return list.
+ */
 public class FindAllNumbersDisappearedInAnArray {
 
     /**
@@ -32,5 +42,21 @@ public class FindAllNumbersDisappearedInAnArray {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+            if (nums[val] > 0) {
+                nums[val] = -nums[val];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
+
+    }
 }
