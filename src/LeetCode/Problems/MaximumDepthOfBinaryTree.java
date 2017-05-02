83844434ff5145947769531039a9169cb67ee43f
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-02 19:50:01 +0530

    Leetcode

--- src/LeetCode/Problems/MaximumDepthOfBinaryTree.java
+++ src/LeetCode/Problems/MaximumDepthOfBinaryTree.java
@@ -9,6 +9,11 @@ package LeetCode.Problems;
  *
  * @author srivemul
  */
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ */
 public class MaximumDepthOfBinaryTree {
 
     /**
@@ -17,5 +22,12 @@ public class MaximumDepthOfBinaryTree {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
 }
---
+++ src/LeetCode/Problems/TreeNode.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
