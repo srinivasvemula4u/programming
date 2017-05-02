ed10633a64cfa989fb1e9d55095667f8804931e0
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-02 20:23:39 +0530

    Leetcode

--- src/LeetCode/Problems/FindTheDifference.java
+++ src/LeetCode/Problems/FindTheDifference.java
@@ -62,4 +62,18 @@ public class FindTheDifference {
         return ' ';
     }
 
+    public char findTheDifference2(String s, String t) {
+        // Initialize variables to store sum of ASCII codes for 
+        // each string
+        int charCodeS = 0, charCodeT = 0;
+        // Iterate through both strings and char codes
+        for (int i = 0; i < s.length(); ++i) {
+            charCodeS += (int) s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            charCodeT += (int) t.charAt(i);
+        }
+        // Return the difference between 2 strings as char
+        return (char) (charCodeT - charCodeS);
+    }
 }
