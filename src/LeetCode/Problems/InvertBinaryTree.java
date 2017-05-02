df8f2c3c66ced53643ac094a1f8ccae012db5fb2
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-02 20:37:00 +0530

    Leetcode

---
+++ src/LeetCode/Problems/InvertBinaryTree.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+public class InvertBinaryTree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
