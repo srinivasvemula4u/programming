ef2a98ba382873f54dff889c89d5a66f0872b284
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-03 08:14:18 +0530

    Leetcode

--- src/LeetCode/Problems/BinaryTreeTilt.java
+++ src/LeetCode/Problems/BinaryTreeTilt.java
@@ -40,5 +40,20 @@ public class BinaryTreeTilt {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    int tilt = 0;
 
+    public int findTilt(TreeNode root) {
+        traverse(root);
+        return tilt;
+    }
+
+    public int traverse(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = traverse(root.left);
+        int right = traverse(root.right);
+        tilt += Math.abs(left - right);
+        return left + right + root.val;
+    }
 }
