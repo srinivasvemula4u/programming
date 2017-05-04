b15bcdc946b7ef2a2a8dc92a2f897239d49e178d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-04 08:11:04 +0530

    Leetcode

--- src/LeetCode/Problems/AssignCookies.java
+++ src/LeetCode/Problems/AssignCookies.java
@@ -60,4 +60,16 @@ public class AssignCookies {
         return count;
     }
 
+    public int findContentChildren2(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        for (int j = 0; i < g.length && j < s.length; j++) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+        }
+        return i;
+    }
+
 }
