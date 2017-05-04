7b3a521b2672e69a3582ec79c7ef0c883aef9de0
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-04 08:19:55 +0530

    Leetcode

---
+++ src/LeetCode/Problems/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ */
+public class MinimumDepthOfBinaryTree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+        int depth = 1;
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            if (temp != null) {
+                if (temp.left == null && temp.right == null) {
+                    return depth;
+                }
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+            } else {
+                depth++;
+                if (!queue.isEmpty()) {
+                    queue.offer(null);
+                }
+            }
+
+        }
+        return 0;
+    }
+
+}
