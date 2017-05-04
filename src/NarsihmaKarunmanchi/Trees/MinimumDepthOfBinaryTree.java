e9cef6561563dbddcd9b1a08b718a33854432677
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-04 08:28:36 +0530

    Leetcode

--- src/NarsihmaKarunmanchi/Trees/MinimumDepthOfBinaryTree.java
+++ src/NarsihmaKarunmanchi/Trees/MinimumDepthOfBinaryTree.java
@@ -97,18 +97,7 @@ public class MinimumDepthOfBinaryTree {
       BinaryTree tree = new BinaryTree();
         tree.insert(1);
         tree.insert(2);
-        tree.insert(3);
-        tree.insert(4);
-        tree.insert(5);
-        tree.insert(6);
-        tree.insert(7);
-        tree.insert(8);
-        tree.insert(9);
-        tree.insert(10);
-        tree.insert(11);
-        tree.insert(12);
-        tree.insert(13);
-        tree.insert(14);
+        
       //  tree.insert(15);
         System.out.println("Height of the tree is " + minimumDepthOfBinaryTreeIterative(tree.getRoot()));
         System.out.println("Height of the tree is " + minimumDepthOfBinaryTreeRecursive(tree.getRoot()));
