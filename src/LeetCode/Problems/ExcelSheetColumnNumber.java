c6c6c4e74ac0956fca2e2ba52c4a16057fd79ebe
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 20:29:00 +0530

    Leetcode

---
+++ src/LeetCode/Problems/ExcelSheetColumnNumber.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+*/
+public class ExcelSheetColumnNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
