2390dec6ee0e43d3ff4738a9304cf76919dc7d88
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 07:17:04 +0530

    Leetcode

--- src/LeetCode/Problems/MinimumMovesToEqualArrayElements.java
+++ src/LeetCode/Problems/MinimumMovesToEqualArrayElements.java
@@ -54,4 +54,16 @@ public class MinimumMovesToEqualArrayElements {
         // TODO code application logic here
     }
 
+    public int minMoves(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            res = res + (nums[i] - min);
+        }
+        return res;
+    }
+
 }
