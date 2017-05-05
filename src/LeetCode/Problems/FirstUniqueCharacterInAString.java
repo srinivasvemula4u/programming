9b11d0e08247253789807239341ec8192dde0ff7
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 19:13:08 +0530

    Leetcode

---
+++ src/LeetCode/Problems/FirstUniqueCharacterInAString.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+ */
+public class FirstUniqueCharacterInAString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
