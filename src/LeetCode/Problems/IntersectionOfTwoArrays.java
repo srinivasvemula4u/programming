4e485d3da2016feb944253cf5bee737114b1a3ed
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 08:01:22 +0530

    Leetcode

--- src/LeetCode/Problems/IntersectionOfTwoArrays.java
+++ src/LeetCode/Problems/IntersectionOfTwoArrays.java
@@ -5,6 +5,10 @@
  */
 package LeetCode.Problems;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author srivemul
@@ -26,6 +30,35 @@ public class IntersectionOfTwoArrays {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int m = nums1.length;
+        int n = nums2.length;
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else if (list.contains(nums1[i])) {
+                i++;
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] res = new int[list.size()];
+        i = 0;
+        for (int val : list) {
+            res[i++] = val;
+        }
+        return res;
     }
 
 }
