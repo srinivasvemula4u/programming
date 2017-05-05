9ebe0084b65d0e977d2f80f97bad4695e6486982
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 07:13:04 +0530

    Leetcode

--- src/LeetCode/Problems/MinimumMovesToEqualArrayElements.java
+++ src/LeetCode/Problems/MinimumMovesToEqualArrayElements.java
@@ -25,7 +25,26 @@ Explanation:
 Only three moves are needed (remember each move increments two elements):
 
 [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
-*/
+ */
+ /*
+******************************************************************************************************************
+Solution Hint : 
+It is a math question
+let's define sum as the sum of all the numbers, before any moves; minNum as the min number int the list; 
+n is the length of the list;
+
+After, say m moves, we get all the numbers as x , and we will get the following equation
+
+ sum + m * (n - 1) = x * n
+and actually,
+
+  x = minNum + m
+and finally, we will get
+
+  sum - minNum * n = m
+So, it is clear and easy now.
+
+ */
 public class MinimumMovesToEqualArrayElements {
 
     /**
@@ -34,5 +53,5 @@ public class MinimumMovesToEqualArrayElements {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
