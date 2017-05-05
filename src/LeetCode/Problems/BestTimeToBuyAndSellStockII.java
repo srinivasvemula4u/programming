15da21155ac8a01355b1d1657c8228af3f79a812
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 19:54:13 +0530

    Leetcode

---
+++ src/LeetCode/Problems/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like 
+(ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at 
+the same time 
+(ie, you must sell the stock before you buy again).
+*/
+public class BestTimeToBuyAndSellStockII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+}
