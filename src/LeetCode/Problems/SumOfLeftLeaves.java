f6cac9715c3fe5e56bf0c5728ef4bba8f6ec5a6b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 18:50:20 +0530

    Leetcode

--- src/LeetCode/Problems/SumOfLeftLeaves.java
+++ src/LeetCode/Problems/SumOfLeftLeaves.java
@@ -30,18 +30,30 @@ public class SumOfLeftLeaves {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
+    /*
+    Recursive method. For given node we check whether its left child is a leaf. If it is the case,
+    we add its value to answer, otherwise recursively call method on left child. For right child we call method 
+    only if it has at least one nonnull child.
+     */
     public int sumOfLeftLeaves(TreeNode root) {
-        if(root == null) 
-          return 0;
-        int ans = 0;
-        if(root.left != null) {
-            if( root.left.left == null && root.left.right == null) 
-               ans = ans + root.left.val; 
-            else 
-               ans = ans + sumOfLeftLeaves(root.left);
+        if (root == null) {
+            return 0;
         }
-         ans = ans + sumOfLeftLeaves(root.right);
+        int ans = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                ans = ans + root.left.val;
+            } else {
+                ans = ans + sumOfLeftLeaves(root.left);
+            }
+        }
+        ans = ans + sumOfLeftLeaves(root.right);
         return ans;
     }
+    /*
+    Iterative method. Here for each node in the tree we check whether its left child is a leaf. If it is true, we add its value to answer, otherwise add left child to the stack to process it later. 
+    For right child we add it to stack only if it is not a leaf.
+     */
 
 }
