397d436e253572dd22379409e224405cb60b43c0
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-05 19:57:51 +0530

    Leetcode

--- src/LeetCode/Problems/BestTimeToBuyAndSellStockII.java
+++ src/LeetCode/Problems/BestTimeToBuyAndSellStockII.java
@@ -16,7 +16,7 @@ Design an algorithm to find the maximum profit. You may complete as many transac
 (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at 
 the same time 
 (ie, you must sell the stock before you buy again).
-*/
+ */
 public class BestTimeToBuyAndSellStockII {
 
     /**
@@ -25,6 +25,41 @@ public class BestTimeToBuyAndSellStockII {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
-    
+
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i]) {
+                total += prices[i + 1] - prices[i];
+            }
+        }
+
+        return total;
+    }
+
+    /*
+    Greedy Way: 
+    The greedy pair-wise approach mentioned in other posts is great for this problem indeed, 
+    but if we're not allowed to buy and sell stocks within the same day it can't be applied (
+    logically, of course; the answer will be the same). Actually, the straight-forward way of finding next 
+    local minimum and next local maximum is not much more complicated, 
+    so, just for the sake of having an alternative I share the code in Java for such case.
+     */
+    public int maxProfitGreedy(int[] prices) {
+        int profit = 0, i = 0;
+        while (i < prices.length) {
+            // find next local minimum
+            while (i < prices.length - 1 && prices[i + 1] <= prices[i]) {
+                i++;
+            }
+            int min = prices[i++]; // need increment to avoid infinite loop for "[1]"
+            // find next local maximum
+            while (i < prices.length - 1 && prices[i + 1] >= prices[i]) {
+                i++;
+            }
+            profit += i < prices.length ? prices[i++] - min : 0;
+        }
+        return profit;
+    }
+
 }
