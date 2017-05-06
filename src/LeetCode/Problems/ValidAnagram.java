af445ca49245c3da2c15656b94c67249e19dce97
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-06 11:40:42 +0530

    Leetcode

--- src/LeetCode/Problems/ValidAnagram.java
+++ src/LeetCode/Problems/ValidAnagram.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author srivemul
@@ -29,6 +32,26 @@ public class ValidAnagram {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+          return false;
+         Map<Character,Integer> map = new HashMap<>();
+         for(int i=0; i < s.length(); i++) {
+             if(map.containsKey(s.charAt(i))) {
+                 map.put(s.charAt(i),map.get(s.charAt(i))+1);
+             } else {
+                 map.put(s.charAt(i),1);
+             }
+         }
+          for(int i=0; i < t.length(); i++) {
+             if(map.containsKey(t.charAt(i)) &&  map.get(t.charAt(i)) != 0) {
+                 map.put(t.charAt(i),map.get(t.charAt(i))-1);
+             } else {
+                 return false;
+             }
+         }
+         return true;
     }
     
 }
