554d4acaa6f92ba710a6597f20a904cedb3d5d9f
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-06 11:07:53 +0530

    Leetcode

---
+++ src/LeetCode/Problems/SameTree.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+*/
+public class SameTree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
