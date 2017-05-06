257650367f413ab2ad8fb3ff254721864eb28cc1
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-06 11:08:26 +0530

    Leetcode

--- src/LeetCode/Problems/SameTree.java
+++ src/LeetCode/Problems/SameTree.java
@@ -13,7 +13,7 @@ package LeetCode.Problems;
 Given two binary trees, write a function to check if they are equal or not.
 
 Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
-*/
+ */
 public class SameTree {
 
     /**
@@ -22,5 +22,15 @@ public class SameTree {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return (p.val == q.val) && (isSameTree(p.left, q.left)) && (isSameTree(p.right, q.right));
+    }
+
 }
