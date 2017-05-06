495f782a6ac3e10a7470edfb847ec80815413ea1
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-06 11:04:12 +0530

    Leetcode

---
+++ src/LeetCode/Problems/DeleteNodeInALinkedList.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, t
+he linked list should become 1 -> 2 -> 4 after calling your function.
+*/
+public class DeleteNodeInALinkedList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
