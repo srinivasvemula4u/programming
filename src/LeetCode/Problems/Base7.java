28781c9caa40ae5d334187c3d6c44371632b0aec
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-06 11:59:06 +0530

    Leetcode

--- src/LeetCode/Problems/Base7.java
+++ src/LeetCode/Problems/Base7.java
@@ -49,4 +49,14 @@ public class Base7 {
         return sb.toString();
     }
 
+    public String convertTo7(int num) {
+        if (num < 0) {
+            return '-' + convertTo7(-num);
+        }
+        if (num < 7) {
+            return num + "";
+        }
+        return convertTo7(num / 7) + num % 7;
+    }
+
 }
