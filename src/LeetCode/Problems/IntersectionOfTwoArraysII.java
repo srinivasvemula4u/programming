528532d4e162d54681a9266cf7291e8c2fafd031
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-07 11:14:30 +0530

    Leetcode

--- src/LeetCode/Problems/IntersectionOfTwoArraysII.java
+++ src/LeetCode/Problems/IntersectionOfTwoArraysII.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author srivemul
@@ -31,6 +34,32 @@ public class IntersectionOfTwoArraysII {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
+            }
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+                result.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+
+        int[] r = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            r[i] = result.get(i);
+        }
+
+        return r;
     }
 
 }
