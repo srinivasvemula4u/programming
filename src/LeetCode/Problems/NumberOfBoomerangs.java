ce6f9978d8ffb1d1b4dbb5b147221a409b38d5fb
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-07 20:48:23 +0530

    Leetcode

--- src/LeetCode/Problems/NumberOfBoomerangs.java
+++ src/LeetCode/Problems/NumberOfBoomerangs.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author srivemul
@@ -23,7 +26,7 @@ Output:
 
 Explanation:
 The two boomerangs are [[1,0],[0,0],[2,0]] and [[1,0],[2,0],[0,0]]
-*/
+ */
 public class NumberOfBoomerangs {
 
     /**
@@ -32,5 +35,35 @@ public class NumberOfBoomerangs {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public int numberOfBoomerangs(int[][] points) {
+        int res = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < points.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                int d = getDistance(points[i], points[j]);
+                map.put(d, map.getOrDefault(d, 0) + 1);
+            }
+
+            for (int val : map.values()) {
+                res += val * (val - 1);
+            }
+            map.clear();
+        }
+
+        return res;
+    }
+
+    private int getDistance(int[] a, int[] b) {
+        int dx = a[0] - b[0];
+        int dy = a[1] - b[1];
+
+        return dx * dx + dy * dy;
+    }
+
 }
