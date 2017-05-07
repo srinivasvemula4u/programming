9d531ed85fa8351facc74ad1fb95e21baac6c7f7
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-07 11:26:41 +0530

    Leetcode

---
+++ src/LeetCode/Problems/StudentAttendanceRecordI.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+You are given a string representing an attendance record for a student. 
+The record only contains the following three characters:
+
+'A' : Absent.
+'L' : Late.
+'P' : Present.
+A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+You need to return whether the student could be rewarded according to his attendance record.
+
+Example 1:
+Input: "PPALLP"
+Output: True
+Example 2:
+Input: "PPALLL"
+Output: False
+*/
+public class StudentAttendanceRecordI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
