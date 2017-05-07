81a9463e8235634ad2eee1d3caa0a7054a066c00
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-07 22:06:38 +0530

    Leetcode

--- src/LeetCode/Problems/SubtreeOfAnotherTree.java
+++ src/LeetCode/Problems/SubtreeOfAnotherTree.java
@@ -90,4 +90,29 @@ public class SubtreeOfAnotherTree {
         return (s.val == q.val) && (findSameTree(s.left, q.left)) && (findSameTree(s.right, q.right));
     }
 
+    public boolean isSubtree2(TreeNode s, TreeNode t) {
+        if (s == null || t == null) {
+            return false;
+        }
+        if (isSame(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+
+        if (s.val != t.val) {
+            return false;
+        }
+
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+
 }
