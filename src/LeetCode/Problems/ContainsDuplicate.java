758c8f7ad06e7846057657304df04e6669cb2942
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-07 10:26:20 +0530

    Leetcode

---
+++ src/LeetCode/Problems/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given an array of integers, find if the array contains any duplicates. Your function should return 
+true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
+*/
+public class ContainsDuplicate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
