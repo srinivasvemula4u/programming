4c8656817010b239233332d6afc8a9d86e9305c9
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-07 10:43:31 +0530

    Leetcode

--- src/LeetCode/Problems/RomanToInteger.java
+++ src/LeetCode/Problems/RomanToInteger.java
@@ -13,7 +13,7 @@ package LeetCode.Problems;
 Given a roman numeral, convert it to an integer.
 
 Input is guaranteed to be within the range from 1 to 3999.
-*/
+ */
 public class RomanToInteger {
 
     /**
@@ -22,5 +22,43 @@ public class RomanToInteger {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public int romanToInt(String s) {
+        int nums[] = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'M':
+                    nums[i] = 1000;
+                    break;
+                case 'D':
+                    nums[i] = 500;
+                    break;
+                case 'C':
+                    nums[i] = 100;
+                    break;
+                case 'L':
+                    nums[i] = 50;
+                    break;
+                case 'X':
+                    nums[i] = 10;
+                    break;
+                case 'V':
+                    nums[i] = 5;
+                    break;
+                case 'I':
+                    nums[i] = 1;
+                    break;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] < nums[i + 1]) {
+                sum -= nums[i];
+            } else {
+                sum += nums[i];
+            }
+        }
+        return sum + nums[nums.length - 1];
+    }
+
 }
