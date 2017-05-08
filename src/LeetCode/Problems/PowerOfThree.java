ecc0bee30ac6ff05a4c1ba3900fcec5bf70f8202
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-08 20:45:28 +0530

    Leet code

---
+++ src/LeetCode/Problems/PowerOfThree.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given an integer, write a function to determine if it is a power of three.
+
+Follow up:
+Could you do it without using any loop / recursion?
+ */
+public class PowerOfThree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
