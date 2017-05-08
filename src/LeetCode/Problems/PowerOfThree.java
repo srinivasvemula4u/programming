5ea71ca247fffdec7f009f790cf04a9bbcc699f4
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-08 20:45:56 +0530

    Leet code

--- src/LeetCode/Problems/PowerOfThree.java
+++ src/LeetCode/Problems/PowerOfThree.java
@@ -24,4 +24,9 @@ public class PowerOfThree {
         // TODO code application logic here
     }
 
+    public boolean isPowerOfThree(int n) {
+        // 1162261467 is 3^19,  3^20 is bigger than int  
+        return (n > 0 && 1162261467 % n == 0);
+    }
+
 }
