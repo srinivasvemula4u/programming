218c83d6306928bee51180f79f84df3a070f86cb
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-08 21:16:33 +0530

    Leet code

---
+++ src/LeetCode/Problems/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+*/
+public class RemoveDuplicatesFromSortedList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
