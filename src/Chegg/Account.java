ed3734869a44c106b54358607e67ac43eece5e7b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-08 23:24:06 +0530

    Leet code

---
+++ src/Chegg/Account.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chegg;
+
+/**
+ *
+ * @author srivemul
+ */
+public class Account {
+    private String type;
+    private String number;
+    private Double balance;
+    public Account() {
+        
+    }
+    public String getType() {
+        return this.type;
+    }
+    public void setType(String pTyp) {
+        this.type = pTyp;
+    }
+    public String getNumber() {
+        return this.number;
+    }
+    public void setNumber(String pNbr) {
+        this.number = pNbr;
+    }
+    public Double getBalance() {
+        return this.balance;
+    }
+    public void setBalance(Double pBal) {
+        this.balance = pBal;
+    }
+    public String toString() {
+        return "Account type is" + getType() + ",\n its number is "+ getNumber()
+                + "\n and  its  balance is "+ getBalance();
+    }
+}
---
+++ src/Chegg/Business.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chegg;
+
+/**
+ *
+ * @author srivemul
+ */
+public class Business {
+    private String name;
+    private String type;
+    private Account acct;
+    public Business() {
+        
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String pName) {
+        this.name = pName;
+    }
+    public String getType() {
+        return this.type;
+    } 
+    public void setType(String pType) {
+        this.type = pType;
+    }
+    public Account getAcct() {
+        return this.acct;
+    }
+    public void setAcct(Account pAcct) {
+        this.acct = pAcct;
+    }
+    public String toString() {
+        return "Business name is " + getName() + ", its type is "+ getType() + " and its account details are "
+                + getAcct().toString();
+    }
+}
