9760591e51923c468540331d55a98081781f2331
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-08 21:53:55 +0530

    Leet code

--- src/LeetCode/Problems/SearchInsertPosition.java
+++ src/LeetCode/Problems/SearchInsertPosition.java
@@ -19,7 +19,7 @@ Here are few examples.
 [1,3,5,6], 2 → 1
 [1,3,5,6], 7 → 4
 [1,3,5,6], 0 → 0
-*/
+ */
 public class SearchInsertPosition {
 
     /**
@@ -28,5 +28,20 @@ public class SearchInsertPosition {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public int searchInsert(int[] A, int target) {
+        int low = 0, high = A.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (A[mid] == target) {
+                return mid;
+            } else if (A[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
 }
