b0bf0fd613baebce0136f0b5203b974f0f5219b9
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-08 08:45:35 +0530

    Leetcode

---
+++ src/LeetCode/Problems/ConvertANumberToHexadecimal.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*Given an integer, write an algorithm to convert it to hexadecimal. For negative integer, two’s complement method is used.
+
+Note:
+
+All letters in hexadecimal (a-f) must be in lowercase.
+The hexadecimal string must not contain extra leading 0s. If the number is zero, 
+it is represented by a single zero character '0'; otherwise, the first character in the hexadecimal 
+string will not be the zero character.
+The given number is guaranteed to fit within the range of a 32-bit signed integer.
+You must not use any method provided by the library which converts/formats the number to hex directly.
+Example 1:
+
+Input:
+26
+
+Output:
+"1a"
+Example 2:
+
+Input:
+-1
+
+Output:
+"ffffffff"
+Subscribe to see which companies asked this question.
+
+Show Tags*/
+public class ConvertANumberToHexadecimal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
