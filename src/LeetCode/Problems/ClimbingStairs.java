7f9db7e32678001c73d5a80ac7d507cbdb267d91
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-08 22:05:04 +0530

    Leet code

---
+++ src/LeetCode/Problems/ClimbingStairs.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+ou are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+*/
+public class ClimbingStairs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
