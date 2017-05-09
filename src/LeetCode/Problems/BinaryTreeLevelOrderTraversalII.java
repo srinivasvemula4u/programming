0b7aae0a7c620087de4a118dc0efa25652d13a8a
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-09 20:07:16 +0530

    Leet code

--- src/LeetCode/Problems/BinaryTreeLevelOrderTraversalII.java
+++ src/LeetCode/Problems/BinaryTreeLevelOrderTraversalII.java
@@ -80,24 +80,49 @@ public class BinaryTreeLevelOrderTraversalII {
         return res;
 
     }
-     public List<List<Integer>> levelOrderBottom2(TreeNode root) {
+
+    public List<List<Integer>> levelOrderBottom2(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
-        
-        if(root == null) return wrapList;
-        
+
+        if (root == null) {
+            return wrapList;
+        }
+
         queue.offer(root);
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int levelNum = queue.size();
             List<Integer> subList = new LinkedList<Integer>();
-            for(int i=0; i<levelNum; i++) {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
                 subList.add(queue.poll().val);
             }
             wrapList.add(0, subList);
         }
         return wrapList;
+    }
+
+    public List<List<Integer>> levelOrderBottom3(TreeNode root) {
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        levelMaker(wrapList, root, 0);
+        return wrapList;
+    }
+
+    public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
+        if (root == null) {
+            return;
+        }
+        if (level >= list.size()) {
+            list.add(0, new LinkedList<Integer>());
+        }
+        levelMaker(list, root.left, level + 1);
+        levelMaker(list, root.right, level + 1);
+        list.get(list.size() - level - 1).add(root.val);
     }
 
 }
