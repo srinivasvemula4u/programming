db3d1181456b6080d8a7b0f0bdc09fc363ddd32e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-09 19:27:32 +0530

    Leet code

--- src/LeetCode/Problems/PathSum.java
+++ src/LeetCode/Problems/PathSum.java
@@ -23,7 +23,7 @@ Given the below binary tree and sum = 22,
          /  \      \
         7    2      1
 return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
-*/
+ */
 public class PathSum {
 
     /**
@@ -32,5 +32,16 @@ public class PathSum {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && root.val == sum) {
+            return true;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+
+    }
+
 }
