8fb4fbc0a1d65af6f68ae242e684188f67099bf6
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-09 20:24:20 +0530

    Leet code

--- src/LeetCode/Problems/MergeTwoSortedLists.java
+++ src/LeetCode/Problems/MergeTwoSortedLists.java
@@ -12,7 +12,7 @@ package LeetCode.Problems;
 /*
 Merge two sorted linked lists and return it as a new list. The new list should be made by splicing 
 together the nodes of the first two lists.
-*/
+ */
 public class MergeTwoSortedLists {
 
     /**
@@ -21,5 +21,23 @@ public class MergeTwoSortedLists {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode head = null;
+        if (l1.val <= l2.val) {
+            head = l1;
+            head.next = mergeTwoLists(l1.next, l2);
+        } else {
+            head = l2;
+            head.next = mergeTwoLists(l1, l2.next);
+        }
+        return head;
+    }
+
 }
