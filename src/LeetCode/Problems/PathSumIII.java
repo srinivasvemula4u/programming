a22e0be77235cb8332b2d46f7bd3b9f83505178b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-09 18:52:54 +0530

    Leet code

--- src/LeetCode/Problems/PathSumIII.java
+++ src/LeetCode/Problems/PathSumIII.java
@@ -41,7 +41,7 @@ Subscribe to see which companies asked this question.
 Show Tags
 Show Similar Problems
 
-*/
+ */
 public class PathSumIII {
 
     /**
@@ -50,5 +50,25 @@ public class PathSumIII {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    /*
+    Typical recursive DFS.
+Space: O(n) due to recursion.
+Time: O(n^2) in worst case (no branching); O(nlogn) in best case (balanced tree).
+     */
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+
+    private int pathSumFrom(TreeNode node, int sum) {
+        if (node == null) {
+            return 0;
+        }
+        return (node.val == sum ? 1 : 0)
+                + pathSumFrom(node.left, sum - node.val) + pathSumFrom(node.right, sum - node.val);
+    }
+
 }
