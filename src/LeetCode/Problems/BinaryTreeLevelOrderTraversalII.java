de9f986402ca220eee7495b49122ad2ad78ca8ef
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-09 20:00:16 +0530

    Leet code

--- src/LeetCode/Problems/BinaryTreeLevelOrderTraversalII.java
+++ src/LeetCode/Problems/BinaryTreeLevelOrderTraversalII.java
@@ -5,6 +5,8 @@
  */
 package LeetCode.Problems;
 
+import java.util.*;
+
 /**
  *
  * @author srivemul
@@ -26,7 +28,7 @@ return its bottom-up level order traversal as:
   [9,20],
   [3]
 ]
-*/
+ */
 public class BinaryTreeLevelOrderTraversalII {
 
     /**
@@ -35,5 +37,48 @@ public class BinaryTreeLevelOrderTraversalII {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        queue.offer(root);
+        queue.offer(null);
+        while (!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            stack.push(temp);
+            if (temp != null) {
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+
+            } else // stack.push(null);
+             if (!queue.isEmpty()) {
+                    queue.offer(null);
+                }
+
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            if (temp == null) {
+                if (list.size() != 0) {
+                    res.add(list);
+                    list = new ArrayList<>();
+                }
+            } else {
+                list.add(temp.val);
+            }
+        }
+        res.add(list);
+        return res;
+
+    }
+
 }
