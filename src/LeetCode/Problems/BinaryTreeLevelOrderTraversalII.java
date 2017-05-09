b7decdef77939eee70b302007b5cc47ef0015829
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-09 20:02:56 +0530

    Leet code

--- src/LeetCode/Problems/BinaryTreeLevelOrderTraversalII.java
+++ src/LeetCode/Problems/BinaryTreeLevelOrderTraversalII.java
@@ -80,5 +80,24 @@ public class BinaryTreeLevelOrderTraversalII {
         return res;
 
     }
+     public List<List<Integer>> levelOrderBottom2(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        
+        if(root == null) return wrapList;
+        
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(0, subList);
+        }
+        return wrapList;
+    }
 
 }
