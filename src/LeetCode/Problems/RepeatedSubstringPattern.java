ada1547a5fcf56147150d56860c889fb2c1fa64c
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-10 22:56:26 +0530

    Leet code

---
+++ src/LeetCode/Problems/RepeatedSubstringPattern.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given a non-empty string check if it can be constructed by taking a substring of it 
+and appending multiple copies of the substring together. You may assume the given string consists of 
+lowercase English letters only and its length will not exceed 10000.
+
+Example 1:
+Input: "abab"
+
+Output: True
+
+Explanation: It's the substring "ab" twice.
+Example 2:
+Input: "aba"
+
+Output: False
+Example 3:
+Input: "abcabcabcabc"
+
+Output: True
+
+Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+*/
+public class RepeatedSubstringPattern {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
