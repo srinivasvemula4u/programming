65130b1d5a62bd3401427219a9b791bad26adc88
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 08:19:47 +0530

    Leet code

--- src/LeetCode/Problems/RepeatedSubstringPattern.java
+++ src/LeetCode/Problems/RepeatedSubstringPattern.java
@@ -31,6 +31,14 @@ Output: True
 
 Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.).
 */
+/*
+***************************************************************************************
+Solution Hint : 
+The length of the repeating substring must be a divisor of the length of the input string
+Search for all possible divisor of str.length, starting for length/2
+If i is a divisor of length, repeat the substring from 0 to i the number of times i is contained in s.length
+If the repeated substring is equals to the input str return true
+*/
 public class RepeatedSubstringPattern {
 
     /**
