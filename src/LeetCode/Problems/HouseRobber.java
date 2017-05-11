0ba0b3b1932a04db9abb49822bdc135f3368793c
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 10:24:30 +0530

    Leet code

--- src/LeetCode/Problems/HouseRobber.java
+++ src/LeetCode/Problems/HouseRobber.java
@@ -55,5 +55,15 @@ public class HouseRobber {
         }
         return Math.max(prevNo, prevYes);
     }
+    public int rob3(int[] num) {
+    int rob = 0; //max monney can get if rob current house
+    int notrob = 0; //max money can get if not rob current house
+    for(int i=0; i<num.length; i++) {
+        int currob = notrob + num[i]; //if rob current value, previous house must not be robbed
+        notrob = Math.max(notrob, rob); //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
+        rob = currob;
+    }
+    return Math.max(rob, notrob);
+}
 
 }
