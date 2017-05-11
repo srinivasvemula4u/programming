b350b7f19dcb477b7efdf75eb3882e0ac7a2640f
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 22:51:42 +0530

    Leet code

--- src/LeetCode/Problems/NumberOfSegmentsInAString.java
+++ src/LeetCode/Problems/NumberOfSegmentsInAString.java
@@ -18,7 +18,7 @@ Example:
 
 Input: "Hello, my name is John"
 Output: 5
-*/
+ */
 public class NumberOfSegmentsInAString {
 
     /**
@@ -27,5 +27,15 @@ public class NumberOfSegmentsInAString {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public int countSegments(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                res++;
+            }
+        }
+        return res;
+    }
+
 }
