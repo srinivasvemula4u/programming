dde3c44b93c0cec094da52fe5e7003b1aec4f94d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 23:28:17 +0530

    Leet code

--- src/LeetCode/Problems/BalancedBinaryTree.java
+++ src/LeetCode/Problems/BalancedBinaryTree.java
@@ -24,4 +24,18 @@ public class BalancedBinaryTree {
         // TODO code application logic here
     }
 
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return (Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1) && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+    }
+
 }
