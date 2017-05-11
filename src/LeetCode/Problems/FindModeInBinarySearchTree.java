82fa439522595dac7dad1dcb98187013df1f7b75
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 08:57:05 +0530

    Leet code

--- src/LeetCode/Problems/FindModeInBinarySearchTree.java
+++ src/LeetCode/Problems/FindModeInBinarySearchTree.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author srivemul
@@ -31,6 +34,46 @@ public class FindModeInBinarySearchTree {
     /**
      * @param args the command line arguments
      */
+    Integer prev = null;
+    int count = 1;
+    int max = 0;
+
+    public int[] findMode(TreeNode root) {
+        if (root == null) {
+            return new int[0];
+        }
+        List<Integer> list = new ArrayList<>();
+        traverse(root, list);
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+
+    private void traverse(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, list);
+        if (prev != null) {
+            if (prev == root.val) {
+                count++;
+            } else {
+                count = 1;
+            }
+        }
+        if (count > max) {
+            max = count;
+            list.clear();
+            list.add(root.val);
+        } else if (count == max) {
+            list.add(root.val);
+        }
+        prev = root.val;
+        traverse(root.right, list);
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
