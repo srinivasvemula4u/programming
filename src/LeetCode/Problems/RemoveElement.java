48175cfcff208b9801739a722c1d4b5663c9e4f6
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 20:05:45 +0530

    Leet code

--- src/LeetCode/Problems/RemoveElement.java
+++ src/LeetCode/Problems/RemoveElement.java
@@ -20,7 +20,7 @@ Example:
 Given input array nums = [3,2,2,3], val = 3
 
 Your function should return length = 2, with the first two elements of nums being 2.
-*/
+ */
 public class RemoveElement {
 
     /**
@@ -29,5 +29,16 @@ public class RemoveElement {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public int removeElement(int[] nums, int val) {
+        int curIndex = 0, length = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[curIndex++] = nums[i];
+                length++;
+            }
+        }
+        return length;
+    }
+
 }
