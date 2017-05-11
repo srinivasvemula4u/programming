4a0bbc73d960b72111595e0f88c11dc58a291fa8
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 22:07:12 +0530

    Leet code

--- src/LeetCode/Problems/PascalsTriangle.java
+++ src/LeetCode/Problems/PascalsTriangle.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author srivemul
@@ -40,5 +43,22 @@ public class PascalsTriangle {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+     public List<List<Integer>> generate(int numRows) {
+       List<List<Integer>> triangle = new ArrayList<>();
+       if(numRows <=0)
+         return triangle;
+       for(int i =0; i < numRows; i++) {
+           List<Integer> row = new ArrayList<>();
+           for(int j =0; j <=i; j++) {
+               if( j ==0 || j == i) {
+                   row.add(1);
+               }
+               else {
+                   row.add(triangle.get(i-1).get(j-1)+ triangle.get(i-1).get(j));
+               }
+           }
+           triangle.add(row);
+       }
+       return triangle;
+    }
 }
