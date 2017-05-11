ffc687b48a22769ff209dc8eb4eb4bc903eece1e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-11 21:02:37 +0530

    Leet code

--- src/LeetCode/Problems/PlusOne.java
+++ src/LeetCode/Problems/PlusOne.java
@@ -24,5 +24,21 @@ public class PlusOne {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+     public int[] plusOne(int[] digits) {
+    for (int i = digits.length - 1; i >=0; i--) {
+        if (digits[i] != 9) {
+            digits[i]++;
+            break;
+        } else {
+            digits[i] = 0;
+        }
+    }
+    if (digits[0] == 0) {
+        int[] res = new int[digits.length+1];
+        res[0] = 1;
+        return res;
+    }
+    return digits;
+}
     
 }
