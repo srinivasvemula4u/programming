90803a1208e245d31b6bcb78bf1171bfa1db7a4a
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-12 19:21:52 +0530

    journalDev

---
+++ src/JournalDev/Threads/WaitNotifyTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Threads;
+
+/**
+ *
+ * @author srivemul
+ */
+public class WaitNotifyTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Message msg = new Message("process it");
+        Waiter waiter = new Waiter(msg);
+        new Thread(waiter, "waiter").start();
+
+        Waiter waiter1 = new Waiter(msg);
+        new Thread(waiter1, "waiter1").start();
+
+        Notifier notifier = new Notifier(msg);
+        new Thread(notifier, "notifier").start();
+        System.out.println("All the threads are started");
+    }
+
+}
