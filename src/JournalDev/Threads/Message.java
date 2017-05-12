cd01988655e8056e8620a7c0b938ccc9130d9a0d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-12 19:19:11 +0530

    journalDev

---
+++ src/JournalDev/Threads/Message.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Threads;
+
+/**
+ *
+ * @author srivemul
+ */
+public class Message {
+
+    private String msg;
+
+    public Message(String str) {
+        this.msg = str;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String str) {
+        this.msg = str;
+    }
+
+}
