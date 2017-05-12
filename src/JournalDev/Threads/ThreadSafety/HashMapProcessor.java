447923bcbfa48fb55ac0e441230efafce7c62f41
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-12 22:57:37 +0530

    journalDev

---
+++ src/JournalDev/Threads/ThreadSafety/HashMapProcessor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Threads.ThreadSafety;
+
+/**
+ *
+ * @author srivemul
+ */
+public class HashMapProcessor {
+    
+}
--- src/JournalDev/Threads/WaitNotifyNotifyAll/Waiter.java
+++ src/JournalDev/Threads/WaitNotifyNotifyAll/Waiter.java
@@ -9,27 +9,27 @@ package JournalDev.Threads.WaitNotifyNotifyAll;
  *
  * @author srivemul
  */
-public class Waiter implements Runnable {
-
+public class Waiter implements Runnable{
+    
     private Message msg;
-
-    public Waiter(Message m) {
-        this.msg = m;
+    
+    public Waiter(Message m){
+        this.msg=m;
     }
 
     @Override
     public void run() {
         String name = Thread.currentThread().getName();
-        synchronized (msg) {
-            try {
-                System.out.println(name + " waiting to get notified at time:" + System.currentTimeMillis());
+       synchronized(msg) {
+            try{
+                System.out.println(name+" waiting to get notified at time:"+System.currentTimeMillis());
                 msg.wait();
-            } catch (InterruptedException e) {
+            }catch(InterruptedException e){
                 e.printStackTrace();
             }
-            System.out.println(name + " waiter thread got notified at time:" + System.currentTimeMillis());
+            System.out.println(name+" waiter thread got notified at time:"+System.currentTimeMillis());
             //process the message now
-            System.out.println(name + " processed: " + msg.getMsg());
+            System.out.println(name+" processed: "+msg.getMsg());
         }
     }
 
