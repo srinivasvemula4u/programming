cc4ebfe1c5f5241dcbab99fe5d30d2eaec478f9f
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-12 19:21:15 +0530

    journalDev

---
+++ src/JournalDev/Threads/Notifier.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Threads;
+
+/**
+ *
+ * @author srivemul
+ */
+public class Notifier implements Runnable {
+
+    private Message msg;
+    
+    public Notifier(Message msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        System.out.println(name+" started");
+        try {
+            Thread.sleep(1000);
+            synchronized (msg) {
+                msg.setMsg(name+" Notifier work done");
+                msg.notify();
+                // msg.notifyAll();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+}
