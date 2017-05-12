1a924d865828065490afbb29d15fa07fdbf17a03
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-12 22:58:13 +0530

    journalDev

--- src/JournalDev/Threads/ThreadSafety/HashMapProcessor.java
+++ src/JournalDev/Threads/ThreadSafety/HashMapProcessor.java
@@ -9,6 +9,42 @@ package JournalDev.Threads.ThreadSafety;
  *
  * @author srivemul
  */
-public class HashMapProcessor {
-    
+public class HashMapProcessor implements Runnable {
+
+    private String[] strArr = null;
+
+    public HashMapProcessor(String[] m) {
+        this.strArr = m;
+    }
+
+    public String[] getMap() {
+        return strArr;
+    }
+
+    @Override
+    public void run() {
+        processArr(Thread.currentThread().getName());
+    }
+
+    private void processArr(String name) {
+        for (int i = 0; i < strArr.length; i++) {
+            //process data and append thread name
+            processSomething(i);
+            addThreadName(i, name);
+        }
+    }
+
+    private void addThreadName(int i, String name) {
+        strArr[i] = strArr[i] + ":" + name;
+    }
+
+    private void processSomething(int index) {
+        // processing some job
+        try {
+            Thread.sleep(index * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
