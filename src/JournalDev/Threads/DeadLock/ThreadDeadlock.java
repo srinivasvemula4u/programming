71908e32567002180208928526af1ed66e58a604
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-13 11:04:01 +0530

    journalDev

--- src/JournalDev/Threads/DeadLock/ThreadDeadlock.java
+++ src/JournalDev/Threads/DeadLock/ThreadDeadlock.java
@@ -22,7 +22,9 @@ public class ThreadDeadlock {
         Thread t1 = new Thread(new SyncThread(obj1, obj2), "t1");
         Thread t2 = new Thread(new SyncThread(obj2, obj3), "t2");
         Thread t3 = new Thread(new SyncThread(obj3, obj1), "t3");
-
+//      Thread t1 = new Thread(new SyncThread(obj1, obj2), "t1");
+//      Thread t2 = new Thread(new SyncThread(obj1, obj2), "t2");
+//       Thread t3 = new Thread(new SyncThread(obj1, obj2), "t3");
         t1.start();
         Thread.sleep(5000);
         t2.start();
@@ -63,7 +65,8 @@ class SyncThread implements Runnable {
 
     private void work() {
         try {
-            Thread.sleep(30000);
+           Thread.sleep(30000);
+          // obj1.wait();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
