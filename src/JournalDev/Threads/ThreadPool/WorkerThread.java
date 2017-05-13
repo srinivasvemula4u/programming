1ef38effb0a53064499ade6cc241e23e65a50e43
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-13 20:37:55 +0530

    journalDev

---
+++ src/JournalDev/Threads/ThreadPool/WorkerThread.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Threads.ThreadPool;
+
+/**
+ *
+ * @author srivemul
+ */
+public class WorkerThread implements Runnable {
+  
+    private String command;
+    
+    public WorkerThread(String s){
+        this.command=s;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName()+" Start. Command = "+command);
+        processCommand();
+        System.out.println(Thread.currentThread().getName()+" End.");
+    }
+
+    private void processCommand() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return this.command;
+    }
+}
+    
+
