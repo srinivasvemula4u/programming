65a3e32d7a91fccb18dda03368251aa27c4e7704
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-13 16:52:05 +0530

    journalDev

---
+++ src/JournalDev/Threads/ProducerAndConsumer/Producer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Threads.ProducerAndConsumer;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author srivemul
+ */
+public class Producer implements Runnable {
+
+    BlockingQueue<Message> queue;
+
+    public Producer(BlockingQueue<Message> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            Message msg = new Message("" + i);
+            try {
+                Thread.sleep(i);
+                queue.put(msg);
+                System.out.println("Produced :" + msg.getMessge());
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        Message msg = new Message("Exit");
+        try {
+            queue.put(msg);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
