2d3554e629fe54afb2729bc2dba76a65548cae26
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 11:52:09 +0530

    LeetCode

--- src/LeetCode/Problems/ArrangingCoins.java
+++ src/LeetCode/Problems/ArrangingCoins.java
@@ -60,5 +60,36 @@ public class ArrangingCoins {
         }
         return start - 1;
     }
+    /*
+    Complexity Analysis
+
+Uniform cost model is used as Cost Model and `n` is the input number. `b` in this case would be `2`.
+
+Time Complexity:
+
+Best Case `O(log_b(n))` : With respect to the input, the algorithm will always depend on the value of input.
+Average Case `O(log_b(n))` : With respect to the input, the algorithm will always depend on the value of input.
+Worst Case `O(log_b(n))` : With respect to the input, the algorithm will always depend on the value of input.
+Auxiliary Space:
+
+Worst Case `O(1)` : Additional variables are of constant size.
+Algorithm
+
+Approach: Binary Search
+
+The problem is basically asking the maximum length of consecutive number that has the running sum lesser or equal to `n`. 
+    In other word, find `x` that satisfy the following condition:
+
+`1 + 2 + 3 + 4 + 5 + 6 + 7 + ... + x <= n`
+`sum_{i=1}^x i <= n`
+Running sum can be simplified,
+
+`(x * ( x + 1)) / 2 <= n`
+Binary search is used in this case to slowly narrow down the `x` that will satisfy the equation. 
+    Note that 0.5 * mid * mid + 0.5 * mid does not have overflow issue as the intermediate result is 
+    implicitly autoboxed to double data type.
+
+
+    */
 
 }
