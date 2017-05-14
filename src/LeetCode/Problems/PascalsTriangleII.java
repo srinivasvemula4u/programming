9ef478567056606fe240b7ac22d88130cf88d0fd
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 12:01:50 +0530

    LeetCode

---
+++ src/LeetCode/Problems/PascalsTriangleII.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+ */
+public class PascalsTriangleII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
