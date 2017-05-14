d9fb074b5beae867106e3acbaa7fb7f56ab9c88e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 21:41:04 +0530

    LeetCode

--- src/LeetCode/Problems/LinkedListCycleII.java
+++ src/LeetCode/Problems/LinkedListCycleII.java
@@ -13,7 +13,7 @@ package LeetCode.Problems;
 Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
 
 Note: Do not modify the linked list.
-*/
+ */
 public class LinkedListCycleII {
 
     /**
@@ -22,5 +22,28 @@ public class LinkedListCycleII {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slowPtr = head, fastPtr = head;
+        boolean loopExists = false;
+        while (fastPtr != null && fastPtr.next != null) {
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if (slowPtr == fastPtr) {
+                loopExists = true;
+                break;
+            }
+        }
+        if (loopExists) {
+            slowPtr = head;
+            while (slowPtr != fastPtr) {
+                slowPtr = slowPtr.next;
+                fastPtr = fastPtr.next;
+            }
+            return fastPtr;
+        } else {
+            return null;
+        }
+
+    }
 }
