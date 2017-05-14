3d081933d683596ea1983685d7b2f3bb09253b53
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 11:50:21 +0530

    LeetCode

--- src/LeetCode/Problems/ArrangingCoins.java
+++ src/LeetCode/Problems/ArrangingCoins.java
@@ -47,4 +47,18 @@ public class ArrangingCoins {
         // TODO code application logic here
     }
 
+    public int arrangeCoins(int n) {
+        int start = 0, end = n;
+        int mid = 0;
+        while (start <= end) {
+            mid = (start + end) >>> 1;
+            if ((0.5 * mid * mid + 0.5 * mid) <= n) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return start - 1;
+    }
+
 }
