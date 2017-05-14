90bfb4938f7695879d6c92afe81aa40e579c25a4
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 19:11:18 +0530

    LeetCode

--- src/LeetCode/Problems/FactorialTrailingZeroes.java
+++ src/LeetCode/Problems/FactorialTrailingZeroes.java
@@ -25,5 +25,14 @@ public class FactorialTrailingZeroes {
       public int trailingZeroes(int n) {
       return n == 0 ? 0 : n / 5 + trailingZeroes(n / 5);
     }
+      
+       public int trailingZeroes2(int n) {
+        int r = 0;
+        while (n > 0) {
+            n /= 5;
+            r += n;
+        }
+        return r;
+    }
 
 }
