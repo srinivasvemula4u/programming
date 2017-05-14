dafec9a60310dc6da2bdebcf18f62d3cd4084277
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 12:02:26 +0530

    LeetCode

--- src/LeetCode/Problems/PascalsTriangleII.java
+++ src/LeetCode/Problems/PascalsTriangleII.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author srivemul
@@ -25,6 +28,26 @@ public class PascalsTriangleII {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        if (rowIndex < 0) {
+            return new ArrayList<Integer>();
+        }
+        for (int i = 0; i < rowIndex + 1; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+                }
+            }
+            triangle.add(row);
+        }
+        return triangle.get(rowIndex);
+
     }
 
 }
