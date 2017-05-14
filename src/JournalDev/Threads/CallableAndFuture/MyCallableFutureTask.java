d77d93cae5d69d9164cea2eebe1e51b3ca4a43e1
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 10:15:50 +0530

    journalDev

--- src/JournalDev/Threads/CallableAndFuture/MyCallableFutureTask.java
+++ src/JournalDev/Threads/CallableAndFuture/MyCallableFutureTask.java
@@ -6,6 +6,12 @@
 package JournalDev.Threads.CallableAndFuture;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  *
@@ -25,5 +31,44 @@ public class MyCallableFutureTask implements Callable<String> {
         //return the thread name executing this callable task
         return Thread.currentThread().getName();
     }
+    public static void main(String[] args) throws ExecutionException {
+		MyCallableFutureTask callable1 = new MyCallableFutureTask(1000);
+		MyCallableFutureTask callable2 = new MyCallableFutureTask(2000);
+
+		FutureTask<String> futureTask1 = new FutureTask<String>(callable1);
+		FutureTask<String> futureTask2 = new FutureTask<String>(callable2);
+
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		executor.execute(futureTask1);
+		executor.execute(futureTask2);
+		
+		while (true) {
+			try {
+				if(futureTask1.isDone() && futureTask2.isDone()){
+					System.out.println("Done");
+					//shut down executor service
+					executor.shutdown();
+					return;
+				}
+				
+				if(!futureTask1.isDone()){
+				//wait indefinitely for future task to complete
+				System.out.println("FutureTask1 output="+futureTask1.get());
+				}
+				
+				System.out.println("Waiting for FutureTask2 to complete");
+				String s = futureTask2.get(200L, TimeUnit.MILLISECONDS);
+				if(s !=null){
+					System.out.println("FutureTask2 output="+s);
+				}
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}catch(TimeoutException e){
+				//do nothing
+			}
+		}
+		
+	}
+
 
 }
