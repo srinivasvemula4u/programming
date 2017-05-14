fd46ce2fb50b776fe5acd81a1e26f14515fd89e9
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 16:13:45 +0530

    LeetCode

---
+++ src/LeetCode/Problems/ImplementStackUsingQueues.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Notes:
+You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and 
+is empty operations are valid.
+Depending on your language, queue may not be supported natively. You may simulate a queue by using a
+list or deque (double-ended queue), as long as you use only standard operations of a queue.
+You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+*/
+public class ImplementStackUsingQueues {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
