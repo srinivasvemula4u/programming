68d607640aa6fab068a5a60e1def4974296a48a8
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 19:01:47 +0530

    LeetCode

---
+++ src/LeetCode/Problems/FactorialTrailingZeroes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LeetCode.Problems;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+ */
+public class FactorialTrailingZeroes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
