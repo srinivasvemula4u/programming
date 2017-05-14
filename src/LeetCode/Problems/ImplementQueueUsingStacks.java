c6c2348b2e952979e37ee2fba93a55ce6a45f733
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 12:56:52 +0530

    LeetCode

--- src/LeetCode/Problems/ImplementQueueUsingStacks.java
+++ src/LeetCode/Problems/ImplementQueueUsingStacks.java
@@ -5,6 +5,8 @@
  */
 package LeetCode.Problems;
 
+import java.util.Stack;
+
 /**
  *
  * @author srivemul
@@ -22,7 +24,7 @@ peek/pop from top, size, and is empty operations are valid.
 Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque 
 (double-ended queue), as long as you use only standard operations of a stack.
 You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
-*/
+ */
 public class ImplementQueueUsingStacks {
 
     /**
@@ -31,5 +33,59 @@ public class ImplementQueueUsingStacks {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    Stack<Integer> s1, s2;
+
+    public ImplementQueueUsingStacks() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    /**
+     * Push element x to the back of queue.
+     */
+    public void push(int x) {
+        s1.push(x);
+    }
+
+    /**
+     * Removes the element from in front of queue and returns that element.
+     */
+    public int pop() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        if (!s2.isEmpty()) {
+            return s2.pop();
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Get the front element.
+     */
+    public int peek() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        if (!s2.isEmpty()) {
+            return s2.peek();
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Returns whether the queue is empty.
+     */
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
 }
+
+    
+
