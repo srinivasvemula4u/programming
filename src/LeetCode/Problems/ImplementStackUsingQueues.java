458498737020264dda65b231e90a480e167ab07b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-14 18:57:43 +0530

    LeetCode

--- src/LeetCode/Problems/ImplementStackUsingQueues.java
+++ src/LeetCode/Problems/ImplementStackUsingQueues.java
@@ -5,6 +5,9 @@
  */
 package LeetCode.Problems;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  *
  * @author srivemul
@@ -22,14 +25,51 @@ is empty operations are valid.
 Depending on your language, queue may not be supported natively. You may simulate a queue by using a
 list or deque (double-ended queue), as long as you use only standard operations of a queue.
 You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
-*/
+ */
 public class ImplementStackUsingQueues {
 
     /**
      * @param args the command line arguments
      */
+    Queue<Integer> q;
+
+    public ImplementStackUsingQueues() {
+        q = new LinkedList<>();
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        q.offer(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.offer(q.poll());
+        }
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return q.poll();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return q.peek();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return q.isEmpty();
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
