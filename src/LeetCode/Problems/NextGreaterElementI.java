927a2fb0d149de86d2ad6722be0fa1f07b5ac18e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-16 07:46:50 +0530

    LeetCode

--- src/LeetCode/Problems/NextGreaterElementI.java
+++ src/LeetCode/Problems/NextGreaterElementI.java
@@ -14,7 +14,7 @@ import java.util.Stack;
  * @author srivemul
  */
 /*
-ou are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. 
+you are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. 
 Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
 
 The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. 
