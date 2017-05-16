171e75192ef2ef6205f8977519cf651d3730c80b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-16 07:46:26 +0530

    LeetCode

--- src/LeetCode/Problems/NextGreaterElementI.java
+++ src/LeetCode/Problems/NextGreaterElementI.java
@@ -14,9 +14,11 @@ import java.util.Stack;
  * @author srivemul
  */
 /*
-ou are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+ou are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. 
+Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
 
-The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. 
+If it does not exist, output -1 for this number.
 
 Example 1:
 Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
@@ -40,12 +42,14 @@ The length of both nums1 and nums2 would not exceed 1000.
 Solution Hint: 
 Key observation:
 Suppose we have a decreasing sequence followed by a greater number
-For example [5, 4, 3, 2, 1, 6] then the greater number 6 is the next greater element for all previous numbers in the sequence
+For example [5, 4, 3, 2, 1, 6] then the greater number 6 is the next greater element for all previous numbers 
+in the sequence
 
 We use a stack to keep a decreasing sub-sequence, whenever we see a number x greater than stack.peek() 
 we pop all elements less than x and for all the popped ones, their next greater element is x
 For example [9, 8, 7, 3, 2, 1, 6]
-The stack will first contain [9, 8, 7, 3, 2, 1] and then we see 6 which is greater than 1 so we pop 1 2 3 whose next greater element should be 6
+The stack will first contain [9, 8, 7, 3, 2, 1] and then we see 6 which is greater than 1 so we pop 1 2 3 whose 
+next greater element should be 6
 ----------------------------------------------------------------------------------------------------------------
  */
 public class NextGreaterElementI {
