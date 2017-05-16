7e8d034a15a4080b46b6a2c232af37b39682dd7b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-16 07:55:15 +0530

    LeetCode

--- src/LeetCode/Problems/NimGame.java
+++ src/LeetCode/Problems/NimGame.java
@@ -10,6 +10,18 @@ package LeetCode.Problems;
  * @author srivemul
  */
 /*
+You are playing the following Nim Game with your friend: There is a heap of stones on the table, 
+each time one of you take turns to remove 1 to 3 stones. 
+The one who removes the last stone will be the winner. You will take the first turn to remove the stones.
+
+Both of you are very clever and have optimal strategies for the game. Write a function to determine whether 
+you can win the game given the number of stones in the heap.
+
+For example, if there are 4 stones in the heap, then you will never win the game: no matter 1, 2, or 3 
+stones you remove, the last stone will always be removed by your friend.
+ */
+
+ /*
 Theorem: The first one who got the number that is multiple of 4 (i.e. n % 4 == 0) will lost, otherwise he/she will win.
 Proof:
 
@@ -27,7 +39,7 @@ Following the second case, for numbers between (2*4 = 8) and (3*4=12), which are
 for the first player again, because the first player can always reduce the number into the death number 8.
 
 Following the above theorem and proof, the solution could not be simpler:
-*/
+ */
 public class NimGame {
 
     /**
@@ -36,8 +48,9 @@ public class NimGame {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
     public boolean canWinNim(int n) {
-       return n % 4 != 0;
+        return n % 4 != 0;
     }
-    
+
 }
