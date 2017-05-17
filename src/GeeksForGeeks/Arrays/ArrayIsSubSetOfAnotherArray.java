fcc575e8d752458f20268cb289a270e37ca08cbe
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-17 08:14:12 +0530

    GeeskForGeeks

--- src/GeeksForGeeks/Arrays/ArrayIsSubSetOfAnotherArray.java
+++ src/GeeksForGeeks/Arrays/ArrayIsSubSetOfAnotherArray.java
@@ -9,6 +9,20 @@ package GeeksForGeeks.Arrays;
  *
  * @author srivemul
  */
+/*
+Given two arrays: arr1[0..m-1] and arr2[0..n-1]. Find whether arr2[] is a subset of arr1[] or not. 
+Both the arrays are not in sorted order. It may be assumed that elements in both array are distinct.
+
+Examples:
+Input: arr1[] = {11, 1, 13, 21, 3, 7}, arr2[] = {11, 3, 7, 1}
+Output: arr2[] is a subset of arr1[]
+
+Input: arr1[] = {1, 2, 3, 4, 5, 6}, arr2[] = {1, 2, 4}
+Output: arr2[] is a subset of arr1[]
+
+Input: arr1[] = {10, 5, 2, 23, 19}, arr2[] = {19, 5, 3}
+Output: arr2[] is not a subset of arr1[]
+*/
 public class ArrayIsSubSetOfAnotherArray {
 
     /**
