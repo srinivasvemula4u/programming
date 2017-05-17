6aa2391281c23f57db8cf7660950dd8f9ed0130e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-17 19:37:05 +0530

    GeeskForGeeks

---
+++ src/GeeksForGeeks/Arrays/MatrixInSpiralForm.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays;
+
+/**
+ *
+ * @author srivemul
+ */
+/*
+Print a given matrix in spiral form
+Given a 2D array, print it in spiral form. See the following examples.
+
+Input:
+        1    2   3   4
+        5    6   7   8
+        9   10  11  12
+        13  14  15  16
+Output: 
+1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10 
+
+
+Input:
+        1   2   3   4  5   6
+        7   8   9  10  11  12
+        13  14  15 16  17  18
+Output: 
+1 2 3 4 5 6 12 18 17 16 15 14 13 7 8 9 10 11
+spiral-matrix
+ */
+public class MatrixInSpiralForm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
