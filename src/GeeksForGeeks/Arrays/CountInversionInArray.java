d791300ec2688bcfa6d8927bf2516da7d96987b0
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-17 19:21:43 +0530

    GeeskForGeeks

--- src/GeeksForGeeks/Arrays/CountInversionInArray.java
+++ src/GeeksForGeeks/Arrays/CountInversionInArray.java
@@ -10,6 +10,14 @@ package GeeksForGeeks.Arrays;
  * @author srivemul METHOD 1 (Simple) For each element, count number of elements
  * which are on right side of it and are smaller than it.
  */
+/*Inversion Count for an array indicates – how far (or close) the array is from being sorted. 
+If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count 
+is the maximum. 
+Formally speaking, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j
+
+Example:
+The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3). */
+
 public class CountInversionInArray {
 
     private static int countInversionsInArray(int[] arr) {
