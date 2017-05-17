3976aaca5919bbe6f26bc2f32a41f57916a6f51e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-17 19:09:58 +0530

    GeeskForGeeks

--- src/GeeksForGeeks/Arrays/BlockSwapArrayRotation.java
+++ src/GeeksForGeeks/Arrays/BlockSwapArrayRotation.java
@@ -9,7 +9,32 @@ package GeeksForGeeks.Arrays;
  *
  * @author srivemul
  */
-public class BlowSwapArrayRotation {
+/*
+Block swap algorithm for array rotation
+Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+
+Array
+
+Rotation of the above array by 2 will make array
+
+ArrayRotation1
+
+Algorithm:
+
+Initialize A = arr[0..d-1] and B = arr[d..n-1]
+1) Do following until size of A is equal to size of B
+
+  a)  If A is shorter, divide B into Bl and Br such that Br is of same 
+       length as A. Swap A and Br to change ABlBr into BrBlA. Now A
+       is at its final place, so recur on pieces of B.  
+
+   b)  If A is longer, divide A into Al and Ar such that Al is of same 
+       length as B Swap Al and B to change AlArB into BArAl. Now B
+       is at its final place, so recur on pieces of A.
+
+2)  Finally when A and B are of equal size, block swap them.
+*/
+public class BlockSwapArrayRotation {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
