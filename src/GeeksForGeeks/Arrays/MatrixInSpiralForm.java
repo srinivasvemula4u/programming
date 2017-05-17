843e7e74a9b03e3bc0423d5e6aa63940e717183c
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-17 19:59:32 +0530

    GeeskForGeeks

--- src/GeeksForGeeks/Arrays/MatrixInSpiralForm.java
+++ src/GeeksForGeeks/Arrays/MatrixInSpiralForm.java
@@ -35,8 +35,43 @@ public class MatrixInSpiralForm {
     /**
      * @param args the command line arguments
      */
+    public static void printSpiralForm(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        int k = 0, l = 0;
+        while (k < m && l < n) {
+            for (int i = l; i < n; i++) {
+                System.out.print(arr[k][i] + " ");
+            }
+            k++;
+            for (int i = k; i < m; i++) {
+                System.out.print(arr[i][n - 1] + " ");
+            }
+            n--;
+            if (k < m) {
+                for (int i = n - 1; i >= l; i--) {
+                    System.out.print(arr[m - 1][i] + " ");
+                }
+                m--;
+            }
+            if (l < n) {
+                for (int i = m - 1; i >= k; i--) {
+                    System.out.print(arr[i][l] + " ");
+                }
+                l++;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[][] a = {{1, 2, 3, 4, 5, 6},
+        {7, 8, 9, 10, 11, 12},
+        {13, 14, 15, 16, 17, 18}
+        };
+
+        printSpiralForm(a);
+        // return 0;
+
     }
 
 }
