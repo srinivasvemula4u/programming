88e2890fa389d9096025850a66c00719cca25c64
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-18 21:22:51 +0530

    GeeskForGeeks

---
+++ src/JournalDev/Reflection/BaseClass.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Reflection;
+
+/**
+ *
+ * @author srivemul
+ */
+public class BaseClass {
+
+    public int baseInt;
+
+    private static void method3() {
+        System.out.println("Method3");
+    }
+
+    public int method4() {
+        System.out.println("Method4");
+        return 0;
+    }
+
+    public static int method5() {
+        System.out.println("Method5");
+        return 0;
+    }
+
+    void method6() {
+        System.out.println("Method6");
+    }
+
+    // inner public class
+    public class BaseClassInnerClass {
+    }
+
+    //member public enum
+    public enum BaseClassMemberEnum {
+    }
+}
