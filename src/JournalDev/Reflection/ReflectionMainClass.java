2f06b309cd340a426594b38c57d991191c3dbd45
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-18 21:33:38 +0530

    GeeskForGeeks

--- src/JournalDev/Reflection/ReflectionMainClass.java
+++ src/JournalDev/Reflection/ReflectionMainClass.java
@@ -17,15 +17,24 @@ public class ReflectionMainClass {
     /*
     We can get Class of an object using three methods – through static variable class,
     using getClass() method of object and java.lang.Class.forName(String fullyClassifiedClassName). For primitive 
-    */
+     */
     public static void main(String[] args) throws ClassNotFoundException {
         // TODO code application logic here
-        Class<?> concreteClass  = ConcreteClass.class;
+        Class<?> concreteClass = ConcreteClass.class;
         System.out.println(concreteClass.getCanonicalName());
         concreteClass = new ConcreteClass(10).getClass();
         System.out.println(concreteClass.getCanonicalName());
         concreteClass = Class.forName("JournalDev.Reflection.ConcreteClass");
         System.out.println(concreteClass.getCanonicalName());
+
+        //for primitive types, wrapper classes and arrays
+        Class<?> booleanClass = boolean.class;
+        System.out.println(booleanClass.getCanonicalName()); // prints boolean
+        
+        Class<?> cDouble = Double.class;
+        System.out.println(cDouble.getCanonicalName());
+        cDouble = Double.TYPE;
+         System.out.println(cDouble.getCanonicalName());
     }
-    
+
 }
