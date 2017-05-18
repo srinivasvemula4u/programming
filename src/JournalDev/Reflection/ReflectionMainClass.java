3f3374f112f6899a270a772f10c22ad0966a9f7e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-18 21:31:24 +0530

    GeeskForGeeks

---
+++ src/JournalDev/Reflection/ReflectionMainClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Reflection;
+
+/**
+ *
+ * @author srivemul
+ */
+public class ReflectionMainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    /*
+    We can get Class of an object using three methods – through static variable class,
+    using getClass() method of object and java.lang.Class.forName(String fullyClassifiedClassName). For primitive 
+    */
+    public static void main(String[] args) throws ClassNotFoundException {
+        // TODO code application logic here
+        Class<?> concreteClass  = ConcreteClass.class;
+        System.out.println(concreteClass.getCanonicalName());
+        concreteClass = new ConcreteClass(10).getClass();
+        System.out.println(concreteClass.getCanonicalName());
+        concreteClass = Class.forName("JournalDev.Reflection.ConcreteClass");
+        System.out.println(concreteClass.getCanonicalName());
+    }
+    
+}
