6fd23057a577e7756eb040b1aea0fd2353df4f9a
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-18 21:24:27 +0530

    GeeskForGeeks

---
+++ src/JournalDev/Reflection/ConcreteClass.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Reflection;
+
+/**
+ *
+ * @author srivemul
+ */
+public class ConcreteClass extends BaseClass implements BaseInterface {
+
+    public int publicInt;
+    private String privateString = "private string";
+    protected boolean protectedBoolean;
+    Object defaultObject;
+
+    public ConcreteClass(int i) {
+        this.publicInt = i;
+    }
+
+    @Override
+    public void method1() {
+        System.out.println("Method1 impl.");
+    }
+
+    @Override
+    public int method2(String str) {
+        System.out.println("Method2 impl.");
+        return 0;
+    }
+
+    @Override
+    public int method4() {
+        System.out.println("Method4 overriden.");
+        return 0;
+    }
+
+    public int method5(int i) {
+        System.out.println("Method4 overriden.");
+        return 0;
+    }
+
+    // inner classes
+    public class ConcreteClassPublicClass {
+    }
+
+    private class ConcreteClassPrivateClass {
+    }
+
+    protected class ConcreteClassProtectedClass {
+    }
+
+    class ConcreteClassDefaultClass {
+    }
+
+    //member enum
+    enum ConcreteClassDefaultEnum {
+    }
+
+    public enum ConcreteClassPublicEnum {
+    }
+
+    //member interface
+    public interface ConcreteClassPublicInterface {
+    }
+
+}
