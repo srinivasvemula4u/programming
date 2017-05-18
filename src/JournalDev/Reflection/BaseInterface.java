d5f059c91d1007055b77f586405552a59a1637d2
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-18 21:13:25 +0530

    GeeskForGeeks

---
+++ src/JournalDev/Reflection/BaseInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JournalDev.Reflection;
+
+/**
+ *
+ * @author srivemul
+ */
+public interface BaseInterface {
+
+    public int interfaceInt = 0;
+
+    void method1();
+
+    int method2(String str);
+}
