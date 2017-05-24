cb90f0505100d89053395632f649f01982f7361b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-24 19:58:25 +0530

    GeeskForGeeks

--- src/SamsungTest.java
+++ src/SamsungTest.java
@@ -16,5 +16,22 @@ public class SamsungTest {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
+    int findLargestNeighbourElem(int[] arr) {
+        int start = 0, end = arr.length - 1, n = arr.length;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+            if ((mid - 1 > 0 && arr[mid] > arr[mid - 1]) && (mid + 1 < n && arr[mid] > arr[mid + 1])) {
+                return arr[mid];
+            } else if (mid - 1 > 0 && arr[mid - 1] > arr[mid]) {
+                end = mid - 1;
+            } else if (mid + 1 < n && arr[mid] < arr[mid + 1]) {
+                start = mid + 1;
+            }
+        }
+        return -1;
+
+    }
+
 }
