e2a56f369f54c89c53bf5e49036aa3ecd33a5cd6
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-05-31 22:38:01 +0530

    JournalDev

--- src/CodeFight/ArcadeUniverse/CheckPalindrome.java
+++ src/CodeFight/ArcadeUniverse/CheckPalindrome.java
@@ -9,6 +9,31 @@ package CodeFight.ArcadeUniverse;
  *
  * @author srivemul
  */
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+Input/Output
+
+[time limit] 3000ms (java)
+[input] string inputString
+
+A non-empty string consisting of lowercase characters.
+
+Guaranteed constraints:
+1 ≤ inputString.length ≤ 105.
+
+[output] boolean
+
+true if inputString is a palindrome, false otherwise.
+ */
 public class CheckPalindrome {
 
     /**
@@ -17,5 +42,5 @@ public class CheckPalindrome {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
