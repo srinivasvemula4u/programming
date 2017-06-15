d93582b297e3244d668df4fc15fd7ff7f4657f41
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-06-15 21:00:37 +0530

    geeksforgeeks

--- src/HackerEarth/Practice/FredoAndArrayUpdate.java
+++ src/HackerEarth/Practice/FredoAndArrayUpdate.java
@@ -9,6 +9,62 @@ package HackerEarth.Practice;
  *
  * @author srivemul
  */
+/*
+Fredo is assigned a new task today. He is given an array 
+A
+A containing 
+N
+N integers. His task is to update all elements of array to some minimum value 
+x
+x , that is, 
+A
+[
+i
+]
+=
+x
+A[i]=x ; 
+1
+≤
+i
+≤
+N
+1≤i≤N such that sum of this new array is strictly greater than the sum of the initial array. Note that 
+x
+x should be as minimum as possible such that sum of the new array is greater than the sum of the initial array.
+
+Input Format:
+First line of input consists of an integer 
+N
+N denoting the number of elements in the array 
+A
+A.
+Second line consists of 
+N
+N space separated integers denoting the array elements.
+
+Output Format:
+The only line of output consists of the value of 
+x
+x.
+
+Input Constraints:
+ 1
+≤
+N
+≤
+10
+5
+1≤N≤105
+ 1
+≤
+A
+[
+i
+]
+≤
+1000
+ */
 public class FredoAndArrayUpdate {
-    
+
 }
