574849262fd647fe23eb639ff5177b4c3f23808b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2017-06-15 20:52:56 +0530

    geeksforgeeks

--- src/HackerEarth/Practice/LittleShinoAndCommonFactors.java
+++ src/HackerEarth/Practice/LittleShinoAndCommonFactors.java
@@ -9,6 +9,34 @@ package HackerEarth.Practice;
  *
  * @author srivemul
  */
+/*
+Little Shino loves maths. Today her teacher gave her two integers. Shino is now wondering how many integers 
+can divide both the numbers. She is busy with her assignments. Help her to solve the problem.
+
+Input:
+First line of the input file contains two integers, 
+a
+a and 
+b
+b.
+
+Output:
+Print the number of common factors of 
+a
+a and 
+b
+b.
+
+Constraints:
+ 1
+≤
+a
+,
+b
+≤
+10
+12
+*/
 public class LittleShinoAndCommonFactors {
     
 }
