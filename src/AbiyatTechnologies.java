0ba9bd8318b6896ffc81fe351070ca4de38314a0
Srinivas <srinivasvemula.mtech@gmail.com>
2017-06-29 18:03:09 +0530

    tets

--- src/AbiyatTechnologies.java
+++ src/AbiyatTechnologies.java
@@ -1,3 +1,6 @@
+
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,24 @@
  *
  * @author Srinivas
  */
+/*
+A -> B, C
+B ->  D
+C -> D
+D -> E
+E -> {}
+List<Task> getAllDependencies()
+b.getAllDependencies = {D,E}
+a.getAllDepencies = {B,C,D,E}
+*/
 public class AbiyatTechnologies {
-    
+  List<Task> getAllDependencies() {
+    // List<Task> lis= new ArrayList
+   for( int i =0; i < this.taskList.size(); i++) {
+         if(!taskList.contains(task.taskList.get(i)) {
+		    taskList.add(task.taskList.get(i));
+            task.taskList.get(i).getAllDependencies( );
+		 }
+}
+}  
 }
