33c3b414bc3d70a03492aa52fead5a5b564ec9f0
Srinivas <srinivasvemula.mtech@gmail.com>
2017-06-29 18:12:04 +0530

    tests

--- src/AbiyatTechnologies.java
+++ src/AbiyatTechnologies.java
@@ -1,5 +1,8 @@
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -21,14 +24,38 @@ List<Task> getAllDependencies()
 b.getAllDependencies = {D,E}
 a.getAllDepencies = {B,C,D,E}
 */
-public class AbiyatTechnologies {
-  List<Task> getAllDependencies() {
+
+
+/*[5:06:06 PM] Srinivas  Vemula: List<Task> getAllDependencies() {
     // List<Task> lis= new ArrayList
    for( int i =0; i < this.taskList.size(); i++) {
          if(!taskList.contains(task.taskList.get(i)) {
-		    taskList.add(task.taskList.get(i));
+      taskList.add(task.taskList.get(i));
             task.taskList.get(i).getAllDependencies( );
+   }
+}
+}*/
+
+public class AbiyatTechnologies {
+ class Task {
+   private Long taskId;
+   private String taskName;
+   private String taskOwner;
+   private String taskStartDate;
+   private String taskEndDate;
+   private List<Task> taskList = new ArrayList<>();
+}   
+ List<Task> getAllDependenciesHelper(Task task, List<Task> taskList) {
+   
+   for( int i =0; i < task.taskList.size(); i++) {
+         if(taskList.contains(task.taskList.get(i))) {
+		     getAllDependenciesHelper(task.taskList.get(i),taskList);
+		 } else {
+		    taskList.add(task.taskList.get(i));
+			 getAllDependenciesHelper(task.taskList.get(i),taskList);
 		 }
 }
-}  
+   return taskList; 
+}
+ 
 }
