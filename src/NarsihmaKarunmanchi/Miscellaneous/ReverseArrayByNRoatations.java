d6edbf79871f207c873edc9d5297277d0788d698
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-09 11:47:41 +0530

    Karumanchi miscellaneous

--- src/NarsihmaKarunmanchi/Miscellaneous/ReverseArrayByNRoatations.java
+++ src/NarsihmaKarunmanchi/Miscellaneous/ReverseArrayByNRoatations.java
@@ -7,11 +7,8 @@ package NarsihmaKarunmanchi.Miscellaneous;
 
 /**
  *
- * @author sv186040
- * Roatate(arr,d, n);
- * Reverse(arr,1,d);
- * Reverse(arr,d+1,n);
- * 
+ * @author sv186040 Roatate(arr,d, n); Reverse(arr,1,d); Reverse(arr,d+1,n);
+ *
  */
 public class ReverseArrayByNRoatations {
 
@@ -19,11 +16,29 @@ public class ReverseArrayByNRoatations {
      * @param args the command line arguments
      */
     public static void leftRotateArray(int[] arr, int n, int d) {
-        
+        reverseArray(arr, 0, d - 1);
+        reverseArray(arr, d, n - 1);
+        reverseArray(arr, 0, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
+
+    private static void reverseArray(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        leftRotateArray(arr, 7, 2);
     }
-    
+
 }
