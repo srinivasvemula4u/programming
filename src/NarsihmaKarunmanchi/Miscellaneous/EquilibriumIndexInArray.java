7cda7a0ea34b548e2586b4083df9a5f1ce396828
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-09 12:56:20 +0530

    Karumanchi miscellaneous

---
+++ src/NarsihmaKarunmanchi/Miscellaneous/EquilibriumIndexInArray.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NarsihmaKarunmanchi.Miscellaneous;
+
+/**
+ *
+ * @author sv186040
+ * Equilibrium index of an array is an index such that the sum of elements at lower indexes
+ * is equal to the sum of elements at higher indexes. Given an n length array, find the K
+ * which makes arr[0]+arr[1]+.....+arr[k-1] = arr[k+1]+....arr[N-1];
+ */
+public class EquilibriumIndexInArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
