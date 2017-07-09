39bd458f31caacafb1eac64159f092a1d2a7e615
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-09 12:07:53 +0530

    Karumanchi miscellaneous

--- src/NarsihmaKarunmanchi/Miscellaneous/MoveSpacesToEndOfTheArray.java
+++ src/NarsihmaKarunmanchi/Miscellaneous/MoveSpacesToEndOfTheArray.java
@@ -14,8 +14,25 @@ public class MoveSpacesToEndOfTheArray {
     /**
      * @param args the command line arguments
      */
+    private static String moveSpacesToEndOptimized(char[] arr) {
+        int len = arr.length - 1;
+        int count = 0;
+        for (int i = 0; i <= len; i++) {
+            if (!Character.isSpaceChar(arr[i])) {
+                arr[count++] = arr[i];
+            }
+        }
+        while (count <= len) {
+            arr[count++] = ' ';
+        }
+        return new String(arr);
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
+        String str = "This is a test";
+        System.out.println(moveSpacesToEndOptimized(str.toCharArray()));
+
     }
-    
+
 }
