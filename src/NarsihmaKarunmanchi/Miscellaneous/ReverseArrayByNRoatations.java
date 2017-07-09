8ac25ff8c11194fd06bc0ae359cf912f1d580af6
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-09 11:39:09 +0530

    Karumanchi miscellaneous

---
+++ src/NarsihmaKarunmanchi/Miscellaneous/ReverseArrayByNRoatations.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NarsihmaKarunmanchi.Miscellaneous;
+
+/**
+ *
+ * @author sv186040
+ * Roatate(arr,d, n);
+ * Reverse(arr,1,d);
+ * Reverse(arr,d+1,n);
+ * 
+ */
+public class ReverseArrayByNRoatations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void leftRotateArray(int[] arr, int n, int d) {
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
