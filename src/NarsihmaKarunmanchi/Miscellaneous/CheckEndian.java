27f225c0626d3807827969236c0328ec4a73790e
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-09 12:53:01 +0530

    Karumanchi miscellaneous

--- src/NarsihmaKarunmanchi/Miscellaneous/CheckEndian.java
+++ src/NarsihmaKarunmanchi/Miscellaneous/CheckEndian.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package NarsihmaKarunmanchi.Miscellaneous;
-
+import java.nio.ByteOrder;
 /**
  *
  * @author sv186040
@@ -14,8 +14,20 @@ public class CheckEndian {
     /**
      * @param args the command line arguments
      */
+    public static boolean isBigEndian() {
+        if(ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN))
+             return true;
+         return false;
+    }
+    public static boolean isLittleEndian() {
+        if(ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN))
+            return true;
+        return false;
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(isBigEndian());
+        System.out.println(isLittleEndian());
     }
     
 }
