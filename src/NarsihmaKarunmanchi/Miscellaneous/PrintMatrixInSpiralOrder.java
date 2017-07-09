7a5f7fe5aa57184605fae6e8f364274d8ea0bdd4
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-09 11:25:50 +0530

    Karumachi miscellaneous

--- src/NarsihmaKarunmanchi/Miscellaneous/PrintMatrixInSpiralOrder.java
+++ src/NarsihmaKarunmanchi/Miscellaneous/PrintMatrixInSpiralOrder.java
@@ -11,7 +11,31 @@ package NarsihmaKarunmanchi.Miscellaneous;
  */
 public class PrintMatrixInSpiralOrder {
 
-    public static void main(String[] args) {
+    public static void spiralOrder(int[][] matrix) {
+        int rowStart = 0, colStart = 0, rowEnd = matrix.length - 1,
+                colEnd = matrix[0].length - 1;
+        while (rowStart <= rowEnd && colStart <= colEnd) {
+            int i = rowStart, j = colStart;
+            for (; j <= colEnd; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            rowStart++;
+            for (i = rowStart,j--; i <= rowEnd; i++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            colEnd--;
+            for (j = colEnd, i--; j >= colStart + 1; j--) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            rowEnd--;
+            for (i = rowEnd,j++; i >= rowStart + 1; i--) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+         int[][] arr = { {1,2,3,4}, {5,6,7,8},{9,10,11,12},{13,14,15,16}};
+         spiralOrder(arr);
     }
 }
