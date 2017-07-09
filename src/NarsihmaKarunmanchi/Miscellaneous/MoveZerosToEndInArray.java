61866b51d6a611c14870b6c76c2e44481858a672
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-09 12:15:30 +0530

    Karumanchi miscellaneous

--- src/NarsihmaKarunmanchi/Miscellaneous/MoveZerosToEndInArray.java
+++ src/NarsihmaKarunmanchi/Miscellaneous/MoveZerosToEndInArray.java
@@ -14,8 +14,23 @@ public class MoveZerosToEndInArray {
     /**
      * @param args the command line arguments
      */
+    private static String moveZerosToEndOptimized(char[] arr) {
+        int len = arr.length - 1;
+        int count = 0;
+        for (int i = 0; i <= len; i++) {
+            if (arr[i] != '0') {
+                arr[count++] = arr[i];
+            }
+        }
+        while (count <= len) {
+            arr[count++] = '0';
+        }
+        return new String(arr);
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+           String str = "19840027060";
+        System.out.println(moveZerosToEndOptimized(str.toCharArray()));
     }
     
 }
