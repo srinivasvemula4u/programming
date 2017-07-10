5e3066d256e95fbb140be316ecbead6c4963a8e2
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-10 20:18:20 +0530

    test

---
+++ src/NarsihmaKarunmanchi/miscellaneousproblems/AddOneToNumber.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NarsihmaKarunmanchi.miscellaneousproblems;
+
+/**
+ *
+ * @author sv186040
+ */
+public class AddOneToNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    public int[] addOneToNumber(int[] digits) {
+        int[] result = new int[digits.length];
+        int one = 1;
+        for(int i = digits.length-1; i >= 0; i--) {
+            result[i] = (digits[i] + one) % 10;
+            one = (digits[i] + one) / 10;
+        }
+        if(one != 0) {
+            int[] more = new int[digits.length + 1];
+            more[0] = one;
+            System.arraycopy(result, 0, more, 1, result.length);
+            result = more;
+        }
+        return result;
+    }
+    
+}
