ef2ce721c3ce9d2c56f88bd398a51ca2a70bcc58
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-18 22:22:22 +0530

    test

--- src/CodeFight/ArcadeUniverse/CenturyFromYear.java
+++ src/CodeFight/ArcadeUniverse/CenturyFromYear.java
@@ -7,7 +7,7 @@ package CodeFight.ArcadeUniverse;
 
 /**
  *
- * @author srivemul
+ * 
  */
 /*
 Given a year, return the century it is in. The first century spans from the year 1 up to and including the year 100, 
