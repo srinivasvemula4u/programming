215db63f60c740f338c7e285e88c6e062e62abb0
Vemula <srinivasvemula.mtech@gmail.com>
2017-07-20 22:35:01 +0530

    test

--- src/NarsihmaKarunmanchi/Miscellaneous/EquilibriumIndexInArray.java
+++ src/NarsihmaKarunmanchi/Miscellaneous/EquilibriumIndexInArray.java
@@ -7,7 +7,7 @@ package NarsihmaKarunmanchi.Miscellaneous;
 
 /**
  *
- * @author sv186040
+ * 
  * Equilibrium index of an array is an index such that the sum of elements at lower indexes
  * is equal to the sum of elements at higher indexes. Given an n length array, find the K
  * which makes arr[0]+arr[1]+.....+arr[k-1] = arr[k+1]+....arr[N-1];
