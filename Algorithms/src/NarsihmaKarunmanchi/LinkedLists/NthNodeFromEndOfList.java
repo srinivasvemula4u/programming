7dcf7807e5b27158d93951c87f9f612bad3eb385
Srinivas <srinivasvemula.mtech@gmail.com>
2018-01-30 22:40:41 +0530

    change

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/NthNodeFromEndOfList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/NthNodeFromEndOfList.java
@@ -10,5 +10,25 @@ package NarsihmaKarunmanchi.LinkedLists;
  * @author sv186040
  */
 public class NthNodeFromEndOfList {
-    
+
+    public LinkedListNode nthNodeFromEndOfList(LinkedListNode head, int N) {
+        LinkedListNode pTemp = head, pNthNode = null;
+        for (int i = 1; i < N; i++) {
+            if (pTemp != null) {
+                pTemp = pTemp.getNext();
+            }
+        }
+        while (pTemp != null) {
+            if (pNthNode == null) {
+                pNthNode = head;
+            } else {
+                pNthNode = pNthNode.getNext();
+            }
+            pTemp = pTemp.getNext();
+        }
+        if (pNthNode != null) {
+            return pNthNode;
+        }
+        return null;
+    }
 }
