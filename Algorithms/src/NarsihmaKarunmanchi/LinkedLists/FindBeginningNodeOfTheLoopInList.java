1fb39ea8fe4502dbafa386df073e545925753ebf
Srinivas <srinivasvemula.mtech@gmail.com>
2018-01-31 21:01:33 +0530

    change

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/FindBeginningNodeOfTheLoopInList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/FindBeginningNodeOfTheLoopInList.java
@@ -10,7 +10,29 @@ package NarsihmaKarunmanchi.LinkedLists;
  * @author sv186040
  */
 public class FindBeginningNodeOfTheLoopInList {
+
     public LinkedListNode findBeginningNodeOfTheLoopInList(LinkedListNode head) {
-        
+        if (head == null) {
+            return null;
+        }
+        LinkedListNode fastPtr = head, slowPtr = head;
+        boolean isLoopExists = false;
+        while (fastPtr.getNext() != null && fastPtr.getNext().getNext() != null) {
+            fastPtr = fastPtr.getNext().getNext();
+            slowPtr = slowPtr.getNext();
+            if (fastPtr == slowPtr) {
+                isLoopExists = true;
+                break;
+            }
+        }
+        if (isLoopExists) {
+            slowPtr = head;
+            while (slowPtr != fastPtr) {
+                slowPtr = slowPtr.getNext();
+                fastPtr = fastPtr.getNext();
+            }
+            return slowPtr;
+        }
+        return null;
     }
 }
