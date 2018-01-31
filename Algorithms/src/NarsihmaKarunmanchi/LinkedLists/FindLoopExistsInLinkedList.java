0715e7fb13155519830ef3b1501314ff8890acb2
Srinivas <srinivasvemula.mtech@gmail.com>
2018-01-31 20:49:56 +0530

    change

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/FindLoopExistsInLinkedList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/FindLoopExistsInLinkedList.java
@@ -10,5 +10,16 @@ package NarsihmaKarunmanchi.LinkedLists;
  * @author sv186040
  */
 public class FindLoopExistsInLinkedList {
-    
+
+    public boolean findLoopExistOrNotInList(LinkedListNode head) {
+        LinkedListNode fastPtr = head, slowPtr = head;
+        while (fastPtr.getNext() != null && fastPtr.getNext().getNext() != null) {
+            fastPtr = fastPtr.getNext().getNext();
+            slowPtr = slowPtr.getNext();
+            if (fastPtr == slowPtr) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
