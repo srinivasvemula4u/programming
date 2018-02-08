c1d79b9df29c4848a55655535513126fa062d206
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-08 18:14:36 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/FindMiddleNodeInList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/FindMiddleNodeInList.java
@@ -10,7 +10,21 @@ package NarsihmaKarunmanchi.LinkedLists;
  * @author sv186040
  */
 public class FindMiddleNodeInList {
+
     public static LinkedListNode findMiddleNodeInList(LinkedListNode head) {
-        return null;
+        LinkedListNode fastPtr = head, slowPtr = head;
+        int index = 0;
+        while (fastPtr.getNext() != null) {
+            if (index == 0) {
+                fastPtr=fastPtr.getNext();
+                index = 1;
+            } else if(index == 1)
+            {
+                fastPtr = fastPtr.getNext();
+                slowPtr = slowPtr.getNext();
+                index = 0;
+            }
+        }
+        return slowPtr;
     }
 }
