2bb98c091847a3b68dd7a7380e343243f313129a
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-08 18:41:01 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/MergeTwoSortedLinkedLists.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/MergeTwoSortedLinkedLists.java
@@ -10,10 +10,25 @@ package NarsihmaKarunmanchi.LinkedLists;
  * @author sv186040
  */
 public class MergeTwoSortedLinkedLists {
-    public static LinkedListNode mergeTwoSortedLinkedList(LinkedList head1, LinkedListNode head2) {
-        return null;
+
+    public static LinkedListNode mergeTwoSortedLinkedList(LinkedListNode head1, LinkedListNode head2) {
+        if (head1 == null) {
+            return head1;
+        }
+        if (head2 == null) {
+            return head2;
+        }
+        if (head1.getData() < head2.getData()) {
+            head1.setNext(mergeTwoSortedLinkedList(head1, head2.getNext()));
+            return head1;
+        } else {
+            head2.setNext(mergeTwoSortedLinkedList(head1.getNext(), head2));
+            return head2;
+        }
+        //return null;
     }
+
     public static void main(String[] args) {
-        
+
     }
 }
