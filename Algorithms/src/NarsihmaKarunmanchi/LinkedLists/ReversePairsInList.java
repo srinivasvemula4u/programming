7a205e55ef35b0d79f405b97ec867395eac26780
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-10 10:22:36 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/ReversePairsInList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/ReversePairsInList.java
@@ -7,7 +7,7 @@ package NarsihmaKarunmanchi.LinkedLists;
 
 /**
  *
- * @author sv186040
+ *  Working correctly
  */
 public class ReversePairsInList {
 
@@ -19,14 +19,26 @@ public class ReversePairsInList {
         list.insert(13, 3);
         list.insert(14, 4);
         list.insert(15, 5);
+        list.insert(16, 6);
+        LinkedListNode head = reversePairsInListRecursive(list.getHead());
+        // LinkedListNode head = reversePairsInList(list.getHead());
+        while (head != null) {
+            System.out.println(head.getData());
+            head = head.getNext();
+        }
     }
 
     public static LinkedListNode reversePairsInListRecursive(LinkedListNode head) {
         if (head == null) {
             return null;
         }
+        if (head.getNext() == null) {
+            return head;
+        }
         LinkedListNode temp = null;
         temp = head.getNext();
+        // if(temp == null)
+        //    return null;
         head.setNext(temp.getNext());
         temp.setNext(head);
         head = temp;
@@ -37,6 +49,9 @@ public class ReversePairsInList {
     public static LinkedListNode reversePairsInList(LinkedListNode head) {
         LinkedListNode temp1 = null, temp2 = null;
         while (head != null && head.getNext() != null) {
+            if (temp1 != null) {
+                temp1.getNext().setNext(head.getNext());
+            }
             temp1 = head.getNext();
             head.setNext(temp1.getNext());
             temp1.setNext(head);
