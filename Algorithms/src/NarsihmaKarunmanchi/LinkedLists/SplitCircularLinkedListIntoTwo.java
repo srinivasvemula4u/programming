8b1851b36c96b3ac58b8103eacc7aaaaebdaf5c9
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-10 11:31:35 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/SplitCircularLinkedListIntoTwo.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/SplitCircularLinkedListIntoTwo.java
@@ -8,11 +8,25 @@ package NarsihmaKarunmanchi.LinkedLists;
 /**
  *
  * @author sv186040 1) Store the mid and last pointers of the circular linked
- * list using Floyd cycle finding algorithm. 
- * 2) Make the second half circular 
- * 3)Make the first half circular 
- * 4) Set head pointers of the two linked lists
+ * list using Floyd cycle finding algorithm. 2) Make the second half circular
+ * 3)Make the first half circular 4) Set head pointers of the two linked lists
  */
 public class SplitCircularLinkedListIntoTwo {
 
+    public static void splitListIntoTwo(LinkedListNode head, LinkedListNode head1, LinkedListNode head2) {
+        LinkedListNode fastPtr = head, slowPtr = head;
+        while (fastPtr.getNext() != head && fastPtr.getNext().getNext() != head) {
+            fastPtr = fastPtr.getNext().getNext();
+            slowPtr = slowPtr.getNext();
+        }
+        if (fastPtr.getNext().getNext() == head) {
+            fastPtr = fastPtr.getNext();
+        }
+        if (slowPtr.getNext() != head) {
+            head2 = slowPtr.getNext();
+        }
+        fastPtr.setNext(head2);
+        head1 = head;
+        slowPtr.setNext(head1);
+    }
 }
