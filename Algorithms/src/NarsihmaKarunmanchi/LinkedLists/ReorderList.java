24336f8beeb6a93c8cca873f7892dc75863bb533
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-12 23:26:35 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/ReorderList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/ReorderList.java
@@ -9,6 +9,9 @@ package NarsihmaKarunmanchi.LinkedLists;
  *
  * @author sv186040 Given a singly linked list L:l1->l2->l3....->ln-1->ln
  * reorder it to l1->ln->l2->ln-1...
+ *  Divide the list into two parts using fastptr and slowptr
+ *  Reverse the second list using stack
+ *  Traverse second list node by node and insert at first list alternatively
  */
 public class ReorderList {
 
