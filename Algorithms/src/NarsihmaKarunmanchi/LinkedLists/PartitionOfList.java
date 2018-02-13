43cb56b3f872a4d99ff801a19835770a141ff300
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-13 23:23:05 +0530

    mission

---
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/PartitionOfList.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NarsihmaKarunmanchi.LinkedLists;
+
+/**
+ *
+ * @author sv186040 
+ * Given a linked list and a value K, Partition it such that
+ * all nodes less than K come before nodes greater than or equal to K. You
+ * should Preserve the original relative order of the nodes in each of the two
+ * partitions. For example, given 1->4->3->2->5->2 and K =3 return
+ * 1->2->2->4->3->5
+ */
+public class PartitionOfList {
+
+}
