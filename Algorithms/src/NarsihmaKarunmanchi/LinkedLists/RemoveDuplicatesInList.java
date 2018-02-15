12fc9f24fe189635f175ffefb9f93b2db2f20802
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-16 00:15:11 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/RemoveDuplicatesInList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/RemoveDuplicatesInList.java
@@ -15,20 +15,34 @@ import java.util.Map;
 public class RemoveDuplicatesInList {
 
     public static LinkedListNode removeDuplicatesInList(LinkedListNode head) {
-        LinkedListNode current = head;
+        LinkedListNode current = head, prev = null;
         Map<Integer, Boolean> map = new HashMap<>();
         while (current != null) {
             if (map.get(current.getData()) == null) {
                 map.put(current.getData(), Boolean.TRUE);
-                current = current.getNext();
+                prev = current;
+
             } else {
-                current.setNext(current.getNext().getNext());
+                prev.setNext(current.getNext());
             }
+            current = current.getNext();
         }
         return head;
     }
 
     public static void main(String[] args) {
-
+        LinkedList list = new LinkedList();
+        list.insert(1, 0);
+        list.insert(5, 1);
+        list.insert(4, 1);
+        list.insert(3, 2);
+        list.insert(2, 3);
+        list.insert(5, 4);
+        list.insert(2, 5);
+        LinkedListNode head = removeDuplicatesInList(list.getHead());
+        while (head != null) {
+            System.out.println(head.getData());
+            head = head.getNext();
+        }
     }
 }
