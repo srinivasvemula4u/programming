a8461fa14415c5074455bf596e889f896e1e14e6
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-16 00:10:51 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/RemoveDuplicatesInList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/RemoveDuplicatesInList.java
@@ -5,14 +5,27 @@
  */
 package NarsihmaKarunmanchi.LinkedLists;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author sv186040
  */
 public class RemoveDuplicatesInList {
 
-    public static void removeDuplicatesInList(LinkedListNode head) {
-       
+    public static LinkedListNode removeDuplicatesInList(LinkedListNode head) {
+        LinkedListNode current = head;
+        Map<Integer, Boolean> map = new HashMap<>();
+        while (current != null) {
+            if (map.get(current.getData()) == null) {
+                map.put(current.getData(), Boolean.TRUE);
+                current = current.getNext();
+            } else {
+                current.setNext(current.getNext().getNext());
+            }
+        }
+        return head;
     }
 
     public static void main(String[] args) {
