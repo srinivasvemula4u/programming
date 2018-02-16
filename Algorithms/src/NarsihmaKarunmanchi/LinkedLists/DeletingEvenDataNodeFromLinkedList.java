e75e99340d3c3b6816560264362c8a37fea3ef54
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-16 23:05:22 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/LinkedLists/DeletingEvenDataNodeFromLinkedList.java
+++ Algorithms/src/NarsihmaKarunmanchi/LinkedLists/DeletingEvenDataNodeFromLinkedList.java
@@ -40,6 +40,18 @@ public class DeletingEvenDataNodeFromLinkedList {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        LinkedList list = new LinkedList();
+        list.insert(10, 0);
+        list.insert(11, 1);
+        list.insert(12, 2);
+        list.insert(13, 3);
+        list.insert(14, 4);
+        list.insert(15, 5);
+        LinkedListNode head = deleteEven(list.getHead());
+        while (head != null) {
+            System.out.println(head.getData());
+            head = head.getNext();
+        }
     }
 
 }
