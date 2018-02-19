4d2e357eeecf473ffb9c46eb931d9a9bec09b022
Srinivas <srinivasvemula.mtech@gmail.com>
2018-02-19 22:57:40 +0530

    mission

--- Algorithms/src/NarsihmaKarunmanchi/Stacks/IsValidSymbolPattern.java
+++ Algorithms/src/NarsihmaKarunmanchi/Stacks/IsValidSymbolPattern.java
@@ -48,4 +48,7 @@ public class IsValidSymbolPattern {
             return false;
         }
     }
+    public static void main(String[] args) {
+        System.out.println(isValidSymbolPattern("{}[{()}]}"));
+    }
 }
