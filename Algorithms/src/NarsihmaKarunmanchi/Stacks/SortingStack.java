92878bd38ece55ac58221bb80748427edfecbf0d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-02-27 20:59:08 +0530

    message

--- Algorithms/src/NarsihmaKarunmanchi/Stacks/SortingStack.java
+++ Algorithms/src/NarsihmaKarunmanchi/Stacks/SortingStack.java
@@ -9,10 +9,24 @@ import java.util.Stack;
 
 /**
  *
- * @author sv186040
+ * @author sv186040 Sorting stack in ascending order
  */
 public class SortingStack {
+
     public static Stack<Integer> sortingStack(Stack<Integer> stk) {
-        return null;
+        // return null;
+        Stack<Integer> rstk = new Stack<>();
+        while (!stk.isEmpty()) {
+            int temp = stk.pop();
+            while (!rstk.isEmpty() && rstk.peek() > temp) {
+                stk.push(rstk.pop());
+            }
+            rstk.push(temp);
+        }
+        return rstk;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
