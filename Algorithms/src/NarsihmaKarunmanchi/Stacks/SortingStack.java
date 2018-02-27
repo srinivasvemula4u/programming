b74c6af952ace14d668f35b9fa47ee18eb6bf6dd
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-02-27 21:03:56 +0530

    message

--- Algorithms/src/NarsihmaKarunmanchi/Stacks/SortingStack.java
+++ Algorithms/src/NarsihmaKarunmanchi/Stacks/SortingStack.java
@@ -27,6 +27,13 @@ public class SortingStack {
     }
 
     public static void main(String[] args) {
-
+        Stack<Integer> stack = new Stack<>();
+        stack.push(2);
+        stack.push(3);
+        stack.push(1);
+        stack.push(5);
+        stack.push(4);
+        System.out.println("before sort" + stack.toString());
+        System.out.println("After sort " + sortingStack(stack).toString());
     }
 }
