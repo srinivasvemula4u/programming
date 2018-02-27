2f5823864d75005e07010f2c453c5b596cad9623
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-02-27 20:55:28 +0530

    message

---
+++ Algorithms/src/NarsihmaKarunmanchi/Stacks/SortingStack.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package NarsihmaKarunmanchi.Stacks;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author sv186040
+ */
+public class SortingStack {
+    public static Stack<Integer> sortingStack(Stack<Integer> stk) {
+        return null;
+    }
+}
