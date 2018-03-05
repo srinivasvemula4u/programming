1fd92fff38cb1ea68ae7dfa2200d478d3ec11907
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-05 23:19:25 +0530

    message

--- Algorithms/src/NarsihmaKarunmanchi/Queues/LinkedListQueue.java
+++ Algorithms/src/NarsihmaKarunmanchi/Queues/LinkedListQueue.java
@@ -5,10 +5,53 @@
  */
 package NarsihmaKarunmanchi.Queues;
 
+import NarsihmaKarunmanchi.LinkedLists.LinkedListNode;
+
 /**
  *
  * @author sv186040
  */
 public class LinkedListQueue {
-    
+
+    private int length;
+    private LinkedListNode front, rear;
+
+    public LinkedListQueue() {
+        length = 0;
+        front = null;
+        rear = null;
+    }
+
+    public void enqueue(int data) {
+        LinkedListNode node = new LinkedListNode(data);
+        if (isEmpty()) {
+            front = node;
+            //rear = node;
+        } else {
+            rear.setNext(node);
+
+        }
+        rear = node;//
+    }
+
+    public int dequeue() {
+        if (isEmpty()) {
+            return -1;
+        }
+        int result = front.getData();
+        front = front.getNext();
+        length--;
+        if (isEmpty()) {
+            rear = null;
+        }
+        return result;
+    }
+
+    public int size() {
+        return length;
+    }
+
+    private boolean isEmpty() {
+        return (length == 0);
+    }
 }
