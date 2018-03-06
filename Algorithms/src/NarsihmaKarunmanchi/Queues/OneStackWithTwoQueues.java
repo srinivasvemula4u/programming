208dba090d3c891012751510814771354ae69a99
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-06 08:25:13 +0530

    message

--- Algorithms/src/NarsihmaKarunmanchi/Queues/OneStackWithTwoQueues.java
+++ Algorithms/src/NarsihmaKarunmanchi/Queues/OneStackWithTwoQueues.java
@@ -13,48 +13,46 @@ import java.util.Queue;
  * @author srivemul
  */
 public class OneStackWithTwoQueues {
+
     private Queue<Integer> Q1 = new LinkedList<Integer>();
     private Queue<Integer> Q2 = new LinkedList<Integer>();
-    public void push(int data)
-    {
-        if(Q1.isEmpty())
+
+    public void push(int data) {
+        if (Q1.isEmpty()) {
             Q2.offer(data);
-        else Q1.offer(data);
+        } else {
+            Q1.offer(data);
+        }
     }
-    public int pop()
-    {
-        int i= 0,size;
-        if(Q2.isEmpty())
-        {
+
+    public int pop() {
+        int i = 0, size;
+        if (Q2.isEmpty()) {
             size = Q1.size();
-            while(i < size - 1)
-            {
+            while (i < size - 1) {
                 Q2.offer(Q1.poll());
                 i++;
             }
             return Q1.poll();
-        }
-        else 
-        {
+        } else {
             size = Q2.size();
-            while(i < size -1) 
-            {
+            while (i < size - 1) {
                 Q1.offer(Q2.poll());
                 i++;
             }
             return Q2.poll();
         }
     }
-      public static void main(String args[])
-    {
+
+    public static void main(String args[]) {
         OneStackWithTwoQueues obj = new OneStackWithTwoQueues();
         obj.push(10);
-         obj.push(6);
-          obj.push(4);
-           obj.push(2);
-           System.out.println(obj.pop());
-           System.out.println(obj.pop());
-           System.out.println(obj.pop());
-           System.out.println(obj.pop());
+        obj.push(6);
+        obj.push(4);
+        obj.push(2);
+        System.out.println(obj.pop());
+        System.out.println(obj.pop());
+        System.out.println(obj.pop());
+        System.out.println(obj.pop());
     }
 }
