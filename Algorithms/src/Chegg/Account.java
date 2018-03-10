8edd1a4681cde27694b22a66cc017a99f6bc6250
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-10 10:37:23 +0530

    ..

--- Algorithms/src/Chegg/Account.java
+++ Algorithms/src/Chegg/Account.java
@@ -7,35 +7,44 @@ package Chegg;
 
 /**
  *
- * 
+ *
  */
 public class Account {
+
     private String type;
     private String number;
     private Double balance;
+
     public Account() {
-        
+
     }
+
     public String getType() {
         return this.type;
     }
+
     public void setType(String pTyp) {
         this.type = pTyp;
     }
+
     public String getNumber() {
         return this.number;
     }
+
     public void setNumber(String pNbr) {
         this.number = pNbr;
     }
+
     public Double getBalance() {
         return this.balance;
     }
+
     public void setBalance(Double pBal) {
         this.balance = pBal;
     }
+
     public String toString() {
-        return "Account type is" + getType() + ",\n its number is "+ getNumber()
-                + "\n and  its  balance is "+ getBalance();
+        return "Account type is" + getType() + ",\n its number is " + getNumber()
+                + "\n and  its  balance is " + getBalance();
     }
 }
