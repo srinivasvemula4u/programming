1413d00328d99ce68be86b0b9ebd8a1675b0ddb6
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-11 23:11:30 +0530

    ..

--- Algorithms/src/Chegg/Business.java
+++ Algorithms/src/Chegg/Business.java
@@ -7,35 +7,44 @@ package Chegg;
 
 /**
  *
- * 
+ *
  */
 public class Business {
+
     private String name;
     private String type;
     private Account acct;
+
     public Business() {
-        
+
     }
+
     public String getName() {
         return this.name;
     }
+
     public void setName(String pName) {
         this.name = pName;
     }
+
     public String getType() {
         return this.type;
-    } 
+    }
+
     public void setType(String pType) {
         this.type = pType;
     }
+
     public Account getAcct() {
         return this.acct;
     }
+
     public void setAcct(Account pAcct) {
         this.acct = pAcct;
     }
+
     public String toString() {
-        return "Business name is " + getName() + ", its type is "+ getType() + " and its account details are "
+        return "Business name is " + getName() + ", its type is " + getType() + " and its account details are "
                 + getAcct().toString();
     }
 }
