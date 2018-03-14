36e4b941a2c687d11f2a24d89a5d46ff337389c8
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-14 22:59:17 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/Solution.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/Solution.java
@@ -9,34 +9,39 @@ import java.io.*;
 import java.util.*;
 
 class Node {
-	int data;
-	Node next;
-	Node(int d) {
+
+    int data;
+    Node next;
+
+    Node(int d) {
         data = d;
         next = null;
     }
 }
 
 class Solution {
-    public static  Node insert(Node head,int data) {
+
+    public static Node insert(Node head, int data) {
         //Complete this method
-        
+
         Node temp = null;
-        if(head.next == null) {
-           temp = new Node(data);
-           head = temp;
-           return head;
+        if (head == null) {
+            temp = new Node(data);
+            head = temp;
+            return head;
         }
         Node current = head;
-        while(current.next != null) 
+        while (current.next != null) {
             current = current.next;
+        }
         temp = new Node(data);
         current.next = temp;
         return head;
     }
+
     public static void display(Node head) {
         Node start = head;
-        while(start != null) {
+        while (start != null) {
             System.out.print(start.data + " ");
             start = start.next;
         }
@@ -47,12 +52,11 @@ class Solution {
         Node head = null;
         int N = sc.nextInt();
 
-        while(N-- > 0) {
+        while (N-- > 0) {
             int ele = sc.nextInt();
-            head = insert(head,ele);
+            head = insert(head, ele);
         }
         display(head);
         sc.close();
     }
 }
-
