d72cdc718e5068a14534ad203b6343b02ee5cd39
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-19 20:44:22 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/WidthOfBinaryTree.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/WidthOfBinaryTree.java
@@ -12,30 +12,33 @@ import static NarsihmaKarunmanchi.Trees.FindingHeightOfTree.findingHeightOfTree;
  * @author srivemul
  */
 public class WidthOfBinaryTree {
-    public static int widthOfBinaryTree(BinaryTreeNode root)
-    {
-       int max = 0;
-       int height = findingHeightOfTree(root);
-       for(int i=0; i< height; i++)
-       {
-           int temp = width(root,i);
-           if(temp > max) max = temp;
-       }
-       return max;
+
+    public static int widthOfBinaryTree(BinaryTreeNode root) {
+        int max = 0;
+        int height = findingHeightOfTree(root);
+        for (int i = 0; i < height; i++) {
+            int temp = width(root, i);
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return max;
     }
-    public static int width(BinaryTreeNode root, int depth)
-    {
-       if(root == null)
-           return 0;
-       else  {
-           if(depth == 0)
-           return 1;
-           else
-               return width(root.getLeft(), depth-1)+width(root.getRight(), depth-1);
-       }
-                 
+
+    public static int width(BinaryTreeNode root, int depth) {
+        if (root == null) {
+            return 0;
+        } else {
+            if (depth == 0) {
+                return 1;
+            } else {
+                return width(root.getLeft(), depth - 1) + width(root.getRight(), depth - 1);
+            }
+        }
+
     }
-     public static void main(String args[]) {
+
+    public static void main(String args[]) {
         BinaryTree tree = new BinaryTree();
         tree.insert(1);
         tree.insert(2);
