7ac949bfa67fed620301af2ee67ebe31c99e3705
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-22 08:29:42 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Arrays/CountNumberOfOccurrences.java
+++ Algorithms/src/GeeksForGeeks/Arrays/CountNumberOfOccurrences.java
@@ -16,60 +16,58 @@ public class CountNumberOfOccurrences {
      */
     public static void main(String[] args) {
         // TODO code application logic hereint 
-        int[] arr = {1, 2, 2, 2, 2,2,2, 3, 4,4, 7 ,8 ,8 };
+        int[] arr = {1, 2, 2, 2, 2, 2, 2, 3, 4, 4, 7, 8, 8};
         int x = 2;
-        countInArray(arr,x);
+        countInArray(arr, x);
         countInArrayMethod2(arr, x);
-        
+
     }
 
-    private static void countInArray(int[] arr,int x) {
+    private static void countInArray(int[] arr, int x) {
         int count = 0;
-        for(int i=0; i < arr.length; i++ )
-            if(x == arr[i])
+        for (int i = 0; i < arr.length; i++) {
+            if (x == arr[i]) {
                 count++;
+            }
+        }
         System.out.println(count);
     }
-    private static void countInArrayMethod2(int[] arr,int x) {
+
+    private static void countInArrayMethod2(int[] arr, int x) {
         int n = arr.length;
-        int i = first(arr,0,n-1,x,n);
-        if( i == -1)
+        int i = first(arr, 0, n - 1, x, n);
+        if (i == -1) {
             return;
-        int j = last(arr,i,n-1,x,n);
-        System.out.println(j-i+1);
+        }
+        int j = last(arr, i, n - 1, x, n);
+        System.out.println(j - i + 1);
     }
 
-    private static int first(int[] arr, int low, int high, int x,int n) {
-        if(high >= low) {
-            int mid = (low + high) / 2; 
-            if(((mid == 0) || x > arr[mid-1]) && arr[mid] == x)
+    private static int first(int[] arr, int low, int high, int x, int n) {
+        if (high >= low) {
+            int mid = (low + high) / 2;
+            if (((mid == 0) || x > arr[mid - 1]) && arr[mid] == x) {
                 return mid;
-            else if(x > arr[mid] ) {
-                return first(arr,mid+1,high,x,n); 
-            }
-            else {
-                return first(arr, low, mid-1, x,n);
+            } else if (x > arr[mid]) {
+                return first(arr, mid + 1, high, x, n);
+            } else {
+                return first(arr, low, mid - 1, x, n);
             }
         }
         return -1;
     }
 
-    private static int last(int[] arr, int low, int high, int x,int n) {
-     if(high >= low) {
-            int mid = (low + high) / 2; 
-            if(((mid == n-1) || x < arr[mid+1]) && arr[mid] == x)
+    private static int last(int[] arr, int low, int high, int x, int n) {
+        if (high >= low) {
+            int mid = (low + high) / 2;
+            if (((mid == n - 1) || x < arr[mid + 1]) && arr[mid] == x) {
                 return mid;
-            else if(x < arr[mid] ) {
-                return last(arr, low, mid-1, x,n);
-            }
-            else {
-                return last(arr,mid+1,high,x,n);
+            } else if (x < arr[mid]) {
+                return last(arr, low, mid - 1, x, n);
+            } else {
+                return last(arr, mid + 1, high, x, n);
             }
         }
         return -1;
     }
 }
-       
-    
-    
-
