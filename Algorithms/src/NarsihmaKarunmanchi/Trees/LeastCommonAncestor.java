c73828538bdfd33aa43cd00b9af1246952a89bdc
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-03-31 18:55:10 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/LeastCommonAncestor.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/LeastCommonAncestor.java
@@ -10,19 +10,24 @@ package NarsihmaKarunmanchi.Trees;
  * @author srivemul
  */
 public class LeastCommonAncestor {
+
     public static BinaryTreeNode leastCommonAncestor(BinaryTreeNode root, BinaryTreeNode node1, BinaryTreeNode node2) {
         BinaryTreeNode left = null, right = null;
-        if(root == null)
+        if (root == null) {
             return root;
-        if(root == node1 || root == node2)
+        }
+        if (root == node1 || root == node2) {
             return root;
+        }
         left = leastCommonAncestor(root.getLeft(), node1, node2);
         right = leastCommonAncestor(root.getRight(), node1, node2);
-        if(left != null && right != null)
+        if (left != null && right != null) {
             return root;
-        else
+        } else {
             return left != null ? left : right;
+        }
     }
+
     public static void main(String args[]) {
         BinaryTree tree = new BinaryTree();
         tree.insert(1);
@@ -32,8 +37,9 @@ public class LeastCommonAncestor {
         tree.insert(5);
         tree.insert(6);
         tree.insert(7);
-        BinaryTreeNode resultNode = leastCommonAncestor(tree.getRoot(), tree.getRoot().getLeft().getLeft(),tree.getRoot().getLeft().getRight()); 
-        if(resultNode != null)
+        BinaryTreeNode resultNode = leastCommonAncestor(tree.getRoot(), tree.getRoot().getLeft().getLeft(), tree.getRoot().getLeft().getRight());
+        if (resultNode != null) {
             System.out.println(resultNode.getData());
+        }
     }
 }
