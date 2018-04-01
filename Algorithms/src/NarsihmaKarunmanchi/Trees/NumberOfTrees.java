7ce768d7e4ed51e698f229b21371d673ca8bbf22
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-01 20:55:59 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/NumberOfTrees.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/NumberOfTrees.java
@@ -10,20 +10,21 @@ package NarsihmaKarunmanchi.Trees;
  * @author srivemul
  */
 public class NumberOfTrees {
-    public static int numberOfTrees(int n)
-    {
-        int[] count = new int[n+1];
+
+    public static int numberOfTrees(int n) {
+        int[] count = new int[n + 1];
         count[0] = 1;
         count[1] = 1;
-        for(int i=2; i<=n; i++) 
-        {
-            for(int j=0; j < i; j++)
-                count[i] += count[j]*count[i-j-1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                count[i] += count[j] * count[i - j - 1];
+            }
         }
         return count[n];
     }
-     public static void main(String args[]) {
-      
-         System.out.println(numberOfTrees(3));
-     }
+
+    public static void main(String args[]) {
+
+        System.out.println(numberOfTrees(3));
+    }
 }
