d9f1856ea0dfbd0f7b1eeffe6314402ab9966af7
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-01 21:09:00 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/BuildBinaryTreeFromPreOrder.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/BuildBinaryTreeFromPreOrder.java
@@ -10,23 +10,27 @@ package NarsihmaKarunmanchi.Trees;
  * @author srivemul
  */
 public class BuildBinaryTreeFromPreOrder {
-    public static BinaryTreeNode buildBinaryTreeFromPreOrder(char[] string,int index)
-    {
-        if(string == null)
+
+    public static BinaryTreeNode buildBinaryTreeFromPreOrder(char[] string, int index) {
+        if (string == null) {
             return null;
-        if(string.length == index)
+        }
+        if (string.length == index) {
             return null;
-        BinaryTreeNode root =new BinaryTreeNode(string[index]);
-        if((char)root.getData() == 'L')
+        }
+        BinaryTreeNode root = new BinaryTreeNode(string[index]);
+        if ((char) root.getData() == 'L') {
             return root;
+        }
         index = index + 1;
-        root.setLeft(buildBinaryTreeFromPreOrder(string,index));
+        root.setLeft(buildBinaryTreeFromPreOrder(string, index));
         index = index + 1;
-        root.setRight(buildBinaryTreeFromPreOrder(string,index));
+        root.setRight(buildBinaryTreeFromPreOrder(string, index));
         return root;
     }
+
     public static void main(String args[]) {
-        BinaryTreeNode root = buildBinaryTreeFromPreOrder(new char[]{'I','L','I','L','L'}, 0);
+        BinaryTreeNode root = buildBinaryTreeFromPreOrder(new char[]{'I', 'L', 'I', 'L', 'L'}, 0);
         TreeTravels.inOrderTraversal(root);
     }
 }
