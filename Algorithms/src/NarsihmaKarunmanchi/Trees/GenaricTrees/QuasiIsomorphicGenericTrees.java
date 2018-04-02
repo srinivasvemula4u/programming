7ceef08a36964ab7e5c58c347e8c7f3491ff1de0
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-02 08:06:19 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/GenaricTrees/QuasiIsomorphicGenericTrees.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/GenaricTrees/QuasiIsomorphicGenericTrees.java
@@ -14,15 +14,17 @@ import NarsihmaKarunmanchi.Trees.BinaryTreeNode;
 /*
 Two tress root1 and root2 are qusai isomorphic if root1 cane be transformed into root2 by swapping left 
 and right children of some of the nodes of root1. Data in the nodes are not imo
-*/
+ */
 public class QuasiIsomorphicGenericTrees {
-    public static boolean quasiIsomorphicGenericTrees(BinaryTreeNode root1, BinaryTreeNode root2)
-    {
-        if(root1 == null && root2 == null)
+
+    public static boolean quasiIsomorphicGenericTrees(BinaryTreeNode root1, BinaryTreeNode root2) {
+        if (root1 == null && root2 == null) {
             return true;
-        if((root1 == null && root2 != null) ||(root1 != null && root2 == null))
+        }
+        if ((root1 == null && root2 != null) || (root1 != null && root2 == null)) {
             return false;
-        return (quasiIsomorphicGenericTrees(root1.getLeft(), root2.getLeft())&&quasiIsomorphicGenericTrees(root1.getRight(), root2.getRight()))
-                || (quasiIsomorphicGenericTrees(root1.getLeft(), root2.getRight()) && quasiIsomorphicGenericTrees(root1.getRight(), root2.getLeft())); 
+        }
+        return (quasiIsomorphicGenericTrees(root1.getLeft(), root2.getLeft()) && quasiIsomorphicGenericTrees(root1.getRight(), root2.getRight()))
+                || (quasiIsomorphicGenericTrees(root1.getLeft(), root2.getRight()) && quasiIsomorphicGenericTrees(root1.getRight(), root2.getLeft()));
     }
 }
