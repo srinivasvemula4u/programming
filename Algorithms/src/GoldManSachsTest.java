8e9202c3a66a867e8351049915201a5514c20cfc
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-03 22:08:51 +0530

    ..

--- Algorithms/src/GoldManSachsTest.java
+++ Algorithms/src/GoldManSachsTest.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author srivemul
@@ -61,49 +60,52 @@ public class GoldManSachsTest {
 //            return -1;
 //
 //    }
-     static int findDuplicates(int[][] data, int operation, int numOfDuplicates) {
-    
+    static int findDuplicates(int[][] data, int operation, int numOfDuplicates) {
+
         int rows = data.length;
         int cols = data[0].length;
         int numOfRows = 0;
-        HashMap<Integer,Integer> map = new HashMap();
-        if( 1 == operation) {
-            for(int i=0; i < rows; i++) {
+        HashMap<Integer, Integer> map = new HashMap();
+        if (1 == operation) {
+            for (int i = 0; i < rows; i++) {
                 int dupCount = 0;
-                for(int j=0; j < cols; j++) {
-                    if(map.get(data[i][j]) != null)
+                for (int j = 0; j < cols; j++) {
+                    if (map.get(data[i][j]) != null) {
                         dupCount++;
-                    else 
-                        map.put(data[i][j],data[i][j]);
+                    } else {
+                        map.put(data[i][j], data[i][j]);
+                    }
                 }
-                if(dupCount >= numOfDuplicates)
+                if (dupCount >= numOfDuplicates) {
                     numOfRows++;
-                map.clear();
-            }
-            return numOfRows;
-        }
-        else if(2 == operation) {
-           int colCount = 0;int minRowCount = 0;
-           for(int i=0; i < cols; i++) {
-                int dupCount = 0;
-                for(int j=0; j < rows; j++) {
-                    if(map.get(data[j][i]) != null)
-                        dupCount++;
-                    else 
-                        map.put(data[j][i],data[j][i]);
                 }
-                 if(dupCount > 0 && dupCount >= numOfDuplicates)  {
-                     colCount++;
-                     if(dupCount > minRowCount && dupCount >= numOfDuplicates) {
-                         minRowCount = dupCount+1;
-                     }
-                 }
                 map.clear();
             }
-              numOfRows = Math.max(colCount,minRowCount);
+            return numOfRows;
+        } else if (2 == operation) {
+            int colCount = 0;
+            int minRowCount = 0;
+            for (int i = 0; i < cols; i++) {
+                int dupCount = 0;
+                for (int j = 0; j < rows; j++) {
+                    if (map.get(data[j][i]) != null) {
+                        dupCount++;
+                    } else {
+                        map.put(data[j][i], data[j][i]);
+                    }
+                }
+                if (dupCount > 0 && dupCount >= numOfDuplicates) {
+                    colCount++;
+                    if (dupCount > minRowCount && dupCount >= numOfDuplicates) {
+                        minRowCount = dupCount + 1;
+                    }
+                }
+                map.clear();
+            }
+            numOfRows = Math.max(colCount, minRowCount);
             return numOfRows;
         }
-            return -1;
+        return -1;
 
     }
 
@@ -149,12 +151,11 @@ public class GoldManSachsTest {
 //            return -1;
 //
 //    }
-
     public static void main(String[] args) {
         // TODO code application logic here
-        int[][] data = { {1,3,5,9},{1,2,1,2},{1,4,7,9},{3,5,9,11}};
-        System.out.println(findDuplicates(data, 2,2));
-        
+        int[][] data = {{1, 3, 5, 9}, {1, 2, 1, 2}, {1, 4, 7, 9}, {3, 5, 9, 11}};
+        System.out.println(findDuplicates(data, 2, 2));
+
     }
-    
+
 }
