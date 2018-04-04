78bd38d86b998687702590deb8a7e1b6b8c03203
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-04 07:32:46 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/BinarySearchTrees/IsBSTRightWay.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/BinarySearchTrees/IsBSTRightWay.java
@@ -10,23 +10,31 @@ package NarsihmaKarunmanchi.Trees.BinarySearchTrees;
  * @author srivemul
  */
 public class IsBSTRightWay {
+
     public static boolean isBST(BinarySearchTreeNode root) {
-       if(root == null)
-           return true;
-       if(root.getLeft() != null && FindingMaximumElementInBST.maximum(root.getLeft()).getData() > root.getData())
-           return false;
-       if(root.getRight() != null && FindingMinimumElementInBST.minimum(root.getRight()).getData() < root.getData())
-           return false;
-       if(!isBST(root.getLeft()) || !isBST(root.getRight()))
-           return false;
-       return true;
-    }
-    public static boolean isBSTBestWay(BinarySearchTreeNode root, int min, int max) {
-        if(root == null)
+        if (root == null) {
             return true;
-        return (root.getData() > min && root.getData() < max) && isBSTBestWay(root.getLeft(), min, root.getData()) && isBSTBestWay(root.getRight(),root.getData(),max);
+        }
+        if (root.getLeft() != null && FindingMaximumElementInBST.maximum(root.getLeft()).getData() > root.getData()) {
+            return false;
+        }
+        if (root.getRight() != null && FindingMinimumElementInBST.minimum(root.getRight()).getData() < root.getData()) {
+            return false;
+        }
+        if (!isBST(root.getLeft()) || !isBST(root.getRight())) {
+            return false;
+        }
+        return true;
     }
-     public static void main(String args[]) {
+
+    public static boolean isBSTBestWay(BinarySearchTreeNode root, int min, int max) {
+        if (root == null) {
+            return true;
+        }
+        return (root.getData() > min && root.getData() < max) && isBSTBestWay(root.getLeft(), min, root.getData()) && isBSTBestWay(root.getRight(), root.getData(), max);
+    }
+
+    public static void main(String args[]) {
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(3);
         tree.insert(1);
@@ -35,7 +43,7 @@ public class IsBSTRightWay {
         tree.insert(5);
         tree.insert(4);
         tree.insert(6);
-         System.out.println(isBST(tree.getRoot()));
-     }
-    
+        System.out.println(isBST(tree.getRoot()));
+    }
+
 }
