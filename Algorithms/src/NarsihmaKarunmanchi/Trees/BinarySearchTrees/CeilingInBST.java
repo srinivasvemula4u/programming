a25a4a30a401b7b57cc093037cc4c91ab214bca4
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-04 21:56:45 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/BinarySearchTrees/CeilingInBST.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/BinarySearchTrees/CeilingInBST.java
@@ -10,23 +10,28 @@ package NarsihmaKarunmanchi.Trees.BinarySearchTrees;
  * @author srivemul
  */
 public class CeilingInBST {
+
     public static BinarySearchTreeNode prev = null;
-    public static BinarySearchTreeNode ceilingInBST(BinarySearchTreeNode root,int data) {
+
+    public static BinarySearchTreeNode ceilingInBST(BinarySearchTreeNode root, int data) {
         BinarySearchTreeNode prevNode = null;
-        return ceilingInBSTUtils(root,prevNode,data);
+        return ceilingInBSTUtils(root, prevNode, data);
     }
-    public static BinarySearchTreeNode ceilingInBST2(BinarySearchTreeNode root,int data) {
+
+    public static BinarySearchTreeNode ceilingInBST2(BinarySearchTreeNode root, int data) {
         //BinarySearchTreeNode prevNode = null;
-         /*
+        /*
              My own test logic need to think further
             in the main function tree if data = -1 it should return 0. So we need to calculate minimum data in the tree.
          */
-         BinarySearchTreeNode minimumDataNode = FindingMinimumElementInBST.minimum(root);
-         if( data < minimumDataNode.getData())
-             return minimumDataNode;       
-        return ceilingInBSTUtils2(root,data);
+        BinarySearchTreeNode minimumDataNode = FindingMinimumElementInBST.minimum(root);
+        if (data < minimumDataNode.getData()) {
+            return minimumDataNode;
+        }
+        return ceilingInBSTUtils2(root, data);
     }
-     /*
+
+    /*
     This method  ceilingInBSTUtils doesnt work for the following tree
      BinarySearchTree tree = new BinarySearchTree();
         tree.insert(3);
@@ -40,34 +45,44 @@ public class CeilingInBST {
         System.out.println(node != null ? node.getData() : node);
     Output : null instead of 5.
     reason is prevNode becoming null;  
-    */
+     */
     private static BinarySearchTreeNode ceilingInBSTUtils(BinarySearchTreeNode root, BinarySearchTreeNode prevNode, int data) {
-        if(root == null)
+        if (root == null) {
             return null;
+        }
         BinarySearchTreeNode right = ceilingInBSTUtils(root.getRight(), prevNode, data);
-        if(right != null)
+        if (right != null) {
             return right;
-        if(root != null && root.getData() == data)
+        }
+        if (root != null && root.getData() == data) {
             return root;
-        if( root.getData()  < data)
+        }
+        if (root.getData() < data) {
             return prevNode;
+        }
         prevNode = root;
         return ceilingInBSTUtils(root.getLeft(), prevNode, data);
     }
-     private static BinarySearchTreeNode ceilingInBSTUtils2(BinarySearchTreeNode root, int data) {
-       if(root == null)
-           return null;
-       BinarySearchTreeNode right = ceilingInBSTUtils2(root.getRight(), data);
-       if( right != null)
-           return right;
-       if( root != null && root.getData() == data)
-           return root;
-       if(root.getData() < data)
-           return prev;
-       prev = root;
-       return ceilingInBSTUtils2(root.getLeft(), data);
+
+    private static BinarySearchTreeNode ceilingInBSTUtils2(BinarySearchTreeNode root, int data) {
+        if (root == null) {
+            return null;
+        }
+        BinarySearchTreeNode right = ceilingInBSTUtils2(root.getRight(), data);
+        if (right != null) {
+            return right;
+        }
+        if (root != null && root.getData() == data) {
+            return root;
+        }
+        if (root.getData() < data) {
+            return prev;
+        }
+        prev = root;
+        return ceilingInBSTUtils2(root.getLeft(), data);
     }
-     public static void main(String args[]) {
+
+    public static void main(String args[]) {
         BinarySearchTree tree = new BinarySearchTree();
         tree.insert(3);
         tree.insert(1);
@@ -76,10 +91,9 @@ public class CeilingInBST {
         tree.insert(5);
         tree.insert(7);
         tree.insert(10);
-       //  BinarySearchTreeNode node = ceilingInBST(tree.getRoot(), 4);
+        //  BinarySearchTreeNode node = ceilingInBST(tree.getRoot(), 4);
         BinarySearchTreeNode node = ceilingInBST2(tree.getRoot(), -100);
         System.out.println(node != null ? node.getData() : node);
     }
 
-   
 }
