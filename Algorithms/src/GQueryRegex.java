b9f2ccb44f3ded1123a611554fbe6b98ccfbd828
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-05 10:42:51 +0530

    ..

---
+++ Algorithms/src/GQueryRegex.java
@@ -0,0 +1,25 @@
+
+import java.util.regex.Pattern;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author sv186040
+ */
+public class GQueryRegex {
+
+    public static final Pattern pat = Pattern.compile("\"([&#]?)([^&#]+)\"");
+
+    static final String parse(String str) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
