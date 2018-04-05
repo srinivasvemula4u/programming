bff5387f7e4ab415441c500653dc009bdb131f63
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-05 10:59:16 +0530

    ..

--- Algorithms/src/RegularExpressionExample.java
+++ Algorithms/src/RegularExpressionExample.java
@@ -24,23 +24,21 @@ public class RegularExpressionExample {
         Pattern pattern1 = Pattern.compile("^(?:http|https)([A-Fa-f0-9]+)$");
         Matcher matcher = pattern.matcher(EXAMPLE_TEST);
         Matcher matcher1 = pattern1.matcher(EXAMPLE_TEST);
-        
-        
+
         // check all occurance
         while (matcher.find()) {
             System.out.print("Start index: " + matcher.start());
             System.out.print(" End index: " + matcher.end() + " ");
             System.out.println(matcher.group());
-            System.out.println(matcher.group(0)+" "+matcher.group(1) + " "+matcher.group(2));
+            System.out.println(matcher.group(0) + " " + matcher.group(1) + " " + matcher.group(2));
         }
-        
-        
+
         while (matcher1.find()) {
             System.out.print("Start index: " + matcher1.start());
             System.out.print(" End index: " + matcher1.end() + " ");
             System.out.println(matcher1.group());
         }
-        
+
         //System.out.println();
     }
 }
