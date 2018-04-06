008ec7b6084a28c86c004b1aea941d63f9c1a134
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-06 22:54:38 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/AVLTree.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/AVLTree.java
@@ -88,7 +88,6 @@ public class AVLTree {
     public static void main(String args[]) {
         AVLTree tree = new AVLTree();
         tree.insert(3);
-        //  
         tree.insert(1);
         tree.insert(2);
 //        tree.insert(0);
