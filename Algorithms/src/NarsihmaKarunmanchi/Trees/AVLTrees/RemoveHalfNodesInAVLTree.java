ff94e60c038cb184bd3d6da51e0417a8460d53ec
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-06 23:20:35 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/RemoveHalfNodesInAVLTree.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/RemoveHalfNodesInAVLTree.java
@@ -10,36 +10,42 @@ package NarsihmaKarunmanchi.Trees.AVLTrees;
  * @author srivemul
  */
 public class RemoveHalfNodesInAVLTree {
+
     public static AVLTreeNode removeHalfNodes(AVLTreeNode root) {
-        if(root == null)
+        if (root == null) {
             return null;
+        }
         root.setLeft(removeHalfNodes(root.getLeft()));
         root.setRight(removeHalfNodes(root.getRight()));
-        if(root.getLeft() == null && root.getRight() == null)
+        if (root.getLeft() == null && root.getRight() == null) {
             return root;
-        if(root.getLeft() == null) 
+        }
+        if (root.getLeft() == null) {
             return root.getRight();
-        if(root.getRight() == null)
+        }
+        if (root.getRight() == null) {
             return root.getLeft();
+        }
         return root;
     }
-      public static void main(String args[]) {
+
+    public static void main(String args[]) {
         AVLTree tree = new AVLTree();
         tree.insert(0);
         tree.insert(3);
         tree.insert(1);
-       tree.insert(2);
-       // tree.insert(5);
+        tree.insert(2);
+        // tree.insert(5);
         //tree.insert(4);
-       // tree.insert(6);
-       // inOrderTraversal(tree.getRoot());
+        // tree.insert(6);
+        // inOrderTraversal(tree.getRoot());
         removeHalfNodes(tree.getRoot());
         AVLTree.inOrderTraversal(tree.getRoot());
         System.out.println();
         System.out.println(tree.getRoot().getData());
-              System.out.println(tree.getRoot().getLeft().getData());
-                         System.out.println(tree.getRoot().getRight().getData());
-       // System.out.println(isAVL(tree.getRoot()));
+        System.out.println(tree.getRoot().getLeft().getData());
+        System.out.println(tree.getRoot().getRight().getData());
+        // System.out.println(isAVL(tree.getRoot()));
         //  postOrderTraversalIterative(tree.getRoot());
         // levelOrderTraversal(tree.getRoot());
     }
