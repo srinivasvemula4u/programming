995adf64c5ee4f403245b7b83cb7905e7e1a63e7
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-06 23:10:00 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/GeneratingHightBalancedTreeOfH.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/GeneratingHightBalancedTreeOfH.java
@@ -11,34 +11,40 @@ package NarsihmaKarunmanchi.Trees.AVLTrees;
  */
 // HB(0) is nothing but generating full binary tree.In full binary tree the number of nodes with height h are 2^(h+1) -1.
 public class GeneratingHightBalancedTreeOfH {
+
     private static int count = 0;
+
     public static AVLTreeNode buildHightBalnacedTree(int h) {
-        if(h == 0)
+        if (h == 0) {
             return null;
-        AVLTreeNode left = buildHightBalnacedTree(h-1);
+        }
+        AVLTreeNode left = buildHightBalnacedTree(h - 1);
         AVLTreeNode newNode = new AVLTreeNode(count++);
         newNode.setLeft(left);
-        newNode.setRight(buildHightBalnacedTree(h-1));
-      newNode.setHeight(Math.max(newNode.getLeft() == null ? 0 : newNode.getLeft().getHeight(),newNode.getRight() == null ?
-                0 : newNode.getRight().getHeight()));
+        newNode.setRight(buildHightBalnacedTree(h - 1));
+        newNode.setHeight(Math.max(newNode.getLeft() == null ? 0 : newNode.getLeft().getHeight(), newNode.getRight() == null
+                ? 0 : newNode.getRight().getHeight()));
         return newNode;
     }
-    public static AVLTreeNode buildHBO(int l,int r) {
-       AVLTreeNode temp;
-       if(l > r)
-           return null;
-       int mid = l+(r-l)/2;
-       AVLTreeNode newNode = new AVLTreeNode(mid);
-       newNode.setLeft(buildHBO(l, mid-1));
-       newNode.setRight(buildHBO(mid+1, r));
-         newNode.setHeight(Math.max(newNode.getLeft() == null ? 0 : newNode.getLeft().getHeight(),newNode.getRight() == null ?
-                0 : newNode.getRight().getHeight()));
-       return newNode;
+
+    public static AVLTreeNode buildHBO(int l, int r) {
+        AVLTreeNode temp;
+        if (l > r) {
+            return null;
+        }
+        int mid = l + (r - l) / 2;
+        AVLTreeNode newNode = new AVLTreeNode(mid);
+        newNode.setLeft(buildHBO(l, mid - 1));
+        newNode.setRight(buildHBO(mid + 1, r));
+        newNode.setHeight(Math.max(newNode.getLeft() == null ? 0 : newNode.getLeft().getHeight(), newNode.getRight() == null
+                ? 0 : newNode.getRight().getHeight()));
+        return newNode;
     }
-       public static void main(String args[]) {
+
+    public static void main(String args[]) {
         AVLTreeNode root = buildHightBalnacedTree(3);
         AVLTree.inOrderTraversal(root);
-        root = buildHBO(0,3);
-        
+        root = buildHBO(0, 3);
+
     }
 }
