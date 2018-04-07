c28d0a7379fbaeb3197ec751de01bab889170b0d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-07 20:59:57 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Arrays/SlidingWindowMaximun.java
+++ Algorithms/src/GeeksForGeeks/Arrays/SlidingWindowMaximun.java
@@ -7,6 +7,7 @@ package GeeksForGeeks.Arrays;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  *
@@ -112,6 +113,51 @@ Time Complexity: Time Complexity of step 1 is O(kLogk). Time Complexity of
     we can observe that every element of array is added and removed at most once. So there are total 2n operations.
 Auxiliary Space: O(k)
      */
+    static void printMax(int arr[], int n, int k) {
+        // Create a Double Ended Queue, Qi that will store indexes of array elements
+        // The queue will store indexes of useful elements in every window and it will
+        // maintain decreasing order of values from front to rear in Qi, i.e., 
+        // arr[Qi.front[]] to arr[Qi.rear()] are sorted in decreasing order
+        Deque<Integer> Qi = new LinkedList<Integer>();
+
+        /* Process first k (or first window) elements of array */
+        int i;
+        for (i = 0; i < k; ++i) {
+            // For very element, the previous smaller elements are useless so
+            // remove them from Qi
+            while (!Qi.isEmpty() && arr[i] >= arr[Qi.peekLast()]) {
+                Qi.removeLast();   // Remove from rear
+            }
+            // Add new element at rear of queue
+            Qi.addLast(i);
+        }
+
+        // Process rest of the elements, i.e., from arr[k] to arr[n-1]
+        for (; i < n; ++i) {
+            // The element at the front of the queue is the largest element of
+            // previous window, so print it
+            System.out.print(arr[Qi.peek()] + " ");
+
+            // Remove the elements which are out of this window
+            while ((!Qi.isEmpty()) && Qi.peek() <= i - k) {
+                Qi.removeFirst();
+            }
+
+            // Remove all elements smaller than the currently
+            // being added element (remove useless elements)
+            while ((!Qi.isEmpty()) && arr[i] >= arr[Qi.peekLast()]) {
+                Qi.removeLast();
+            }
+
+            // Add current element at the rear of Qi
+            Qi.addLast(i);
+
+        }
+
+        // Print the maximum element of last window
+        System.out.print(arr[Qi.peek()]);
+    }
+
     public static void main(String[] args) {
         int[] arr = {8, 5, 10, 7, 9, 4, 15, 12, 90, 13};
         slidingWindowMaximum(arr, 4);
