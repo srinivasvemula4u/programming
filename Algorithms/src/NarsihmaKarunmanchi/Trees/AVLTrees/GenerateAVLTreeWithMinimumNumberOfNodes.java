f8e2300052f883b1b75d4507c6ef251c7693d269
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-07 09:45:50 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/GenerateAVLTreeWithMinimumNumberOfNodes.java
+++ Algorithms/src/NarsihmaKarunmanchi/Trees/AVLTrees/GenerateAVLTreeWithMinimumNumberOfNodes.java
@@ -11,23 +11,27 @@ package NarsihmaKarunmanchi.Trees.AVLTrees;
  */
 /*
 To get minimum number of nodes fill one level with h-1 and other with h-2
-*/
+ */
 public class GenerateAVLTreeWithMinimumNumberOfNodes {
+
     private static int count = 0;
+
     public static AVLTreeNode GenerateAVLTree(int h) {
-        if(h <= 0)
+        if (h <= 0) {
             return null;
-        AVLTreeNode left = GenerateAVLTree(h-1);
+        }
+        AVLTreeNode left = GenerateAVLTree(h - 1);
         AVLTreeNode newNode = new AVLTreeNode(count++);
         newNode.setLeft(left);
-        newNode.setRight(GenerateAVLTree(h-2)); 
-        newNode.setHeight(Math.max(newNode.getLeft() == null ? 0 : newNode.getLeft().getHeight(),newNode.getRight() == null ?
-                0 : newNode.getRight().getHeight()));
+        newNode.setRight(GenerateAVLTree(h - 2));
+        newNode.setHeight(Math.max(newNode.getLeft() == null ? 0 : newNode.getLeft().getHeight(), newNode.getRight() == null
+                ? 0 : newNode.getRight().getHeight()));
         return newNode;
     }
+
     public static void main(String args[]) {
         AVLTreeNode root = GenerateAVLTree(3);
         AVLTree.inOrderTraversal(root);
-        
+
     }
 }
