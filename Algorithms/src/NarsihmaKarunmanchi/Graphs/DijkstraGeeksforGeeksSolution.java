b8b4080a23559cd34c77cab3f6dcaff629c4e727
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-08 19:01:12 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Graphs/DijkstraGeeksforGeeksSolution.java
+++ Algorithms/src/NarsihmaKarunmanchi/Graphs/DijkstraGeeksforGeeksSolution.java
@@ -87,15 +87,16 @@ public class DijkstraGeeksforGeeksSolution {
 
     public static void main(String[] args) {
         /* Let us create the example graph discussed above */
-        int graph[][] = new int[][]{{0, 4, 0, 0, 0, 0, 0, 8, 0},
-        {4, 0, 8, 0, 0, 0, 0, 11, 0},
-        {0, 8, 0, 7, 0, 4, 0, 0, 2},
-        {0, 0, 7, 0, 9, 14, 0, 0, 0},
-        {0, 0, 0, 9, 0, 10, 0, 0, 0},
-        {0, 0, 4, 0, 10, 0, 2, 0, 0},
-        {0, 0, 0, 14, 0, 2, 0, 1, 6},
-        {8, 11, 0, 0, 0, 0, 1, 0, 7},
-        {0, 0, 2, 0, 0, 0, 6, 7, 0}
+        int graph[][] = new int[][]{
+            {0, 4, 0, 0, 0, 0, 0, 8, 0},
+            {4, 0, 8, 0, 0, 0, 0, 11, 0},
+            {0, 8, 0, 7, 0, 4, 0, 0, 2},
+            {0, 0, 7, 0, 9, 14, 0, 0, 0},
+            {0, 0, 0, 9, 0, 10, 0, 0, 0},
+            {0, 0, 4, 0, 10, 0, 2, 0, 0},
+            {0, 0, 0, 14, 0, 2, 0, 1, 6},
+            {8, 11, 0, 0, 0, 0, 1, 0, 7},
+            {0, 0, 2, 0, 0, 0, 6, 7, 0}
         };
         DijkstraGeeksforGeeksSolution t = new DijkstraGeeksforGeeksSolution();
         t.dijkstra(graph, 0);
