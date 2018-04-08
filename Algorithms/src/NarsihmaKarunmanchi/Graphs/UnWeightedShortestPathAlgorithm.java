ce0bf85566de8467ad67561838750a9ab1959dd7
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-08 16:42:46 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Graphs/UnWeightedShortestPathAlgorithm.java
+++ Algorithms/src/NarsihmaKarunmanchi/Graphs/UnWeightedShortestPathAlgorithm.java
@@ -30,38 +30,39 @@ public class UnWeightedShortestPathAlgorithm {
             path[i] = -1;
         }
         distance[v] = 0;
-       // path[v] = -1;
+        // path[v] = -1;
         while (!queue.isEmpty()) {
             int prevNode = queue.poll();
             Iterator<Integer> it = G.getAdjList()[prevNode].iterator();
             while (it.hasNext()) {
                 int nextNode = it.next();
                 if (distance[nextNode] == 0 && nextNode != v) {
-                  distance[nextNode] = distance[prevNode] +1;
-                  path[nextNode] = prevNode;
-                  queue.offer(nextNode);
+                    distance[nextNode] = distance[prevNode] + 1;
+                    path[nextNode] = prevNode;
+                    queue.offer(nextNode);
                 }
             }
 
         }
-        System.out.println("node"+ " "+"distance"+ " "+"path(From Prvious Node)");
-        for(int i=0; i < G.getNumVertices(); i++) {
-            System.out.println( i + "        "+ distance[i] + "        "+path[i]);
+        System.out.println("node" + " " + "distance" + " " + "path(From Prvious Node)");
+        for (int i = 0; i < G.getNumVertices(); i++) {
+            System.out.println(i + "        " + distance[i] + "        " + path[i]);
         }
     }
+
     public static void main(String args[]) {
-      Graph g = new Graph(7);
-      g.addEdge(0, 3);
-      g.addEdge(0, 1);
-      g.addEdge(1, 4);
-      g.addEdge(1, 3);
-      g.addEdge(2, 0);
-      g.addEdge(2, 5);
-      g.addEdge(3, 5);
-      g.addEdge(3, 6);
-      g.addEdge(4, 6);
-      g.addEdge(6, 5);
-     new UnWeightedShortestPathAlgorithm().unWeightedShortestPath(g,2);
-      //g.DFS2();
-  }
+        Graph g = new Graph(7);
+        g.addEdge(0, 3);
+        g.addEdge(0, 1);
+        g.addEdge(1, 4);
+        g.addEdge(1, 3);
+        g.addEdge(2, 0);
+        g.addEdge(2, 5);
+        g.addEdge(3, 5);
+        g.addEdge(3, 6);
+        g.addEdge(4, 6);
+        g.addEdge(6, 5);
+        new UnWeightedShortestPathAlgorithm().unWeightedShortestPath(g, 2);
+        //g.DFS2();
+    }
 }
