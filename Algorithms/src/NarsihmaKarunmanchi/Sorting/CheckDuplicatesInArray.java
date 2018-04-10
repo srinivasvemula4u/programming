e2babb24ea3585890c3d45a78f429de176e0cc13
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-10 22:10:37 +0530

    ...

--- Algorithms/src/NarsihmaKarunmanchi/Sorting/CheckDuplicatesInArray.java
+++ Algorithms/src/NarsihmaKarunmanchi/Sorting/CheckDuplicatesInArray.java
@@ -12,14 +12,16 @@ import NarsihmaKarunmanchi.PriorityQueues.Heap;
  * @author srivemul
  */
 public class CheckDuplicatesInArray {
+
     public static void main(String args[]) {
         int arr[] = {170, 45, 75, 90, 802, 45, 2, 66};
         int n = arr.length;
         print(arr);
         System.out.println(checkDuplicatesInArrayOptimized(arr));
-       // print(arr);
+        // print(arr);
     }
-     private static void print(int[] ar) {
+
+    private static void print(int[] ar) {
         for (int n : ar) {
             System.out.print(n + " ");
         }
@@ -27,20 +29,24 @@ public class CheckDuplicatesInArray {
     }
 
     private static boolean checkDuplicatesInArray(int[] arr) {
-       for(int i=0; i < arr.length; i++) {
-           for(int j= i+1; j < arr.length; j++) {
-               if(arr[i] == arr[j])
-                   return true;
-           }
-       }
-       return false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
+
     private static boolean checkDuplicatesInArrayOptimized(int[] arr) {
-         Heap heap = new Heap(arr.length, 0,arr);
-         heap.heapSort(arr, arr.length);
-         for(int i=0; i < arr.length-1; i++)
-             if(arr[i] == arr[i+1])
-                 return true;
-         return false;
+        Heap heap = new Heap(arr.length, 0, arr);
+        heap.heapSort(arr, arr.length);
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return true;
+            }
+        }
+        return false;
     }
 }
