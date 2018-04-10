868e238ca8bd1533b3a71504fed98307e87029ca
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-10 22:06:29 +0530

    ...

--- Algorithms/src/princtonbookprograms/anaalysisOfAlgorithms/sorting/InsertionSort.java
+++ Algorithms/src/princtonbookprograms/anaalysisOfAlgorithms/sorting/InsertionSort.java
@@ -10,17 +10,16 @@ package princtonbookprograms.anaalysisOfAlgorithms.sorting;
  * @author srivemul
  */
 public class InsertionSort {
-    public static void sort(Comparable[] a)
-    {
+
+    public static void sort(Comparable[] a) {
         int N = a.length;
-        for(int i =1; i < N; i++)
-        {
-            for(int j = i; j >0 && less(a[j],a[j-1]); j--)
-            {
-                exchange(a,j,j-1);
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exchange(a, j, j - 1);
             }
         }
     }
+
     private static boolean less(Comparable a, Comparable b) {
         //return a < b;
         return a.compareTo(b) < 0;
@@ -32,18 +31,18 @@ public class InsertionSort {
         a[i] = a[j];
         a[j] = temp;
     }
-      public static void print(Comparable a[])
-    {
+
+    public static void print(Comparable a[]) {
         for (Comparable a1 : a) {
             System.out.print(" " + a1);
         }
     }
-      public static void main(String args[])
-    {
-        Comparable[] a = {8,9,1,3,4,6,5,2,7};
+
+    public static void main(String args[]) {
+        Comparable[] a = {8, 9, 1, 3, 4, 6, 5, 2, 7};
         print(a);
         sort(a);
         System.out.println();
-         print(a);
+        print(a);
     }
 }
