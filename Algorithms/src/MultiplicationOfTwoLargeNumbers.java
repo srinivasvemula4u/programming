c3bb3259229436b3422b02744044e7eb9f305caa
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-10 22:38:07 +0530

    ...

---
+++ Algorithms/src/MultiplicationOfTwoLargeNumbers.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sv186040
+ * use integer array to store a biginteger like 297897654 will be stored as {2,9,7,8,9,7,6,5,4}
+ * implement the multiply function for bigintegers
+ * Expamples: {2, 9, 8, 8, 9, 8} * {3,6,3,4,5,8,9,1,2} = {1,0,8,6,3,7,1,4,1,8,7,8,9,7,6}
+ *
+ * 0000298898 x 0363458912 = 00000108637141878976
+ *  int a[10] = {8,9,8,8,9,2};
+ * int b[10] = {2,1,9,8,5,4,3,6,3};
+ */
+public class MultiplicationOfTwoLargeNumbers {
+         
+}
