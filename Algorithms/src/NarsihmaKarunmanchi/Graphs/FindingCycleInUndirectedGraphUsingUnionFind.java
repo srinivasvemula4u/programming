a193328e99fa8887e75f865bb5a0d5029e79a5b1
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-10 08:51:00 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/Graphs/FindingCycleInUndirectedGraphUsingUnionFind.java
+++ Algorithms/src/NarsihmaKarunmanchi/Graphs/FindingCycleInUndirectedGraphUsingUnionFind.java
@@ -28,39 +28,41 @@ public class FindingCycleInUndirectedGraphUsingUnionFind {
         nEdges = e;
         edges = new Edge[e];
         for (int i = 0; i < e; i++) {
-         edges[i] = new Edge();
+            edges[i] = new Edge();
         }
     }
-    int find(int parent[],int i) {
-        if(parent[i] == -1)
+
+    int find(int parent[], int i) {
+        if (parent[i] == -1) {
             return i;
+        }
         return find(parent, parent[i]);
     }
-    void union(int[] parent,int x,int y) {
+
+    void union(int[] parent, int x, int y) {
         int xSet = find(parent, x);
-        int ySet = find(parent,y);
+        int ySet = find(parent, y);
         parent[xSet] = ySet;
     }
-    public boolean isCycle()
-    {
+
+    public boolean isCycle() {
         int[] parent = new int[nVertices];
-        for(int i=0; i < nVertices; i++)
-        {
+        for (int i = 0; i < nVertices; i++) {
             parent[i] = -1;
         }
-        for(int i=0; i < nEdges; i++)
-        {
+        for (int i = 0; i < nEdges; i++) {
             int srcSet = find(parent, edges[i].src);
-            int dstSet = find(parent,edges[i].dest);
-            if(srcSet == dstSet)
+            int dstSet = find(parent, edges[i].dest);
+            if (srcSet == dstSet) {
                 return true;
+            }
             union(parent, srcSet, dstSet);
         }
         return false;
     }
-      // Driver Method
-    public static void main (String[] args)
-    {
+    // Driver Method
+
+    public static void main(String[] args) {
         /* Let us create following graph
          0
         |  \
@@ -68,23 +70,24 @@ public class FindingCycleInUndirectedGraphUsingUnionFind {
         1-----2 */
         int V = 3, E = 3;
         FindingCycleInUndirectedGraphUsingUnionFind graph = new FindingCycleInUndirectedGraphUsingUnionFind(V, E);
- 
+
         // add edge 0-1
         graph.edges[0].src = 0;
         graph.edges[0].dest = 1;
- 
+
         // add edge 1-2
         graph.edges[1].src = 1;
         graph.edges[1].dest = 2;
- 
+
         // add edge 0-2
         graph.edges[2].src = 0;
         graph.edges[2].dest = 2;
- 
-        if (graph.isCycle()== true)
-            System.out.println( "graph contains cycle" );
-        else
-            System.out.println( "graph doesn't contain cycle" );
+
+        if (graph.isCycle() == true) {
+            System.out.println("graph contains cycle");
+        } else {
+            System.out.println("graph doesn't contain cycle");
+        }
     }
 
 }
