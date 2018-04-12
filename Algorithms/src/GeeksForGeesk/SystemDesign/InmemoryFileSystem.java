f433e61fceac65fdbc7654a68dc3b92377664a60
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-12 22:26:49 +0530

    ..

---
+++ Algorithms/src/GeeksForGeesk/SystemDesign/InmemoryFileSystem.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeesk.SystemDesign;
+
+/**
+ *
+ * @author sv186040
+ * 
+ * Explain the data structures and algorithms that you would use to design an in-memory file system. Illustrate with an example in the code logic where possible.
+
+Asked In: Amazon
+
+A file system, in its most simplistic version, 
+* consists of Files and Directories. Each Directory contains a set of 
+* Files and Directories. Since Files and Directories share so many characteristics, 
+* we’ve implemented them such that they inherit from the same class, Entry.
+* 
+ */
+public class InmemoryFileSystem {
+    
+}
