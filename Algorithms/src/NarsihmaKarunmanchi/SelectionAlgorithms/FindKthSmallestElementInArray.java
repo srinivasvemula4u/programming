236b4b8c20cf06849a7569325931f9463cec1910
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-12 14:04:35 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/SelectionAlgorithms/FindKthSmallestElementInArray.java
+++ Algorithms/src/NarsihmaKarunmanchi/SelectionAlgorithms/FindKthSmallestElementInArray.java
@@ -10,57 +10,67 @@ package NarsihmaKarunmanchi.SelectionAlgorithms;
  * @author srivemul
  */
 public class FindKthSmallestElementInArray {
-   private static int getPartition(int[] arr,int low,int high) {
-       int left = low;
+
+    private static int getPartition(int[] arr, int low, int high) {
+        int left = low;
         int right = high;
         int pivot_item = arr[low];
-        while(left < right) {
-            while(left <= high && arr[left] <= pivot_item)
+        while (left < right) {
+            while (left <= high && arr[left] <= pivot_item) {
                 left++;
-            while(right >= low && arr[right] > pivot_item)
+            }
+            while (right >= low && arr[right] > pivot_item) {
                 right--;
-            if(left < right)
-               swap(arr,left,right);
+            }
+            if (left < right) {
+                swap(arr, left, right);
+            }
         }
         arr[low] = arr[right];
         arr[right] = pivot_item;
         return right;
-   }
-   private static int orderstatistics(int[] arr,int start,int end,int k) {
-       int pivot = getPartition(arr, start, end);
-       if(k-1 == pivot)
-           return arr[k-1];
-       if(k-1 < pivot) {
-           return orderstatistics(arr, start, pivot-1, k);
-       }
-       return orderstatistics(arr, pivot+1, end, k);
-   }
-   // Iterative version;
-   private static int orderstatisticsIterative(int[] arr,int start,int end,int k) {
-       int pivot = getPartition(arr, start, end);
-       while(pivot != k-1) {
-           if(k-1 < pivot)
-               end = pivot -1 ;
-           else 
-               start = pivot+1;
-           pivot = getPartition(arr, start, end);
-       }
-       return arr[k-1];
-   }
+    }
+
+    private static int orderstatistics(int[] arr, int start, int end, int k) {
+        int pivot = getPartition(arr, start, end);
+        if (k - 1 == pivot) {
+            return arr[k - 1];
+        }
+        if (k - 1 < pivot) {
+            return orderstatistics(arr, start, pivot - 1, k);
+        }
+        return orderstatistics(arr, pivot + 1, end, k);
+    }
+    // Iterative version;
+
+    private static int orderstatisticsIterative(int[] arr, int start, int end, int k) {
+        int pivot = getPartition(arr, start, end);
+        while (pivot != k - 1) {
+            if (k - 1 < pivot) {
+                end = pivot - 1;
+            } else {
+                start = pivot + 1;
+            }
+            pivot = getPartition(arr, start, end);
+        }
+        return arr[k - 1];
+    }
 
     private static void swap(int[] arr, int start, int end) {
-      int temp = arr[start];
-      arr[start] = arr[end];
-      arr[end] = temp;
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
-    public static int kthSmallest(int [] arr, int k) {
-      return orderstatistics(arr, 0, arr.length-1, k);
+
+    public static int kthSmallest(int[] arr, int k) {
+        return orderstatistics(arr, 0, arr.length - 1, k);
     }
+
     public static void main(String args[]) {
-        
-        int[] a = {9,8,1,3,4,6,5,2,7};
-        int k= kthSmallest(a, 9);
+
+        int[] a = {9, 8, 1, 3, 4, 6, 5, 2, 7};
+        int k = kthSmallest(a, 9);
         System.out.println(k);
-       
+
     }
 }
