12f794e8e9d9fa150be4b8753b4cbf28ebc7911a
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-12 14:35:08 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/DivideAndConquer/ShuffleAnArray.java
+++ Algorithms/src/NarsihmaKarunmanchi/DivideAndConquer/ShuffleAnArray.java
@@ -19,8 +19,8 @@ public class ShuffleAnArray {
     }
 
     private static void shuffleArray(int[] arr, int l, int r) {
-        int c = (l + r) / 2 ;
-        int q =  l + (c - l) / 2 + 1;
+        int c = (l + r) / 2;
+        int q = l + (c - l) / 2 + 1;
         if (l == r) {
             return;
         }
@@ -34,11 +34,12 @@ public class ShuffleAnArray {
     }
 
     public static void main(String args[]) {
-         int[] arr = {1,3,5,7,9,11,13,2,4,6,8,10,12,14};
-         shuffleArray(arr);
-         for(int i : arr) 
-             System.out.print(i+" ");
-         System.out.println();
+        int[] arr = {1, 3, 5, 7, 9, 11, 13, 2, 4, 6, 8, 10, 12, 14};
+        shuffleArray(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 
 }
