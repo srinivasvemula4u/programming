62bca73a6277653b8cf7e6a3d7495229eaf2352a
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-12 14:44:05 +0530

    ..

--- Algorithms/src/princtonbookprograms/anaalysisOfAlgorithms/sorting/QuickSort3Way.java
+++ Algorithms/src/princtonbookprograms/anaalysisOfAlgorithms/sorting/QuickSort3Way.java
@@ -10,54 +10,56 @@ package princtonbookprograms.anaalysisOfAlgorithms.sorting;
  * @author srivemul
  */
 public class QuickSort3Way {
-    public static void sort(Comparable[] a)
-    {
-        sort(a,0,a.length-1);
+
+    public static void sort(Comparable[] a) {
+        sort(a, 0, a.length - 1);
     }
-    private static void sort(Comparable[] a, int low , int high)
-    {
-        if( high <= low)
+
+    private static void sort(Comparable[] a, int low, int high) {
+        if (high <= low) {
             return;
-        int i = low +1;
+        }
+        int i = low + 1;
         Comparable pivot = a[low];
         int lt = low;
         int gt = high;
-        while (i <= gt )
-        {
+        while (i <= gt) {
             int comp = a[i].compareTo(pivot);
-            if( comp < 0)  exchange(a,lt++,i++);
-            else if(comp > 0) exchange(a,i,gt--);
-            else i++;
+            if (comp < 0) {
+                exchange(a, lt++, i++);
+            } else if (comp > 0) {
+                exchange(a, i, gt--);
+            } else {
+                i++;
+            }
         }
-        
-        sort(a,low,lt-1); 
-        if(gt > low) {
-        sort(a,gt-1,high); }
-        else 
-        {
-            sort(a,gt,high);
+
+        sort(a, low, lt - 1);
+        if (gt > low) {
+            sort(a, gt - 1, high);
+        } else {
+            sort(a, gt, high);
         }
     }
 
     private static void exchange(Comparable[] a, int i, int j) {
-        
+
         Comparable temp = a[j];
         a[j] = a[i];
         a[i] = temp;
     }
-    public static void print(Comparable[] a)
-    {
-        for( Comparable a1 : a)
-        {
-            System.out.print(" "+a1);
+
+    public static void print(Comparable[] a) {
+        for (Comparable a1 : a) {
+            System.out.print(" " + a1);
         }
     }
-    public static void main(String args[])
-    {
-        Comparable[] a = {8,9,1,3,4,6,5,2,7};
+
+    public static void main(String args[]) {
+        Comparable[] a = {8, 9, 1, 3, 4, 6, 5, 2, 7};
         print(a);
         sort(a);
         System.out.println();
-         print(a);
+        print(a);
     }
 }
