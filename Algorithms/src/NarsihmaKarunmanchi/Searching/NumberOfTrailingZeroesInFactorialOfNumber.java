d934d7139021291e6b00e3d6f99c2b98bbaed527
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-12 10:32:42 +0530

    ...

--- Algorithms/src/NarsihmaKarunmanchi/Searching/NumberOfTrailingZeroesInFactorialOfNumber.java
+++ Algorithms/src/NarsihmaKarunmanchi/Searching/NumberOfTrailingZeroesInFactorialOfNumber.java
@@ -10,21 +10,23 @@ package NarsihmaKarunmanchi.Searching;
  * @author srivemul
  */
 public class NumberOfTrailingZeroesInFactorialOfNumber {
+
     public static int NumberOfTrailingZeroesInNumber(int n) {
-        int i,count = 0;
-        if(n < 0)
+        int i, count = 0;
+        if (n < 0) {
             return -1;
-        for(i = 5; n/i > 0; i*= 5) {
-            count += n/i;
+        }
+        for (i = 5; n / i > 0; i *= 5) {
+            count += n / i;
         }
         return count;
     }
+
     public static void main(String args[]) {
-      //  int[] Arr = {0,1,1,0,1,2,1,2,0,0,0,1};
-     //   print(Arr);
-      // dutchNationalFlagProblemWithThreeColors(Arr);
-      //   print(Arr);
-        
+        //  int[] Arr = {0,1,1,0,1,2,1,2,0,0,0,1};
+        //   print(Arr);
+        // dutchNationalFlagProblemWithThreeColors(Arr);
+        //   print(Arr);
 
         System.out.println(NumberOfTrailingZeroesInNumber(10));
     }
