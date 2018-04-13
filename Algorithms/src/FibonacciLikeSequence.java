fb607242ef8326148e7a4f11a08ae5792d28a273
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-13 21:35:36 +0530

    ..

---
+++ Algorithms/src/FibonacciLikeSequence.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sv186040
+ * A Fibonacci-like sequence is a sequence of integers a1, a2, ... for which an = an-1+an-2 for all n > 2. You are given the first two elements of the sequnce a1 and a2, and the 1-based index n. Output the n-th element of the sequence.
+
+The input data consists of a single line which contains integers a1, a2 and n separated by single spaces. 0 < a1, a2, n < 10.
+
+Example
+
+input
+
+1 2 3
+
+output
+
+3
+ */
+public class FibonacciLikeSequence {
+    
+}
