81934efb7390d5eb1c27200e2c9eb0f4ce868e89
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-16 06:32:34 +0530

    ..

--- Algorithms/src/NonRepeatingDigitInProduct.java
+++ Algorithms/src/NonRepeatingDigitInProduct.java
@@ -7,48 +7,53 @@ import java.util.Set;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author sv186040
  */
 public class NonRepeatingDigitInProduct {
-     static int nonRepeatingDigitProductCount(int x, int y, int z) {
-         /*
+
+    static int nonRepeatingDigitProductCount(int x, int y, int z) {
+        /*
          * Write your code here.
          */
-        if(x == 1)
+        if (x == 1) {
             return 0;
-        if( y > z) {
-            if(validNonRepatingDigitProductCount(x,y))
+        }
+        if (y > z) {
+            if (validNonRepatingDigitProductCount(x, y)) {
                 return 1;
+            }
         }
         int index = y;
         int counter = 0;
-        while(index <= z) {
-            if(validNonRepatingDigitProductCount(x,index))
+        while (index <= z) {
+            if (validNonRepatingDigitProductCount(x, index)) {
                 counter++;
+            }
             index++;
         }
         return counter;
 
     }
+
     static boolean validNonRepatingDigitProductCount(int x, int index) {
-         Set<Integer> set = new HashSet<>();
-         int temp = index;
-         while(temp > 0) {
-             int rem = temp % 10;
-             temp = temp/10;
+        Set<Integer> set = new HashSet<>();
+        int temp = index;
+        while (temp > 0) {
+            int rem = temp % 10;
+            temp = temp / 10;
             set.add(rem);
-         }
-         long product = x * index;
-         while(product > 0) {
-             int rem = (int)product % 10;
-             product = product / 10;
-             if(set.contains(rem))
-                 return false;
-         }
+        }
+        long product = x * index;
+        while (product > 0) {
+            int rem = (int) product % 10;
+            product = product / 10;
+            if (set.contains(rem)) {
+                return false;
+            }
+        }
         return true;
     }
-	
+
 }
