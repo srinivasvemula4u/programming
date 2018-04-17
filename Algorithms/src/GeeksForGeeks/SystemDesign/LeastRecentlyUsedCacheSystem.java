630a625e9f618c9ad4292473a610b219dff4dd0e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-17 09:07:16 +0530

    ..

--- Algorithms/src/GeeksForGeeks/SystemDesign/LeastRecentlyUsedCacheSystem.java
+++ Algorithms/src/GeeksForGeeks/SystemDesign/LeastRecentlyUsedCacheSystem.java
@@ -4,79 +4,76 @@
  * and open the template in the editor.
  */
 package GeeksForGeeks.SystemDesign;
+
 import NarsihmaKarunmanchi.LinkedLists.DoubleLinkedList;
-import  NarsihmaKarunmanchi.LinkedLists.DoubleLinkedListNode;
+import NarsihmaKarunmanchi.LinkedLists.DoubleLinkedListNode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  *
  * @author sv186040
  */
 
 public class LeastRecentlyUsedCacheSystem {
-   
- private int c;
-    private HashMap<Integer,Integer> hm= new HashMap<Integer,Integer>();
-    private ArrayList<Integer> l= new ArrayList<Integer>();
-    private int count=0;
-    
+
+    private int c;
+    private HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+    private ArrayList<Integer> l = new ArrayList<Integer>();
+    private int count = 0;
+
     public LeastRecentlyUsedCacheSystem(int capacity) {
-        this.c= capacity;
+        this.c = capacity;
     }
-    
+
     /*
     list with keys
     list with key and value
-    */
-    
+     */
     public int get(int key) {
-            if(hm.containsKey(key)){
-                removeElement(key);
-                addInFront(key,hm.get(key));
-                return hm.get(key);
-            }
-            else{
-                return -1;
-            }
-    }
-    
-    public void set(int key, int value) {
-        if(!hm.containsKey(key)){
-            if(count < c){
-                addInFront(key,value);
-            }
-            else{
-                removeLast();
-                addInFront(key,value);
-                
-            }
-        }
-        else{
+        if (hm.containsKey(key)) {
             removeElement(key);
-            addInFront(key,value);
+            addInFront(key, hm.get(key));
+            return hm.get(key);
+        } else {
+            return -1;
         }
-        
     }
-    
-    private void removeElement(int no){
-        int index= l.indexOf(no);
+
+    public void set(int key, int value) {
+        if (!hm.containsKey(key)) {
+            if (count < c) {
+                addInFront(key, value);
+            } else {
+                removeLast();
+                addInFront(key, value);
+
+            }
+        } else {
+            removeElement(key);
+            addInFront(key, value);
+        }
+
+    }
+
+    private void removeElement(int no) {
+        int index = l.indexOf(no);
         l.remove(index);
         count--;
     }
-    
-    private void addInFront(int key,int no){
-        l.add(0,key);
-        hm.put(key,no);
+
+    private void addInFront(int key, int no) {
+        l.add(0, key);
+        hm.put(key, no);
         count++;
     }
-    
-    private void removeLast(){
-        int key=l.get(l.size()-1);
-        l.remove(l.size()-1);
+
+    private void removeLast() {
+        int key = l.get(l.size() - 1);
+        l.remove(l.size() - 1);
         hm.remove(key);
         count--;
     }
-    
-    
+
 }
