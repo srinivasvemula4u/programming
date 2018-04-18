68600effd87f56b89c6dacd5e18492295fda4475
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-18 14:12:29 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/DynamicProgramming/KanpSackProblem.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.DynamicProgramming;
+
+/**
+ *
+ * @author sv186040
+ */
+public class KanpSackProblem {
+     // A utility function that returns maximum of two integers
+     static int max(int a, int b) { return (a > b)? a : b; }
+}
