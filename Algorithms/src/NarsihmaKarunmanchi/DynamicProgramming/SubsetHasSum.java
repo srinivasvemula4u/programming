8d0f7ea9a8078d063ad060e7c9ccb660ed1b9b93
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-18 09:45:36 +0530

    ..

--- Algorithms/src/NarsihmaKarunmanchi/DynamicProgramming/SubsetHasSum.java
+++ Algorithms/src/NarsihmaKarunmanchi/DynamicProgramming/SubsetHasSum.java
@@ -10,23 +10,27 @@ package NarsihmaKarunmanchi.DynamicProgramming;
  * @author srivemul
  */
 public class SubsetHasSum {
+
     public static int subsetHasSum(int[] arr) {
         int K = 0;
-        for(int i =0; i < arr.length; i++) 
-           K += arr[i];
-        int[] T = new int[K+1];
+        for (int i = 0; i < arr.length; i++) {
+            K += arr[i];
+        }
+        int[] T = new int[K + 1];
         T[0] = 1;
-        for(int i=0; i < arr.length; i++) {
-            for(int j = K - arr[i]; j >= 0; j--) {
-                if(T[j] == 1)
-                    T[j+arr[i]] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = K - arr[i]; j >= 0; j--) {
+                if (T[j] == 1) {
+                    T[j + arr[i]] = 1;
+                }
             }
         }
-       
-       return T[K/2]; 
+
+        return T[K / 2];
     }
+
     public static void main(String args[]) {
-        int[] arr = {1,2,3,4,20};
+        int[] arr = {1, 2, 3, 4, 20};
         System.out.println(subsetHasSum(arr));
     }
 }
