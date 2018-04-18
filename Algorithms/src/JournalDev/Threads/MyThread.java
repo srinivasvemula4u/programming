3605b2f1e28b3be550356e8452dc682576461a9b
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-18 08:42:05 +0530

    ..

--- Algorithms/src/JournalDev/Threads/MyThread.java
+++ Algorithms/src/JournalDev/Threads/MyThread.java
@@ -17,7 +17,7 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        System.out.println("MyThread - START "+Thread.currentThread().getName());
+        System.out.println("MyThread - START " + Thread.currentThread().getName());
         try {
             Thread.sleep(1000);
             //Get database connection, delete unused data from DB
@@ -25,11 +25,11 @@ public class MyThread extends Thread {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("MyThread - END "+Thread.currentThread().getName());
+        System.out.println("MyThread - END " + Thread.currentThread().getName());
     }
 
     private void doDBProcessing() throws InterruptedException {
         Thread.sleep(5000);
     }
-    
+
 }
