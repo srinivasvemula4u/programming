3021549cdde4b6773eaf40aaa48f554eb190a5bf
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-19 21:55:22 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Java/FunctionalInterfaces.java
+++ Algorithms/src/GeeksForGeeks/Java/FunctionalInterfaces.java
@@ -24,6 +24,8 @@ public class FunctionalInterfaces {
             }
         }).start();
         /* Java 8 onwards, we can assign lambda expression to its functional interface object like this:*/
-        
+         // lambda expression to create the object
+    new Thread(()->
+       {System.out.println("New thread created");}).start();
     }
 }
