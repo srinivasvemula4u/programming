12a0ebceae3d451374bb8f759293dc3041f1755a
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-19 21:47:36 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Java/InnerClassInsideIfBlock.java
+++ Algorithms/src/GeeksForGeeks/Java/InnerClassInsideIfBlock.java
@@ -9,6 +9,38 @@ package GeeksForGeeks.Java;
  *
  * @author sv186040
  */
-public class InnerClassInsideIfBlock {
-    
+// Java program to illustrate Declaration of 
+// local inner classes inside an if statement
+public class InnerClassInsideIfBlock
+{
+    public int data = 10;
+    public int getData()
+    {
+        return data;
+    }
+    public static void main(String[] args)
+    {
+        InnerClassInsideIfBlock outer = new InnerClassInsideIfBlock();
+         
+        if(outer.getData() < 20)
+        {
+            // Local inner class inside if clause
+            class Inner
+            {
+                public int getValue()
+                {
+                    System.out.println("Inside Inner class");
+                    return outer.data;
+                }
+            }
+  
+            Inner inner = new Inner();
+            System.out.println(inner.getValue());
+        }
+        else
+        {
+            System.out.println("Inside Outer class");
+        }
+    }
 }
+
