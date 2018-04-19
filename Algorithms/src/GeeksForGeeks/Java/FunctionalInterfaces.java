8d675515122f06d5311aecaed32b88c223b07376
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-19 21:54:26 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Java/FunctionalInterfaces.java
+++ Algorithms/src/GeeksForGeeks/Java/FunctionalInterfaces.java
@@ -12,7 +12,18 @@ package GeeksForGeeks.Java;
 /*
 A functional interface is an interface that contains only one abstract method. They can have only one functionality to exhibit. From Java 8 onwards, lambda expressions can be used to represent the instance of a functional interface. A functional interface can have any number of default methods. Runnable, ActionListener, Comparable are some of the examples of functional interfaces.
 Before Java 8, we had to create anonymous inner class objects or implement these interfaces.
-*/
+ */
 public class FunctionalInterfaces {
-    
+
+    public static void main(String args[]) {
+        // create anonymous inner class object
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("New thread created");
+            }
+        }).start();
+        /* Java 8 onwards, we can assign lambda expression to its functional interface object like this:*/
+        
+    }
 }
