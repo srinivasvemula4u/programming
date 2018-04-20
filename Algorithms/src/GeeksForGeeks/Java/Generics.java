f048fd742f06f455e702b0e22d8944e72be1c182
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-20 06:32:46 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Java/Generics.java
+++ Algorithms/src/GeeksForGeeks/Java/Generics.java
@@ -40,6 +40,28 @@ class Test<T> {
     }
 }
 
+//We can also pass multiple Type parameters in Generic classes.
+// A Simple Java program to show multiple
+// type parameters in Java Generics
+// We use < > to specify Parameter type
+class Test1<T, U> {
+
+    T obj1;  // An object of type T
+    U obj2;  // An object of type U
+
+    // constructor
+    Test1(T obj1, U obj2) {
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+    }
+
+    // To print objects of T and U
+    public void print() {
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+}
+
 public class Generics {
 
     public static void main(String[] args) {
@@ -51,5 +73,10 @@ public class Generics {
         Test<String> sObj
                 = new Test<String>("GeeksForGeeks");
         System.out.println(sObj.getObject());
+
+        Test1<String, Integer> obj
+                = new Test1("GfG", 15);
+
+        obj.print();
     }
 }
