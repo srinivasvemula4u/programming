e6782d47079669d192f31a2f6d551d88b289a858
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-20 06:41:09 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Java/Generics.java
+++ Algorithms/src/GeeksForGeeks/Java/Generics.java
@@ -94,3 +94,15 @@ public class Generics {
                 + " = " + element);
     }
 }
+/*
+
+Advantages of Generics:
+
+Programs that uses Generics has got many benefits over non-generic code.
+Code Reuse: We can write a method/class/interface once and use for any type we want.
+.
+
+Type Safety : Generics make errors to appear compile time than at run time (It’s always better to know problems in your code at compile time rather than making your code fail at run time). Suppose you want to create an ArrayList that store name of students and if by mistake programmer adds an integer object instead of string, compiler allows it. But, when we retrieve this data from ArrayList, it causes problems at runtime.
+Individual Type Casting is not needed: If we do not use generics, then, in the above example every-time we retrieve data from ArrayList, we have to typecast it. Typecasting at every retrieval operation is a big headache. If we already know that our list only holds string data then we need not to typecast it every time.
+Implementing generic algorithms: By using generics, we can implement algorithms that work on different types of objects and at the same they are type safe too.
+*/
