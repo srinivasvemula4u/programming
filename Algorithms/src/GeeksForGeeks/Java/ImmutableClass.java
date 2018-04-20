326bf51efb46ab62afd2e050b5f05d103172145f
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-20 06:04:55 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Java/ImmutableClass.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Java;
+
+/**
+ *
+ * @author sv186040
+ */
+/*
+Immutable class means that once an object is created, we cannot change its content. In Java, all the wrapper classes (like String, Boolean, Byte, Short) and String class is immutable. We can create our own immutable class as well.
+
+Following are the requirements:
+• Class must be declared as final (So that child classes can’t be created)
+• Data members in the class must be declared as final (So that we can’t change the value of it after object creation)
+• A parameterized constructor
+• Getter method for all the variables in it
+• No setters(To not have option to change the value of the instance variable)
+
+Example to create Immutable class
+*/
+public class ImmutableClass {
+    
+}
