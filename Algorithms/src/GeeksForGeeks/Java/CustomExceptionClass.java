6af9fdb6bbf187aa178581b9e5459a90875ff7db
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-21 15:42:55 +0530

    test

--- Algorithms/src/GeeksForGeeks/Java/CustomExceptionClass.java
+++ Algorithms/src/GeeksForGeeks/Java/CustomExceptionClass.java
@@ -9,6 +9,24 @@ package GeeksForGeeks.Java;
  *
  * @author sv186040
  */
+class MyException extends Exception {
+
+    public MyException(String str) {
+        super(str);
+    }
+}
+
 public class CustomExceptionClass {
-    
+
+    public static void main(String[] args) {
+        try {
+            // Throw an object of user defined exception
+            throw new MyException("GeeksGeeks");
+        } catch (MyException ex) {
+            System.out.println("Caught");
+
+            // Print the message from MyException object
+            System.out.println(ex.getMessage());
+        }
+    }
 }
