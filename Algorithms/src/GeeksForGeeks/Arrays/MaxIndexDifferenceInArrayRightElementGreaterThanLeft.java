3e78034a1d0256b283b545f030d46a81f642b97f
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-23 07:49:56 +0530

    test

--- Algorithms/src/GeeksForGeeks/Arrays/MaxIndexDifferenceInArrayRightElementGreaterThanLeft.java
+++ Algorithms/src/GeeksForGeeks/Arrays/MaxIndexDifferenceInArrayRightElementGreaterThanLeft.java
@@ -17,42 +17,45 @@ public class MaxIndexDifferenceInArrayRightElementGreaterThanLeft {
     private static int maxIndexDiff(int[] arr) {
         int n = arr.length;
         int maxDiff = Integer.MIN_VALUE;
-        for(int i =0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                if( arr[j] > arr[i] &&  (j - i) > maxDiff)
-                    maxDiff = (j-i);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] > arr[i] && (j - i) > maxDiff) {
+                    maxDiff = (j - i);
+                }
             }
         }
         return maxDiff;
     }
-    
+
     private static int maxIndexDiffMethod2(int[] arr) {
         int n = arr.length;
         int[] minLeft = new int[n];
         int[] maxRight = new int[n];
         minLeft[0] = arr[0];
-        for(int i =1; i < n; i++) 
-            minLeft[i] = Math.min(minLeft[i-1], arr[i]);
-        maxRight[n-1] = arr[n-1];
-        for(int i = n-2; i >=0; i--) {
-            maxRight[i] = Math.max(arr[i], maxRight[i+1]);
+        for (int i = 1; i < n; i++) {
+            minLeft[i] = Math.min(minLeft[i - 1], arr[i]);
+        }
+        maxRight[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(arr[i], maxRight[i + 1]);
         }
         int maxDiff = Integer.MIN_VALUE;
-        for(int i =0, j = 0; i < n && j < n;) {
-            if(minLeft[i] < maxRight[j]) {
-               maxDiff = Math.max(maxDiff, j-i);
-               j++;
-        }
-            else 
+        for (int i = 0, j = 0; i < n && j < n;) {
+            if (minLeft[i] < maxRight[j]) {
+                maxDiff = Math.max(maxDiff, j - i);
+                j++;
+            } else {
                 i++;
+            }
+        }
+        return maxDiff;
     }
-       return maxDiff; 
-    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arr = {34, 8, 10, 3, 2, 80, 30, 33, 1};
         System.out.println(maxIndexDiff(arr));
         System.out.println(maxIndexDiffMethod2(arr));
     }
-    
+
 }
