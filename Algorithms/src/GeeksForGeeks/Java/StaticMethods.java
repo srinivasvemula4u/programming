cd8a3cb4fd1ecfcb67ec74730e1b790368d40c47
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-24 21:44:00 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Java/StaticMethods.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Java;
+
+/**
+ *
+ * @author sv186040
+ */
+class A {
+    public void method1() {
+        System.out.println("A::Method1()");
+    }
+    protected void method2() {
+        System.out.println("A::Method2()");
+    }
+    private void method3() {
+        System.out.println("A::Method3()");
+    }
+    private static void method4() {
+        System.out.println("A::Method4()");
+    }
+    public static void method5() {
+        System.out.println("A::Method5()");
+    }
+    protected static void method6() {
+        System.out.println("A::Method6()");
+    }
+}
+public class StaticMethods {
+    
+}
