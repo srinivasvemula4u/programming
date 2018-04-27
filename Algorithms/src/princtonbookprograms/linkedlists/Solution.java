f1b53eb747b4db0411e446e0ba0db80cbd372e93
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-27 22:35:59 +0530

    ..

--- Algorithms/src/princtonbookprograms/linkedlists/Solution.java
+++ Algorithms/src/princtonbookprograms/linkedlists/Solution.java
@@ -8,7 +8,8 @@ package princtonbookprograms.linkedlists;
 /**
  *
  * @author srivemul
- * */
+ *
+ */
 import java.io.*;
 import java.util.*;
 
@@ -18,23 +19,25 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
         int maxSum = Integer.MIN_VALUE;
-       
+
         int n = scan.nextInt();
         int[][] arr = new int[n][n];
-        for(int i=0; i < n; i++)
-            for(int j=0; j < n; j++) {
-            arr[i][j] = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scan.nextInt();
+            }
         }
-        for(int i=0; i < n-2; i++) {
-            for(int j=0; j < n-2; j++) {
-               int sum = 0;
-                for(int k = j ; k <= j + 2; k++) {
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                int sum = 0;
+                for (int k = j; k <= j + 2; k++) {
                     sum += arr[i][k];
-                    sum += arr[i+2][k];
+                    sum += arr[i + 2][k];
                 }
-                sum += arr[i+1][j+1];
-                if(sum > maxSum) 
+                sum += arr[i + 1][j + 1];
+                if (sum > maxSum) {
                     maxSum = sum;
+                }
             }
         }
         System.out.println(maxSum);
