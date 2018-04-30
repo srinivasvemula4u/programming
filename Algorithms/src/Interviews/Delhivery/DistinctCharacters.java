c2c5b181201412316dc511f01f24cc33b2f81329
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-04-30 23:15:45 +0530

    ..

--- Algorithms/src/Interviews/Delhivery/DistinctCharacters.java
+++ Algorithms/src/Interviews/Delhivery/DistinctCharacters.java
@@ -8,39 +8,32 @@ package Interviews.Delhivery;
 /**
  *
  * @author sv186040
- * 
- * You are given a string S of length N and there are Q queries. In each query, you are provided with 2 integers L and R. You need to find the number of distinct characters present in the range L to R.
-
-Input format
-
-First line of Input contains N, length of string S.
-Second line of input contains string S containing lowercase alphabets only.
-Third line of Input contains Q, number of queries
-Each following Q lines contains 2 integers , L and R .
-Output format
-
-For each query, output number of distinct characters in a new line.
-
-Constraints
-
-
-
-
-
-Sample Input
-6
-nikunj 
-2
-1 6
-1 4
-Sample Output
-5
-4
-Explanation
-In the First Query , there are 5 distinct characters from substring 'nikunj ' .
-
-In the Second Query , 4 there are 4 distinct characters from substring 'niku'.
-
+ *
+ * You are given a string S of length N and there are Q queries. In each query,
+ * you are provided with 2 integers L and R. You need to find the number of
+ * distinct characters present in the range L to R.
+ *
+ * Input format
+ *
+ * First line of Input contains N, length of string S. Second line of input
+ * contains string S containing lowercase alphabets only. Third line of Input
+ * contains Q, number of queries Each following Q lines contains 2 integers , L
+ * and R . Output format
+ *
+ * For each query, output number of distinct characters in a new line.
+ *
+ * Constraints
+ *
+ *
+ *
+ *
+ *
+ * Sample Input 6 nikunj 2 1 6 1 4 Sample Output 5 4 Explanation In the First
+ * Query , there are 5 distinct characters from substring 'nikunj ' .
+ *
+ * In the Second Query , 4 there are 4 distinct characters from substring
+ * 'niku'.
+ *
  */
 public class DistinctCharacters {
 
@@ -50,5 +43,5 @@ public class DistinctCharacters {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
