019396d26fc61c09aa496db399ec0af0dcb57824
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 17:16:09 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/CountDivisiblePairs.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays.PairsRelatedQuestions;
+
+/**
+ *
+ * @author sv186040 
+ * Given an array, count pairs in the array such that one
+ * element of pair divides other.
+ *
+ * Examples:
+ *
+ * Input : arr[] = {1, 2, 3} Output : 2 The two pairs are (1, 2) and (1, 3)
+ *
+ * Input : arr[] = {2, 3, 5, 7} Output: 0
+ */
+public class CountDivisiblePairs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
