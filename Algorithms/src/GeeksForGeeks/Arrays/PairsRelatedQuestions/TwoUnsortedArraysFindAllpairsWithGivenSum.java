136eaeeba92c00c8e22937d7ec9c38302afcba7e
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 17:52:07 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/TwoUnsortedArraysFindAllpairsWithGivenSum.java
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/TwoUnsortedArraysFindAllpairsWithGivenSum.java
@@ -23,14 +23,33 @@ Input : arr1[] = {1, 2, 4, 5, 7}
 Output : 1 8
          4 5
          5 4
-     */
- 
+ */
 public class TwoUnsortedArraysFindAllpairsWithGivenSum {
 
-   
- 
+    /*
+    A Naive approach is to simply run two loops and pick elements from both arrays. 
+    One by one check that both elements sum is equal to given value x or not
+     */
+    // Function to print all pairs in both arrays
+    // whose sum is equal to given value x
+    static void findPairs(int arr1[], int arr2[], int n,
+            int m, int x) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr1[i] + arr2[j] == x) {
+                    System.out.println(arr1[i] + " "
+                            + arr2[j]);
+                }
+            }
+        }
+    }
+
+    // Driver code
     public static void main(String[] args) {
-        // TODO code application logic here
+        int arr1[] = {1, 2, 3, 7, 5, 4};
+        int arr2[] = {0, 7, 4, 3, 2, 1};
+        int x = 8;
+        findPairs(arr1, arr2, arr1.length, arr2.length, x);
     }
 
 }
