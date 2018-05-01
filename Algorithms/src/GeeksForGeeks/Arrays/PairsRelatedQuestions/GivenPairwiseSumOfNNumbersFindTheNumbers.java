d809a369c027586b8a8cfd509e033752cb8e165d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 21:17:04 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/GivenPairwiseSumOfNNumbersFindTheNumbers.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays.PairsRelatedQuestions;
+
+/**
+ *
+ * https://www.geeksforgeeks.org/given-pairwise-sum-n-numbers-find-numbers/
+ * Pairwise sum of n (where n >= 3) numbers are given in a specified order, 
+ * find the numbers. The order has pair sum of first and second, then first 
+ * and third, first and fourth, … second and third, second and fourth, .. 
+ * and so on. Consider an example: n = 4, let the numbers be {a, b, c, d}, 
+ * their pairwise sum is given in the order 
+ * arr[] = {a+b, a+c, a+d, b+c, b+d, c+d}.
+
+Examples:
+
+Input  : arr[] = {11, 18, 13, 13, 8, 5}
+Output : {8, 3, 10, 5}
+8+3 = 11, 8+10 = 18, 8+5 = 13, 3+10 = 13,
+3+5 = 8, ...
+
+Input  : arr[] = {13, 10, 14, 9, 17, 21, 
+                  16, 18, 13, 17}
+Output : {3, 10, 7, 11, 6}
+ */
+public class GivenPairwiseSumOfNNumbersFindTheNumbers {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
