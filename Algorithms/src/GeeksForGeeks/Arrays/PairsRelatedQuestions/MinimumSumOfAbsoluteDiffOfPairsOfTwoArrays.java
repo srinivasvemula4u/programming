9abdc3c0b9abf5eda2dc6384ff911dc28883e174
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 21:06:08 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/MinimumSumOfAbsoluteDiffOfPairsOfTwoArrays.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays.PairsRelatedQuestions;
+
+/**
+ *
+ * https://www.geeksforgeeks.org/minimum-sum-absolute-difference-pairs-two-arrays/
+ * Given two arrays a[] and b[] of equal length n. The task is to pair each 
+ * element of array a to an element in array b, such that sum S of absolute 
+ * differences of all the pairs is minimum.
+
+Suppose, two elements a[i] and a[j] (i != j) of a are paired with elements b[p]
+* and b[q] of b respectively,
+then p should not be equal to q.
+
+Examples:
+
+Input :  a[] = {3, 2, 1}
+         b[] = {2, 1, 3}
+Output : 0
+
+Input :  n = 4
+         a[] = {4, 1, 8, 7}
+         b[] = {2, 3, 6, 5}
+Output : 6
+ */
+public class MinimumSumOfAbsoluteDiffOfPairsOfTwoArrays {
+    
+}
