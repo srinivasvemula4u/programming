f5dcc3acbee29a8d4f2dd5898b8f4c703c812800
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 17:50:41 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/TwoUnsortedArraysFindAllpairsWithGivenSum.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays.PairsRelatedQuestions;
+
+/*
+Given two unsorted arrays of distinct elements, 
+the task is to find all pairs from both arrays whose sum is equal to x.
+
+Examples:
+
+Input :  arr1[] = {-1, -2, 4, -6, 5, 7}
+         arr2[] = {6, 3, 4, 0}  
+         x = 8
+Output : 4 4
+         5 3
+
+Input : arr1[] = {1, 2, 4, 5, 7} 
+        arr2[] = {5, 6, 3, 4, 8}  
+        x = 9
+Output : 1 8
+         4 5
+         5 4
+     */
+ 
+public class TwoUnsortedArraysFindAllpairsWithGivenSum {
+
+   
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
