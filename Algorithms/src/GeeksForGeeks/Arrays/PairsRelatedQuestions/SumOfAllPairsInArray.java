f3f91049cf6d53a238f105d44e4c84595f470637
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 20:51:25 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/SumOfAllPairsInArray.java
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/SumOfAllPairsInArray.java
@@ -5,6 +5,9 @@
  */
 package GeeksForGeeks.Arrays.PairsRelatedQuestions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author sv186040
@@ -45,8 +48,48 @@ A efficient approach will be to use a map/hash function to keep a count of
 
 Time Complexity : O(n)
      */
+    public static int sum(int a[], int n) {
+        // Map to keep a count of occurrences
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+
+        // Traverse in the list from start to end
+        // number of times a[i] can be in a pair and
+        // to get the difference we subtract pre_sum
+        int ans = 0, pre_sum = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (i * a[i]) - pre_sum;
+            pre_sum += a[i];
+
+            // If the (a[i]-1) is present then subtract
+            // that value as f(a[i], a[i]-1) = 0
+            if (cnt.containsKey(a[i] - 1)) {
+                ans -= cnt.get(a[i] - 1);
+            }
+
+            // If the (a[i]+1) is present then
+            // add that value as f(a[i], a[i]-1)=0
+            // here we add as a[i]-(a[i]-1)<0 which would 
+            // have been added as negative sum, so we add 
+            // to remove this pair from the sum value
+            if (cnt.containsKey(a[i] + 1)) {
+                ans += cnt.get(a[i] + 1);
+            }
+
+            // keeping a counter for every element
+            if (cnt.containsKey(a[i])) {
+                cnt.put(a[i], cnt.get(a[i]) + 1);
+            } else {
+                cnt.put(a[i], 1);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
+        int a[] = {1, 2, 3, 1, 3};
+        int n = a.length;
+        System.out.println(sum(a, n));
     }
 
 }
