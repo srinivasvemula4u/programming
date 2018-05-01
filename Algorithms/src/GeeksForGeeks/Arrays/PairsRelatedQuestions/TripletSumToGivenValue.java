d7989ce1321c98dd66902310b00f04562269f76d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 17:41:42 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/TripletSumToGivenValue.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays.PairsRelatedQuestions;
+
+/**
+ *
+ * @author sv186040
+ * https://www.geeksforgeeks.org/find-a-triplet-that-sum-to-a-given-value/
+ * Given an array and a value, find if there is a triplet in array whose sum is 
+ * equal to the given value. If there is such a triplet present in array, 
+ * then print the triplet and return true. Else return false. For example, 
+ * if the given array is {12, 3, 4, 1, 6, 9} and given sum is 24, 
+ * then there is a triplet (12, 3 and 9) present in array whose sum is 24.
+ */
+public class TripletSumToGivenValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
