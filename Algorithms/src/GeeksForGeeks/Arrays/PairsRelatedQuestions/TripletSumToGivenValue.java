116396ca7fa1886c8901c0f10239b8c940a00208
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 17:47:02 +0530

    ..

--- Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/TripletSumToGivenValue.java
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/TripletSumToGivenValue.java
@@ -47,6 +47,85 @@ A simple method is to generate all possible triplets and compare the sum
         return false;
     }
 
+    /*
+    Method 2 (Use Sorting)
+Time complexity of the method 1 is O(n^3). 
+The complexity can be reduced to O(n^2) by sorting the array first, 
+and then using method 1 of this post in a loop.
+1) Sort the input array.
+2) Fix the first element as A[i] where i is from 0 to array size – 2. 
+    After fixing the first element of triplet, find the other two elements using
+    method 1 of this post.
+     */
+    // returns true if there is triplet with sum equal
+    // to 'sum' present in A[]. Also, prints the triplet
+    boolean find3NumbersOptimized(int A[], int arr_size, int sum) {
+        int l, r;
+
+        /* Sort the elements */
+        quickSort(A, 0, arr_size - 1);
+
+        /* Now fix the first element one by one and find the
+           other two elements */
+        for (int i = 0; i < arr_size - 2; i++) {
+            // To find the other two elements, start two index variables
+            // from two corners of the array and move them toward each
+            // other
+            l = i + 1; // index of the first element in the remaining elements
+            r = arr_size - 1; // index of the last element
+            while (l < r) {
+                if (A[i] + A[l] + A[r] == sum) {
+                    System.out.print("Triplet is " + A[i] + " ," + A[l]
+                            + " ," + A[r]);
+                    return true;
+                } else if (A[i] + A[l] + A[r] < sum) {
+                    l++;
+                } else // A[i] + A[l] + A[r] > sum
+                {
+                    r--;
+                }
+            }
+        }
+
+        // If we reach here, then no triplet was found
+        return false;
+    }
+
+    int partition(int A[], int si, int ei) {
+        int x = A[ei];
+        int i = (si - 1);
+        int j;
+
+        for (j = si; j <= ei - 1; j++) {
+            if (A[j] <= x) {
+                i++;
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+        int temp = A[i + 1];
+        A[i + 1] = A[ei];
+        A[ei] = temp;
+        return (i + 1);
+    }
+
+    /* Implementation of Quick Sort
+    A[] --> Array to be sorted
+    si  --> Starting index
+    ei  --> Ending index
+     */
+    void quickSort(int A[], int si, int ei) {
+        int pi;
+
+        /* Partitioning index */
+        if (si < ei) {
+            pi = partition(A, si, ei);
+            quickSort(A, si, pi - 1);
+            quickSort(A, pi + 1, ei);
+        }
+    }
+
     // Driver program to test above functions
     public static void main(String[] args) {
         TripletSumToGivenValue triplet = new TripletSumToGivenValue();
