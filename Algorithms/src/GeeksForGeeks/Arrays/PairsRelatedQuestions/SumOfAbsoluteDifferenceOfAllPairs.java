1452bd09b73550e580995e4b8b4edaae0ae7443d
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-01 20:55:47 +0530

    ..

---
+++ Algorithms/src/GeeksForGeeks/Arrays/PairsRelatedQuestions/SumOfAbsoluteDifferenceOfAllPairs.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GeeksForGeeks.Arrays.PairsRelatedQuestions;
+
+/**
+ *
+ * @author sv186040
+ * https://www.geeksforgeeks.org/sum-absolute-differences-pairs-given-array/
+ * Given a sorted array of distinct elements, the task is to find the summation
+ * of absolute differences of all pairs in the given array.
+ *
+ * Examples:
+ *
+ * Input : arr[] = {1, 2, 3, 4} Output: 10 Sum of |2-1| + |3-1| + |4-1| + |3-2|
+ * + |4-2| + |4-3| = 10
+ *
+ * Input : arr[] = {1, 8, 9, 15, 16} Output: 74
+ *
+ * Input : arr[] = {1, 2, 3, 4, 5, 7, 9, 11, 14} Output: 188
+ */
+public class SumOfAbsoluteDifferenceOfAllPairs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
