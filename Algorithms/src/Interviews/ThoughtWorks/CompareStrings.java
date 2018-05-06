125cc754b3858d75b21be4a133069e66897ea768
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-06 19:24:28 +0530

    ..

--- Algorithms/src/Interviews/ThoughtWorks/CompareStrings.java
+++ Algorithms/src/Interviews/ThoughtWorks/CompareStrings.java
@@ -25,11 +25,10 @@ import java.util.Scanner;
  *
  * For each query, print the desired output in a new line.
  *
- * Input Constraints ,  *
- * Sample Input 5 5 11111 00010 1 2 3 4 5 Sample Output NO NO NO NO YES
- * Explanation After 1st query: B = 10010. B < A. (NO) After 2nd query: B =
- * 11010. B < A. (NO) After 3rd query: B = 11110. B < A. (NO) After 4th query: B
- * = 11110. B < A. (NO) After 5th query: B = 11111. B = A. (YES)
+ * Input Constraints , * Sample Input 5 5 11111 00010 1 2 3 4 5 Sample Output NO
+ * NO NO NO YES Explanation After 1st query: B = 10010. B < A. (NO) After 2nd
+ * query: B = 11010. B < A. (NO) After 3rd query: B = 11110. B < A. (NO) After
+ * 4th query: B = 11110. B < A. (NO) After 5th query: B = 11111. B = A. (YES)
  */
 public class CompareStrings {
 
@@ -37,21 +36,15 @@ public class CompareStrings {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         int Q = s.nextInt();
         s.nextLine();
         String A = s.nextLine();
         String B = s.nextLine();
-        //System.out.println(A);
-        //System.out.println(B);
         for (int i = 0; i < Q; i++) {
             int idx = s.nextInt();
-            //   System.out.println(idx);
-            //  System.out.println(B);
             B = B.replaceFirst(Character.toString(B.charAt(idx - 1)), "1");
-            //  System.out.println(B);
             if (A.compareTo(B) <= 0) {
                 System.out.println("YES");
             } else {
@@ -61,4 +54,3 @@ public class CompareStrings {
         }
     }
 }
-
