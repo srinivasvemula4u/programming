8cdd441acbb25385f6df40fe296f20a7993075b9
Srinivas Vemula <srinivasvemula.mtech@gmail.com>
2018-05-25 20:43:11 +0530

    ..

--- Algorithms/src/Interviews/ThoughtWorks/DigitsInANumber.java
+++ Algorithms/src/Interviews/ThoughtWorks/DigitsInANumber.java
@@ -9,25 +9,24 @@ import java.util.Scanner;
 
 /**
  *
- * @author sv186040
- * Digits in a Number
-Let's say  are the only digits which exists in a unique number system, so the numbers will be  in ascending order .
-
- 1 st number is 1 and number of digits is 1
-
-4th  will be 11  and number of digits is 2
-
- 14th will be  and number of digits will be 3
-
- will be  and number of digits will be 4
-
-You have to find number of digits of  term in unique number system .
-
-Input :
-
-First line T contains number of testcases
-
-Next T lines contains N
+ * @author sv186040 Digits in a Number Let's say are the only digits which
+ * exists in a unique number system, so the numbers will be in ascending order .
+ *
+ * 1 st number is 1 and number of digits is 1
+ *
+ * 4th will be 11 and number of digits is 2
+ *
+ * 14th will be and number of digits will be 3
+ *
+ * will be and number of digits will be 4
+ *
+ * You have to find number of digits of term in unique number system .
+ *
+ * Input :
+ *
+ * First line T contains number of testcases
+ *
+ * Next T lines contains N
  */
 public class DigitsInANumber {
 
@@ -35,22 +34,22 @@ public class DigitsInANumber {
      * @param args the command line arguments
      * @throws java.lang.Exception
      */
-     public static void main(String args[] ) throws Exception {
+    public static void main(String args[]) throws Exception {
         //Scanner
         Scanner s = new Scanner(System.in);
-        int T = s.nextInt(); 
-        for(int i = 0; i < T; i++) {
+        int T = s.nextInt();
+        for (int i = 0; i < T; i++) {
             long N = s.nextLong();
             long count = 0;
             long j = 1;
-            while(N > 0) {
-              N = N-(long)Math.pow(3,j);
-              count++;
-              j++;
+            while (N > 0) {
+                N = N - (long) Math.pow(3, j);
+                count++;
+                j++;
             }
             System.out.println(count);
         }
 
     }
-    
+
 }
